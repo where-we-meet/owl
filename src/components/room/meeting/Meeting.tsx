96f9336d5205a1d96a34661920efd612303d49b3
heechul94 <yhcu3517@gmail.com>
2024-03-30 23:39:01 +0900

    feat: 일정과 장소를 탭으로 전환 할 수 있는 Meeting 컴포넌트 생성

---
+++ src/components/room/meeting/Meeting.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { type ReactElement, useState } from "react";
+import Calender from "./Calender";
+import Place from "./Place";
+
+type TabList = { [key: string]: ReactElement };
+
+const tabList: TabList = {
+  일정: <Calender />,
+  장소: <Place />,
+};
+
+const Meeting = () => {
+  const [currentTab, setCurrentTab] = useState("일정");
+
+  return (
+    <section>
+      <ul>
+        <li onClick={() => setCurrentTab("일정")}>일정</li>
+        <li onClick={() => setCurrentTab("장소")}>장소</li>
+      </ul>
+      <div>{tabList[currentTab]}</div>
+    </section>
+  );
+};
+
+export default Meeting;
