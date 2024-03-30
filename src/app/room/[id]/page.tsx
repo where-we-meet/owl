9e92f82b08894529bdc9a6b91026d14a74315b8c
heechul94 <yhcu3517@gmail.com>
2024-03-30 23:41:28 +0900

    feat: RoomPage 레이아웃 생성

---
+++ src/app/room/[id]/page.tsx
@@ -0,0 +1,13 @@
+import UserList from "@/components/room/UserList";
+import Meeting from "@/components/room/meeting/Meeting";
+
+const RoomPage = () => {
+  return (
+    <main>
+      <UserList />
+      <Meeting />
+    </main>
+  );
+};
+
+export default RoomPage;
