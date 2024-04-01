564974f4f6582ba1e6e5bee5b386a38359b901e5
lauichan <te06230623@gmail.com>
2024-04-01 17:06:53 +0900

    feat: 로그인 로그아웃 버튼 컴포넌트

---
+++ src/components/auth/LogoutButton.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
+
+const Logout = () => {
+  const supabase = createClient();
+  const router = useRouter();
+
+  const signOut = async () => {
+    await supabase.auth.signOut();
+    router.push("/login");
+  };
+
+  return <button onClick={signOut}>로그아웃</button>;
+};
+
+export default Logout;
