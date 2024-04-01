229afcec91f7f6d751146097710b868ed16e8739
ketchup0211 <dlcka1111@naver.com>
2024-04-01 14:59:24 +0900

    style : 유저 프로필 UI edit mode 전환을 위한 상태 추가

--- src/components/my-owl/profile/editable/UserInfo.tsx
+++ src/components/my-owl/profile/editable/UserInfo.tsx
@@ -1,11 +1,29 @@
 'use client';
 
+import { useState } from 'react';
+
+import styles from './UserInfo.module.css';
+
 const UserInfo = () => {
+  const [editMode, setEditMode] = useState(true);
+
+  const handleEditMode = () => {
+    setEditMode((prev) => !prev);
+  };
+
+  const handleEditDone = () => {
+    //  변경사항 저장 로직
+    setEditMode(false);
+  };
+
   return (
     <div>
-      <div>UserInfo Image</div>
-      <p>User Name</p>
-      <button>프로필 편집</button>
+      <div className={styles.profile_image}>UserInfo Image</div>
+      <p className={styles.user_name}>User Name</p>
+      <div className={styles.button_container}>
+        <button onClick={handleEditMode}>{editMode ? '취소' : '프로필 편집'}</button>
+        {editMode ? <button onClick={handleEditDone}>완료</button> : null}
+      </div>
     </div>
   );
 };
