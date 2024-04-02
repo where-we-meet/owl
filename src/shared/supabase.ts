430be751bc7c93221012a7d93ed168790964c0d3
lauichan <te06230623@gmail.com>
2024-04-02 11:13:45 +0900

    remove: createClient -> createBrowserClient

--- src/shared/supabase.ts
+++
@@ -1,7 +0,0 @@
-import type { Database } from "@/types/supabase";
-import { createClient, type SupabaseClient } from "@supabase/supabase-js";
-
-export const supabase: SupabaseClient = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
