c05de6bbb9bf66b44c0164430ff78998aadd79c7
lauichan <te06230623@gmail.com>
2024-04-02 11:38:22 +0900

    feat: 라우트 보호 미들웨어

---
+++ src/middlewares/protectRoute.ts
@@ -0,0 +1,30 @@
+import {
+  NextResponse,
+  type NextFetchEvent,
+  type NextRequest,
+} from "next/server";
+
+import { CustomMiddleware } from "./chain";
+import { createClient } from "@/utils/supabase/server";
+
+export function protectRoute(middleware: CustomMiddleware) {
+  return async (
+    request: NextRequest,
+    event: NextFetchEvent,
+    response: NextResponse
+  ) => {
+    const supabase = createClient();
+    const { data, error } = await supabase.auth.getUser();
+
+    const pathname = request.nextUrl.pathname;
+    const protectedPath = ["/room", "/my-owl"];
+
+    if (protectedPath.some((path) => pathname.startsWith(path))) {
+      if (error || !data?.user) {
+        return NextResponse.redirect(new URL(`/login`, request.url));
+      }
+    }
+
+    return middleware(request, event, response);
+  };
+}
