985ae475cabceb9ec07e7a11caf50fbdd6c86900
lauichan <te06230623@gmail.com>
2024-04-02 16:20:21 +0900

    feat: 마이페이지 로그인 로그아웃 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,25 @@
+import { createClient } from '@/utils/supabase/server';
 import Logout from '../auth/LogoutButton';
 import styles from './Header.module.css';
+import Link from 'next/link';
 
-const Header = () => {
+const Header = async () => {
+  const supabase = createClient();
+  const {
+    data: { session }
+  } = await supabase.auth.getSession();
+
   return (
     <header className={styles.header}>
-      Header
-      <Logout />
+      <Link href="/">owl-link</Link>
+      {session ? (
+        <Logout />
+      ) : (
+        <>
+          <Link href="/my-owl">마이페이지</Link>
+          <Link href="/login">로그인</Link>
+        </>
+      )}
     </header>
   );
 };
