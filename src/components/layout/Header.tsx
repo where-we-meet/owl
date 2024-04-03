15754db23d2cd5392df0bf2c907188168a582884
lauichan <te06230623@gmail.com>
2024-04-03 12:39:39 +0900

    fix: 로그인 로그아웃 버튼 안바뀌는 버그 임시 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,19 +1,32 @@
-import { createClient } from '@/utils/supabase/server';
+'use client';
+import { createClient } from '@/utils/supabase/client';
 import Logout from '../auth/LogoutButton';
 import styles from './Header.module.css';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 const Header = async () => {
   const supabase = createClient();
-  const {
-    data: { user }
-  } = await supabase.auth.getUser();
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    const checkUser = async () => {
+      supabase.auth.onAuthStateChange((event, session) => {
+        if (event === 'SIGNED_OUT') {
+          setIsLogin(false);
+        } else if (event === 'SIGNED_IN') {
+          setIsLogin(true);
+        }
+      });
+    };
+    checkUser();
+  }, []);
 
   return (
     <header className={styles.header}>
       <Link href="/">owl-link</Link>
       <div className={styles.menu}>
-        {user ? (
+        {isLogin ? (
           <>
             <Link href="/my-owl">마이페이지</Link>
             <Logout />
