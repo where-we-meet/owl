baa43774e563840044e536b02aa6ad01dcf8e3ba
ketchup0211 <dlcka1111@naver.com>
2024-04-03 12:12:20 +0900

    feat: 유저가 선택한 이미지 supabase에 업로드 중 문제 발생 - 업로드가 안됨

--- src/components/my-owl/profile/editable/modal/Modal.tsx
+++ src/components/my-owl/profile/editable/modal/Modal.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 import styles from './Modal.module.css';
+import { createClient } from '@/utils/supabase/client';
 
 const MAX_FILE_SIZE_BYTE = 2097152; //2MB
+
 export const ImageUploadModal = ({ handleToggleModal }: { handleToggleModal: () => void }) => {
   const [fileSizeExceed, setFileSizeExceed] = useState(false);
+  const [file, setFile] = useState<File | null>(null);
+  const supabase = createClient();
 
   //util
   const byteCalculater = (byte: number) => {
@@ -30,6 +34,7 @@ export const ImageUploadModal = ({ handleToggleModal }: { handleToggleModal: () 
     const { files } = e.target;
     if (files !== null) {
       const size = files[0].size;
+      const file = files[0];
       if (size > MAX_FILE_SIZE_BYTE) {
         alert(
           `선택하신 파일의 용량은 ${byteCalculater(size)}입니다. ${byteCalculater(
@@ -39,8 +44,35 @@ export const ImageUploadModal = ({ handleToggleModal }: { handleToggleModal: () 
         setFileSizeExceed(true);
       } else {
         setFileSizeExceed(false);
+        setFile(file);
       }
-    } else console.log('no data');
+    } else {
+      console.log('no data');
+    }
+  };
+
+  const uploadImage = async () => {
+    console.log('지금부터 이미지를 업로드 해볼게?');
+
+    if (file) {
+      const { data, error } = await supabase.storage.from('images').upload(`users_profile/${file.name}`, file);
+      console.log(error);
+      if (error) {
+        console.error('Error uploading image:', error.message);
+        console.log('에러났다구?');
+      } else {
+        console.log('ㅋㅋ');
+        console.log('Image uploaded successfully:', data);
+
+        setFile(null);
+        return `${process.env.NEXT_PUBLIC_SUPABASE_URL!}/storage/v1/object/public/images/users_profile/${file.name}`;
+      }
+    }
+  };
+
+  const handleUploadImage = async () => {
+    const profile_url = uploadImage();
+    //profile_url
   };
 
   return (
@@ -48,7 +80,9 @@ export const ImageUploadModal = ({ handleToggleModal }: { handleToggleModal: () 
       <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
         <form>
           <input type="file" accept="image/jpeg,image/png,image/jpg" onChange={handleFileMaxSize} />
-          <button disabled={fileSizeExceed}>사진 업로드하기</button>
+          <button disabled={fileSizeExceed} onClick={handleUploadImage}>
+            사진 업로드하기
+          </button>
         </form>
         <p>또는</p>
         <form>
