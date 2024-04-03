f0ee89bbb7d7bc0132b49445f1d95c360f4f1a03
lauichan <te06230623@gmail.com>
2024-04-03 11:24:32 +0900

    chore: 문법 수정

--- src/middlewares/protectRoute.ts
+++ src/middlewares/protectRoute.ts
@@ -1,23 +1,15 @@
-import {
-  NextResponse,
-  type NextFetchEvent,
-  type NextRequest,
-} from "next/server";
+import { NextResponse, type NextFetchEvent, type NextRequest } from 'next/server';
 
-import { CustomMiddleware } from "./chain";
-import { createClient } from "@/utils/supabase/server";
+import { CustomMiddleware } from './chain';
+import { createClient } from '@/utils/supabase/server';
 
 export function protectRoute(middleware: CustomMiddleware) {
-  return async (
-    request: NextRequest,
-    event: NextFetchEvent,
-    response: NextResponse
-  ) => {
+  return async (request: NextRequest, event: NextFetchEvent, response: NextResponse) => {
     const supabase = createClient();
     const { data, error } = await supabase.auth.getUser();
 
     const pathname = request.nextUrl.pathname;
-    const protectedPath = ["/room", "/my-owl"];
+    const protectedPath = ['/room', '/my-owl'];
 
     if (protectedPath.some((path) => pathname.startsWith(path))) {
       if (error || !data?.user) {
