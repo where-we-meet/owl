b3ee6857d0d17ea58877713778a31fbd9a0d5e0b
lauichan <te06230623@gmail.com>
2024-04-03 20:41:47 +0900

    fix: 문법 수정

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,11 +1,11 @@
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
-import { type CookieOptions, createServerClient } from "@supabase/ssr";
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+import { type CookieOptions, createServerClient } from '@supabase/ssr';
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
-  const code = searchParams.get("code");
-  const next = searchParams.get("next") ?? "/";
+  const code = searchParams.get('code');
+  const next = searchParams.get('next') ?? '/';
 
   if (code) {
     const cookieStore = cookies();
@@ -22,13 +22,11 @@ export async function GET(request: Request) {
           },
           remove(name: string, options: CookieOptions) {
             cookieStore.delete({ name, ...options });
-          },
-        },
+          }
+        }
       }
     );
-
     const { error } = await supabase.auth.exchangeCodeForSession(code);
-
     if (!error) {
       return NextResponse.redirect(`${origin}${next}`);
     }
