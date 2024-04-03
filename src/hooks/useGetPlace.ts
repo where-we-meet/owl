5490868269adfed7b467badf3d9f2213c6c54124
heechul94 <yhcu3517@gmail.com>
2024-04-03 15:42:39 +0900

    rename: place 관련 query 모음 목적으로 파일명 변경

--- src/hooks/useGetPlace.ts
+++ src/hooks/useGetPlace.ts
@@ -1,12 +1,19 @@
-import { getAddress } from '@/api/place/placeData';
+import { getAddress, getSearchPlace } from '@/api/place/placeData';
 import { useQuery } from '@tanstack/react-query';
 
 export const useGetRoadAddress = (center: { lat: number; lng: number }) => {
   const { data, isPending } = useQuery({
     queryKey: ['room', center],
     queryFn: async () => await getAddress(center),
-
     enabled: !!center
+  });
+  return { data, isPending };
+};
+
+export const useGetSearchPlace = (searchKeyword: string) => {
+  const { data, isPending } = useQuery({
+    queryKey: ['room', searchKeyword],
+    queryFn: async () => await getSearchPlace(searchKeyword)
   });
   return { data, isPending };
 };
