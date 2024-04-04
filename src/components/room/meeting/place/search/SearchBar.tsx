bcc2be631b66051d3dd7167641535732b1e40cc9
heechul94 <yhcu3517@gmail.com>
2024-04-04 10:07:33 +0900

    feat: 검색바 구현

--- src/components/room/meeting/place/search/SearchBar.tsx
+++ src/components/room/meeting/place/search/SearchBar.tsx
@@ -1,25 +1,48 @@
-import { ChangeEvent, ReactEventHandler, useState } from 'react';
+import { type ChangeEvent, useState, useCallback } from 'react';
+import _ from 'lodash';
+
 import styles from './SearchBar.module.css';
+
+import SearchResultList from './SearchResultList';
+import { useGetSearchPlace } from '@/hooks/useGetPlace';
 
 const SearchBar = () => {
   const [searchKeyword, setSearchKeyword] = useState('');
+  const [listViewState, setListViewState] = useState({
+    inputFocused: false,
+    containerHovered: false
+  });
 
-  const handleSearchInput = (event: ChangeEvent<HTMLInputElement>) => {
-    event.stopPropagation();
-    setSearchKeyword(event.target.value);
+  const handleSearchInput = useCallback(
+    _.debounce((event: ChangeEvent<HTMLInputElement>) => {
+      event.stopPropagation();
+      setSearchKeyword(event.target.value);
+    }, 600),
+    []
+  );
+
+  const handleInputFocus = () => {
+    setListViewState((prev) => ({ ...prev, inputFocused: !prev.inputFocused }));
   };
 
-  console.log(searchKeyword);
+  const { data: placeList, isPending } = useGetSearchPlace(searchKeyword);
+
+  const activatePlaceList = (listViewState.inputFocused && searchKeyword !== '') || listViewState.containerHovered;
+
   return (
-    <form className={styles.search_bar}>
-      <input
-        id="searchBar"
-        placeholder="출발 위치 검색하기"
-        value={searchKeyword}
-        onChange={handleSearchInput}
-        autoComplete="off"
-      />
-    </form>
+    <div className={styles.container}>
+      <form className={styles.search_bar}>
+        <input
+          id="searchBar"
+          placeholder="출발 위치 검색하기"
+          onChange={handleSearchInput}
+          onFocus={handleInputFocus}
+          onBlur={handleInputFocus}
+          autoComplete="off"
+        />
+      </form>
+      {activatePlaceList && <SearchResultList placeList={placeList} setListViewState={setListViewState} />}
+    </div>
   );
 };
 
