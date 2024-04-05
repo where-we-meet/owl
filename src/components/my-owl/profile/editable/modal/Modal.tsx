1c85c56fad65d034154feafa000621f037c4514f
ketchup0211 <dlcka1111@naver.com>
2024-04-05 11:47:35 +0900

    feat: 프로필 이미지를 url을 첨부하여 수정할 수 있도록 구현

--- src/components/my-owl/profile/editable/modal/Modal.tsx
+++ src/components/my-owl/profile/editable/modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useState } from 'react';
+import { ChangeEvent, MouseEvent, useState } from 'react';
 import { changeUserProfile, uploadImage } from '@/api/supabaseCSR/supabase';
 import { byteCalculator } from '@/utils/my-owl/profile/modal/byteCalculator';
 import { getUserId } from '@/utils/my-owl/getUserId';
@@ -10,6 +10,28 @@ const MAX_FILE_SIZE_BYTE = 2097152; //2MB
 export const ImageUploadModal = ({ handleToggleModal }: { handleToggleModal: () => void }) => {
   const [fileSizeExceed, setFileSizeExceed] = useState(false);
   const [file, setFile] = useState<File | null>(null);
+  const [url, setUrl] = useState('');
+  const [isValidUrl, setIsValidUrl] = useState(true);
+
+  //util
+  const byteCalculater = (byte: number) => {
+    const KB = byte / 1024;
+    const MB = KB / 1024;
+    const GB = MB / 1024;
+    const TB = GB / 1024;
+
+    if (TB >= 1) {
+      return `${TB.toFixed(2)} TB`;
+    } else if (GB >= 1) {
+      return `${GB.toFixed(2)} GB`;
+    } else if (MB >= 1) {
+      return `${MB.toFixed(2)} MB`;
+    } else if (KB >= 1) {
+      return `${KB.toFixed(2)} KB`;
+    } else {
+      return `${byte} bytes`;
+    }
+  };
 
   //파일 용량 제한 로직
   const handleFileMaxSize = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -49,27 +71,37 @@ export const ImageUploadModal = ({ handleToggleModal }: { handleToggleModal: () 
   };
 
   const isValidImageUrl = async (url: string) => {
+    // TODO : HTTP 요청을 보내서 응답의 Content-Type 확인하는 로직 넣기 (현재 로직은 불완전함)
     const imageExtensions = ['jpg', 'jpeg', 'png'];
-    const extension = url.split('.').pop();
+    let extension = url.split('.').pop();
     if (extension) {
-      if (!imageExtensions.includes(extension)) {
-        return false;
+      if (extension.includes('?')) {
+        extension = extension.split('?')[0];
       }
-    }
-    try {
-      // HTTP 요청을 보내서 응답의 Content-Type 확인
-      const response = await fetch(url);
-      const contentType = response.headers.get('content-type');
-
-      // Content-Type이 이미지인지 확인
-      if (contentType && contentType.startsWith('image/')) {
+      if (imageExtensions.includes(extension)) {
         return true;
       }
-    } catch (error) {
-      console.error('Error while validating image URL:', error);
     }
-
     return false;
+  };
+
+  const handleChangeURL = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setUrl(e.target.value);
+  };
+
+  const handleUploadURL = async (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setIsValidUrl(await isValidImageUrl(url));
+    if (!isValidUrl) {
+      alert('유효하지 않은 URL 형식입니다. URL 파일의 형식을 확인해주세요.');
+      setUrl('');
+    }
+    const userId = await getUserId();
+    if (userId) changeUserProfile({ userId, profile_url: url });
+    else alert('유저의 신원 확인이 불가능합니다.');
+    setUrl('');
+    handleToggleModal();
   };
 
   return (
@@ -83,8 +115,8 @@ export const ImageUploadModal = ({ handleToggleModal }: { handleToggleModal: () 
         </form>
         <p>또는</p>
         <form>
-          <input type="url" placeholder="Paste link to an image..." />
-          <button>링크 첨부하기</button>
+          <input type="url" placeholder="Paste link to an image..." onChange={handleChangeURL} value={url} />
+          <button onClick={handleUploadURL}>링크 첨부하기</button>
         </form>
         <div className={styles.close_btn} onClick={handleToggleModal}>
           Exit
