df8d7ae659ee2b94f13f6242c3cc0cddd0a71aa9
heechul94 <yhcu3517@gmail.com>
2024-04-05 21:11:27 +0900

    fix: 유저 위치설정 서비스 로직 분리, 전역관린 상태 적용

--- src/hooks/useCenterState.ts
+++ src/hooks/useCenterState.ts
@@ -1,48 +1,55 @@
-import { CenterData } from '@/types/place.types';
-import { useEffect, useState } from 'react';
+import { useGetRoadAddress } from '@/hooks/useGetPlace';
+import { useCallback, useEffect, useState } from 'react';
+import { useSearchDataStore } from '@/store/store';
+import _ from 'lodash';
 
 export const useCenterState = () => {
-  const [centerData, setCenterData] = useState<CenterData>({
-    center: {
-      lat: 33.450701,
-      lng: 126.570667
-    },
-    errMsg: null,
-    isLoading: true,
-    roadAddress: null
-  });
+  const [isGpsLoading, setIsGpsLoading] = useState(true);
+  const [errorMassage, setErrorMassage] = useState('');
+  const [isDrag, setIsDrag] = useState(false);
+
+  const userLocationData = useSearchDataStore((state) => state);
+  const setLocation = useSearchDataStore((state) => state.setLocation);
+  const setAddress = useSearchDataStore((state) => state.setAddress);
+
+  const { data, isPending } = useGetRoadAddress(userLocationData.location, isDrag);
+
+  const setCenter = (map: kakao.maps.Map) => {
+    const latlng = map.getCenter();
+    setLocation({ lat: latlng.getLat(), lng: latlng.getLng() });
+  };
+
+  const handleChangeCenter = useCallback(_.debounce(setCenter, 1000), []);
 
   useEffect(() => {
     if (navigator.geolocation) {
       // GeoLocation을 이용해서 접속 위치를 얻어옵니다
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          setCenterData((prev) => ({
-            ...prev,
-            center: {
-              lat: position.coords.latitude, // 위도
-              lng: position.coords.longitude // 경도
-            },
-            isLoading: false
-          }));
+          setLocation({
+            lat: position.coords.latitude, // 위도
+            lng: position.coords.longitude // 경도
+          });
+          setIsGpsLoading(false);
         },
         (err) => {
-          setCenterData((prev) => ({
-            ...prev,
-            errMsg: err.message,
-            isLoading: false
-          }));
+          setErrorMassage(err.message);
+          setIsGpsLoading(false);
         }
       );
     } else {
       // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-      setCenterData((prev) => ({
-        ...prev,
-        errMsg: 'geolocation을 사용할수 없어요..',
-        isLoading: false
-      }));
+      setErrorMassage('geolocation을 사용할수 없어요..');
+      setIsGpsLoading(false);
     }
   }, []);
 
-  return { centerData, setCenterData };
+  useEffect(() => {
+    if (data) {
+      const address = data.road_address?.address_name || data.address?.address_name;
+      setAddress(address);
+    }
+  }, [data]);
+
+  return { userLocationData, setLocation, handleChangeCenter, isGpsLoading, isDrag, setIsDrag, errorMassage };
 };
