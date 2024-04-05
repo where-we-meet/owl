5216d0a82ac74c52462f873ee1af56f09d459e6e
lauichan <te06230623@gmail.com>
2024-04-05 15:18:57 +0900

    feat: 로그인페이지 레이아웃

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,22 @@
 import LoginButton from '@/components/auth/LoginButton';
+import PaypalMark from '@/components/auth/PaypalMark';
 import styles from './page.module.css';
+import Link from 'next/link';
 
 export default function LoginPage() {
   return (
     <main className={styles.main}>
-      <h1>OWL-LiNK</h1>
-      <LoginButton />
+      <section className={styles.hero}>
+        <h1 className={styles.logo}>
+          <Link href="/">OWL-LiNK</Link>
+        </h1>
+        <LoginButton />
+      </section>
+
+      <button className={styles.paypal} role="button">
+        <PaypalMark />
+        <span>개발자 응원하기</span>
+      </button>
     </main>
   );
 }
