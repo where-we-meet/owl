50a81819a03dc926976971b5dd0ebc4ebc3f0f9f
SohyangO <shyang903@naver.com>
2024-04-05 16:43:03 +0900

    fix: 중복 선택 코드 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { createClient } from '@/utils/supabase/server';
-import { redirect } from 'next/navigation';
 
 export default async function Home() {
   const supabase = await createClient();
