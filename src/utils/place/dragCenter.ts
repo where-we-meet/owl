48f6053e8cc32cba3d9aaaaf332b802abc3f0c1e
heechul94 <yhcu3517@gmail.com>
2024-04-05 21:02:37 +0900

    remove: 필요 없어서 삭제

--- src/utils/place/dragCenter.ts
+++
@@ -1,7 +0,0 @@
-import type { CenterData } from '@/types/place.types';
-import type { SetStateAction, Dispatch } from 'react';
-
-export const dragCenter = (map: kakao.maps.Map, setStater: Dispatch<SetStateAction<CenterData>>) => {
-  const latlng = map.getCenter();
-  setStater((prev) => ({ ...prev, center: { lat: latlng.getLat(), lng: latlng.getLng() } }));
-};
