8f307d9385118c1e4c77989d4b01c8cb085adec2
lauichan <te06230623@gmail.com>
2024-04-05 22:17:11 +0900

    feat: 커스텀 훅으로 분리

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,30 +1,15 @@
 'use client';
 
-import { getRealtimeRoomData } from '@/api/supabaseCSR/supabase';
 import Meeting from '@/components/room/meeting/Meeting';
 import Sidebar from '@/components/room/sidebar/Sidebar';
-import { RoomData } from '@/components/room/sidebar/user/UserList';
-import { userDataFetch } from '@/utils/supabase/userDataFetch';
-import { useEffect, useState } from 'react';
+import { useGetRoomData } from '@/hooks/useGetRoomData';
 
 const RoomPage = ({ params }: { params: { id: string } }) => {
-  const [roomData, setRoomData] = useState<RoomData>([]);
-
-  useEffect(() => {
-    getRealtimeRoomData(params.id, setRoomData);
-    const sortedUserData = async () => {
-      // const updateLocation = await
-
-      const data = await userDataFetch(params.id);
-
-      setRoomData(data);
-    };
-    sortedUserData();
-  }, [params]);
+  const { roomUsers } = useGetRoomData(params.id);
 
   return (
     <main style={{ display: 'flex' }}>
-      <Sidebar roomData={roomData} />
+      <Sidebar roomUsers={roomUsers} />
       <Meeting id={params.id} />
     </main>
   );
