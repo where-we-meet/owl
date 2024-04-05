5d43dc70132489d9fab9374138c3cbeb91176dd1
lauichan <te06230623@gmail.com>
2024-04-05 14:35:20 +0900

    feat: 로그인 버튼에 소셜 로고 심볼 추가

--- src/components/auth/LoginButton.tsx
+++ src/components/auth/LoginButton.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import { createClient } from '@/utils/supabase/client';
+import KakaoSymbol from './KakaoSymbol';
+import GoogleLogo from './GoogleLogo';
+import styles from './LoginButton.module.css';
 
 const LoginButton = () => {
   const supabase = createClient();
@@ -24,12 +27,14 @@ const LoginButton = () => {
   };
 
   return (
-    <form>
-      <button onClick={signInWithGoogle} role="button">
-        Continue with Google
+    <form className={styles.form}>
+      <button className={styles.kakao} onClick={signInWithKakao} role="button">
+        <KakaoSymbol />
+        <span>카카오 로그인</span>
       </button>
-      <button onClick={signInWithKakao} role="button">
-        Continue with Kakao
+      <button className={styles.google} onClick={signInWithGoogle} role="button">
+        <GoogleLogo />
+        <span>구글 로그인</span>
       </button>
     </form>
   );
