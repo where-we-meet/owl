90a334975e7e5a95f6d9808a83d1147d3c5dc3c8
heechul94 <yhcu3517@gmail.com>
2024-04-08 01:40:26 +0900

    fix: RoomUser 타입 위도 경도 수정

--- src/types/roomUser.ts
+++ src/types/roomUser.ts
@@ -4,6 +4,8 @@ export type RoomUser = {
   is_admin: boolean;
   room_id: string;
   start_location: string | null;
+  lat: string | null;
+  lng: string | null;
   user_id: string;
   users: {
     profile_url: string | null;
