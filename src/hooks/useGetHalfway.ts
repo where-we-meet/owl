c5f3f6270f2bfb50181e06910c670dd30ccd7477
heechul94 <yhcu3517@gmail.com>
2024-04-08 05:10:29 +0900

    feat: 중간 지점 구하는 로직 커스텀 훅으로 분리

---
+++ src/hooks/useGetHalfway.ts
@@ -0,0 +1,24 @@
+import type { RoomUser } from '@/types/roomUser';
+import calcCenterPoint from '@/utils/place/calcCenterPoint';
+import convexHull from '@/utils/place/convexHull';
+import { useEffect, useState } from 'react';
+
+export const useGetHalfway = (roomUsers: RoomUser[]) => {
+  const [halfwayPoint, setHalfwayPoint] = useState({ lat: 0, lng: 0 });
+
+  const hasLocationUsers = roomUsers
+    .filter((user) => user.start_location !== undefined)
+    .map((user) => ({ location: { lat: Number(user.lat), lng: Number(user.lng) } }));
+  const sortedMarkers = convexHull(hasLocationUsers);
+  const result = calcCenterPoint(sortedMarkers);
+
+  const numberOfStartLocation = hasLocationUsers.length;
+
+  useEffect(() => {
+    if (result && numberOfStartLocation >= 2) {
+      setHalfwayPoint(result);
+    }
+  }, [numberOfStartLocation, result]);
+
+  return { halfwayPoint };
+};
