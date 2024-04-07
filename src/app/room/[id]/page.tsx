668def864f866df4b0d52997d3b043d5a2b1ba74
lauichan <te06230623@gmail.com>
2024-04-08 02:58:24 +0900

    feat: 새로 접속한 유저 추가 기능

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -3,11 +3,28 @@
 import Meeting from '@/components/room/meeting/Meeting';
 import Sidebar from '@/components/room/sidebar/Sidebar';
 import { useGetRoomData } from '@/hooks/useGetRoomData';
+import styles from './page.module.css';
+import { useEffect } from 'react';
+import { useRoomUserDataStore } from '@/store/store';
+import { createClient } from '@/utils/supabase/client';
 
 const RoomPage = ({ params }: { params: { id: string } }) => {
-  useGetRoomData(params.id);
+  const { userId } = useGetRoomData(params.id);
+  const roomUsers = useRoomUserDataStore((state) => state.roomUsers);
+
+  useEffect(() => {
+    const joinNewUser = async () => {
+      const supabase = createClient();
+      const checkNewUser = roomUsers.find((user) => user.user_id === userId) ? true : false;
+      if (userId && !checkNewUser) {
+        await supabase.from('userdata_room').insert([{ room_id: params.id, user_id: userId, is_admin: false }]);
+      }
+    };
+    joinNewUser();
+  }, [userId]);
+
   return (
-    <main style={{ display: 'flex' }}>
+    <main className={styles.main}>
       <Sidebar />
       <Meeting id={params.id} />
     </main>
