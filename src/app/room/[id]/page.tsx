3bc5b7d5a84697bd6f4eea89c76c01057727f311
heechul94 <yhcu3517@gmail.com>
2024-04-08 01:39:45 +0900

    fix: roomUsers 상태 전역 관리로 변경

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -5,11 +5,10 @@ import Sidebar from '@/components/room/sidebar/Sidebar';
 import { useGetRoomData } from '@/hooks/useGetRoomData';
 
 const RoomPage = ({ params }: { params: { id: string } }) => {
-  const { roomUsers } = useGetRoomData(params.id);
-
+  useGetRoomData(params.id);
   return (
     <main style={{ display: 'flex' }}>
-      <Sidebar roomUsers={roomUsers} />
+      <Sidebar />
       <Meeting id={params.id} />
     </main>
   );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,20 +1,24 @@
+import type { RoomUser } from '@/types/roomUser';
 import { create } from 'zustand';
 
-type State = {
+type Location = {
   location: {
     lat: number;
     lng: number;
   };
   address: string;
   addressName?: string;
+  setLocation: (location: Location['location']) => void;
+  setAddress: (address: Location['address']) => void;
 };
 
-type Action = {
-  setLocation: (location: State['location']) => void;
-  setAddress: (address: State['address']) => void;
+type RoomUserData = {
+  roomUsers: RoomUser[];
+  setRoomUsers: (roomUsers: RoomUser[]) => void;
+  updateRoomUserData: (payload: RoomUser) => void;
 };
 
-export const useSearchDataStore = create<State & Action>((set) => ({
+export const useSearchDataStore = create<Location>((set) => ({
   location: {
     lat: 33.450701,
     lng: 126.570667
@@ -23,4 +27,17 @@ export const useSearchDataStore = create<State & Action>((set) => ({
   addressName: '',
   setLocation: (location) => set({ location }),
   setAddress: (address) => set({ address })
+}));
+
+export const useRoomUserDataStore = create<RoomUserData>((set) => ({
+  roomUsers: [],
+  setRoomUsers: (roomUsers) => set({ roomUsers }),
+  updateRoomUserData: (payload) =>
+    set((state) => ({
+      roomUsers: state.roomUsers.map((user) =>
+        user.id === payload.id
+          ? { ...user, start_location: payload.start_location, lat: payload.lat, lng: payload.lng }
+          : user
+      )
+    }))
 }));
