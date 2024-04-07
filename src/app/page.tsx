3d80941784cd97b6b4ab01d756de42f1d3ae6c1e
lauichan <te06230623@gmail.com>
2024-04-07 20:31:34 +0900

    feat: 모임 시작 버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+import StartMeeting from '@/components/room/StartMeeting';
 import { createClient } from '@/utils/supabase/server';
 
 export default async function Home() {
   const supabase = await createClient();
 
   const {
-    data: { user },
-    error
+    data: { user }
   } = await supabase.auth.getUser();
 
-  return <p>Hello {user?.email}</p>;
+  return (
+    <>
+      <p>Hello {user?.email}</p>
+      <StartMeeting user={user} />
+    </>
+  );
 }
