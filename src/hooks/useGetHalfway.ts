ead90e517d1ae989a985a9b0b41537918f50a88e
heechul94 <yhcu3517@gmail.com>
2024-04-08 12:10:36 +0900

    fix: useEffect 의존성 배열 수정 result -> result.lat, result.lng

--- src/hooks/useGetHalfway.ts
+++ src/hooks/useGetHalfway.ts
@@ -1,3 +1,4 @@
+import { useRoomUserDataStore } from '@/store/store';
 import type { RoomUser } from '@/types/roomUser';
 import calcCenterPoint from '@/utils/place/calcCenterPoint';
 import convexHull from '@/utils/place/convexHull';
@@ -7,18 +8,17 @@ export const useGetHalfway = (roomUsers: RoomUser[]) => {
   const [halfwayPoint, setHalfwayPoint] = useState({ lat: 0, lng: 0 });
 
   const hasLocationUsers = roomUsers
-    .filter((user) => user.start_location !== undefined)
+    .filter((user) => !!user.start_location)
     .map((user) => ({ location: { lat: Number(user.lat), lng: Number(user.lng) } }));
   const sortedMarkers = convexHull(hasLocationUsers);
   const result = calcCenterPoint(sortedMarkers);
-
   const numberOfStartLocation = hasLocationUsers.length;
 
   useEffect(() => {
     if (result && numberOfStartLocation >= 2) {
       setHalfwayPoint(result);
     }
-  }, [numberOfStartLocation, result]);
+  }, [result?.lat, result?.lng]);
 
   return { halfwayPoint };
 };
