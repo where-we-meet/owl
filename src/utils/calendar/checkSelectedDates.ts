2da4c8ce63a8286a81546592c10ad5e7ceab702e
SohyangO <shyang903@naver.com>
2024-04-08 22:08:14 +0900

    fix: 오타수정

--- src/utils/calendar/checkSelectedDates.ts
+++ src/utils/calendar/checkSelectedDates.ts
@@ -1,5 +1,5 @@
-const checkSelectedDates = (selectedDate: Date[]) => {
-  return selectedDate.length > 0; // 선택된 날짜가 있는지 여부를 확인하여 반환합니다.
+const checkSelectedDates = (selectedDates: Date[]) => {
+  return selectedDates.length > 0; // 선택된 날짜가 있는지 여부를 확인하여 반환합니다.
 };
 
 export default checkSelectedDates;
