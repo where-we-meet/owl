a7508aeeb5bb7ed713284ba81804f38241a0e001
SohyangO <shyang903@naver.com>
2024-04-08 12:48:33 +0900

    refactor: 월을 계산하는 로직 분리

---
+++ src/components/room/meeting/calender/CalculateOfMonth.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { startOfMonth, endOfMonth, startOfWeek, endOfWeek, addDays } from 'date-fns';
+
+const CalculateOfMonth = (nowDate: Date) => {
+  const monthStart = startOfMonth(nowDate);
+  const monthEnd = endOfMonth(monthStart);
+  const startDay = startOfWeek(monthStart);
+  const endDay = endOfWeek(monthEnd);
+  const entireOfMonth = [];
+
+  let startWeek = startDay;
+  let entireOfWeek = [];
+
+  while (startWeek <= endDay) {
+    for (let i = 0; i < 7; i++) {
+      entireOfWeek.push(startWeek);
+      startWeek = addDays(startWeek, 1);
+    }
+    entireOfMonth.push(entireOfWeek);
+    entireOfWeek = [];
+  }
+  return entireOfMonth;
+};
+
+export default CalculateOfMonth;
