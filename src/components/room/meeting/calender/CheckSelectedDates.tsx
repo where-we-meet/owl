82ebef866a29a96823d0325d193986f4c05b4c28
gidalim <gidalimforsomeday@gmail.com>
2024-04-08 20:21:19 +0900

    rename: 오타수정

--- src/components/room/meeting/calender/CheckSelectedDates.tsx
+++ src/components/room/meeting/calender/CheckSelectedDates.tsx
@@ -1,5 +1,5 @@
-const CheckSelectedDates = (selectedDate: Date[]) => {
+const checkSelectedDates = (selectedDate: Date[]) => {
   return selectedDate.length > 0; // 선택된 날짜가 있는지 여부를 확인하여 반환합니다.
 };
 
-export default CheckSelectedDates;
+export default checkSelectedDates;
