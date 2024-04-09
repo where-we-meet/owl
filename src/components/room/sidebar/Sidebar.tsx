191f330218d5625fe8a03ba2fb72484a873f8d88
lauichan <te06230623@gmail.com>
2024-04-09 16:13:30 +0900

    feat: 모임 페이지 사이드바에 공유 컴포넌트 추가

--- src/components/room/sidebar/Sidebar.tsx
+++ src/components/room/sidebar/Sidebar.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 
 import style from './Sidebar.module.css';
 import UserList from './user/UserList';
+import LinkShare from '../share/LinkShare';
 
 const Sidebar = () => {
   return (
     <div className={style.container}>
       <section>
         <h2>방 제목</h2>
-        <button>수정하기</button>
-        <div>
-          <button>공유하기</button>
-        </div>
+        <LinkShare />
       </section>
       <UserList />
     </div>
