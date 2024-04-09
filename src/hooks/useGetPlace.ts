15ecb7c26ca96456a1dffabfec085c0ef76e16f1
heechul94 <yhcu3517@gmail.com>
2024-04-10 01:52:07 +0900

    feat: 카테고리 검색 api react-query로 상태관리

--- src/hooks/useGetPlace.ts
+++ src/hooks/useGetPlace.ts
@@ -1,4 +1,5 @@
 import { getAddress, getSearchPlace } from '@/api/place/placeData';
+import type { Place, SearchOptionData } from '@/types/place.types';
 import { useQuery } from '@tanstack/react-query';
 
 export const useGetRoadAddress = (location: { lat: number; lng: number }, isDrag: boolean) => {
@@ -13,9 +14,17 @@ export const useGetRoadAddress = (location: { lat: number; lng: number }, isDrag
 export const useGetSearchPlace = (searchKeyword: string) => {
   const { data, isPending } = useQuery({
     queryKey: ['room', searchKeyword],
-    queryFn: async () => await getSearchPlace(searchKeyword),
-    select: (data) => data.documents,
+    queryFn: async () => await getSearchPlace(searchKeyword, null),
     enabled: !!searchKeyword
+  });
+  return { data, isPending };
+};
+
+export const useGetSearchCategory = (searchOption: SearchOptionData) => {
+  const { data, isPending } = useQuery<Place[]>({
+    queryKey: ['room', searchOption],
+    queryFn: async () => await getSearchPlace('', searchOption),
+    enabled: !!searchOption
   });
   return { data, isPending };
 };
