a3ad3e4a8c7932ef2f610e80c7f2817e46a1cc56
heechul94 <yhcu3517@gmail.com>
2024-04-10 01:50:13 +0900

    feat: halfway store 추가, searchOption 전역 상태 추가

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,29 @@
+import { SearchOptionData } from '@/types/place.types';
 import type { RoomUser } from '@/types/roomUser';
 import { create } from 'zustand';
 
-type Location = {
+type SearchData = {
   location: {
     lat: number;
     lng: number;
   };
   address: string;
   addressName?: string;
-  setLocation: (location: Location['location']) => void;
-  setAddress: (address: Location['address']) => void;
+  searchOption: SearchOptionData;
+  setLocation: (location: SearchData['location']) => void;
+  setAddress: (address: SearchData['address']) => void;
+  setSearchOption: (payload: SearchOptionData) => void;
+};
+
+type HalfwayData = {
+  halfwayData: {
+    lat: number | null;
+    lng: number | null;
+    location: string | null;
+    range: number | null;
+    verified: false;
+  };
+  updateHalfwayData: (payload: { [key: string]: string | number | null }) => void;
 };
 
 type RoomUserData = {
@@ -19,15 +33,17 @@ type RoomUserData = {
   updateRoomUserData: (payload: RoomUser) => void;
 };
 
-export const useSearchDataStore = create<Location>((set) => ({
+export const useSearchDataStore = create<SearchData>((set) => ({
   location: {
     lat: 33.450701,
     lng: 126.570667
   },
   address: '',
   addressName: '',
+  searchOption: null,
   setLocation: (location) => set({ location }),
-  setAddress: (address) => set({ address })
+  setAddress: (address) => set({ address }),
+  setSearchOption: (payload: SearchOptionData) => set({ searchOption: payload })
 }));
 
 export const useRoomUserDataStore = create<RoomUserData>((set) => ({
@@ -45,4 +61,15 @@ export const useRoomUserDataStore = create<RoomUserData>((set) => ({
           : user
       )
     }))
+}));
+
+export const useHalfwayDataStore = create<HalfwayData>((set) => ({
+  halfwayData: {
+    lat: null,
+    lng: null,
+    location: null,
+    range: null,
+    verified: false
+  },
+  updateHalfwayData: (payload) => set((state) => ({ halfwayData: { ...state.halfwayData, ...payload } }))
 }));
