aacbe8ffd2d101a9e9abc4e339371d2cb13fea5f
lauichan <te06230623@gmail.com>
2024-04-11 21:10:28 +0900

    feat: 새로운 룹 생성 시작일자 종료일자 추가

--- src/api/room.ts
+++ src/api/room.ts
@@ -2,14 +2,32 @@ import { createClient } from '@/utils/supabase/client';
 
 const supabase = createClient();
 
-export const insertNewRoom = async (name: string, created_by: string) => {
-  const { data, error } = await supabase.from('rooms').insert([{ name, created_by }]).select();
+export const insertNewRoom = async ({
+  name,
+  created_by,
+  start_date,
+  end_date
+}: {
+  name: string;
+  created_by: string;
+  start_date: string;
+  end_date: string;
+}) => {
+  const { data, error } = await supabase.from('rooms').insert([{ name, created_by, start_date, end_date }]).select();
   if (error) throw error;
   return data;
 };
 
-export const insertRoomUser = async (room_id: string, user_id: string, is_admin: boolean) => {
-  const { data, error } = await supabase.from('userdata_room').insert([{ room_id, user_id, is_admin: true }]);
+export const insertRoomUser = async ({
+  room_id,
+  user_id,
+  is_admin
+}: {
+  room_id: string;
+  user_id: string;
+  is_admin: boolean;
+}) => {
+  const { data, error } = await supabase.from('userdata_room').insert([{ room_id, user_id, is_admin }]);
   if (error) throw error;
   return data;
 };
