8bef68c1fa14696209639d82cb0c10f1302da27d
gidalim <gidalimforsomeday@gmail.com>
2024-04-11 21:04:57 +0900

    rename: Meeting 컴포넌트를 분리

--- src/app/my-owl/page.tsx
+++ src/app/my-owl/page.tsx
@@ -1,11 +1,9 @@
-import { Meeting } from '@/components/my-owl/meeting/Meeting';
 import Profile from '@/components/my-owl/profile/Profile';
 
 export default function MyOwl() {
   return (
     <main style={{ display: 'flex' }}>
       <Profile />
-      <Meeting />
     </main>
   );
 }
