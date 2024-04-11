49c30b9f102fa075b6410c25e01883afed54e008
SohyangO <shyang903@naver.com>
2024-04-12 00:20:45 +0900

    fix: room id를 가져와서 Link 클릭 시 이동할 수 있게 함

--- src/app/room/[id]/(setting)/layout.tsx
+++ src/app/room/[id]/(setting)/layout.tsx
@@ -1,18 +1,28 @@
-import React, { useEffect } from 'react';
-import styles from '@/app/room/[id]/(setting)/layout.module.css';
+'use client';
 
-export default function layout({ children }: { children: React.ReactNode }) {
+import React from 'react';
+import styles from '@/app/room/[id]/(setting)/layout.module.css';
+import Link from 'next/link';
+import { useParams } from 'next/navigation';
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  const { id } = useParams();
+
   return (
     <>
       <div className={styles.room_container}>
         <h1 className={styles.room_name}>호기심 많은 올빼미</h1>
         <ol className={styles.step_container}>
           <li className={styles.step_item}>
-            <h2 className={styles.step_title}>1</h2>
+            <Link href={`/room/${id}/pick-calendar`} className={styles.step_title}>
+              1
+            </Link>
             <p>일정</p>
           </li>
           <li className={styles.step_item}>
-            <h2 className={styles.step_title}>2</h2>
+            <Link href={`/room/${id}/pick-place`} className={styles.step_title}>
+              2
+            </Link>
             <p>장소</p>
           </li>
           <li className={styles.step_item}>
@@ -24,4 +34,6 @@ export default function layout({ children }: { children: React.ReactNode }) {
       </div>
     </>
   );
-}
+};
+
+export default layout;
