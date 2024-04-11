80cb28452f0bbf7e650b0ff4ac397d71511f1b2a
lauichan <te06230623@gmail.com>
2024-04-11 21:11:09 +0900

    feat: 새로운 방 생성 시작일자, 종료일자 UI

--- src/components/room/StartMeeting.tsx
+++ src/components/room/StartMeeting.tsx
@@ -1,29 +1,48 @@
 'use client';
 
 import { insertNewRoom, insertRoomUser } from '@/api/room';
-import { useSession } from '@/hooks/useSession';
 import { useRouter } from 'next/navigation';
+import type { User } from '@supabase/supabase-js';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
-const StartMeeting = () => {
+const StartMeeting = ({ user }: { user: User | null }) => {
   const router = useRouter();
-  const { isLoadingUser, currentUser: user } = useSession();
+  const [startDate, setStartDate] = useState(new Date().toDateString());
 
-  const startNewRoom = async () => {
+  const changeDate = (e: ChangeEvent<HTMLInputElement>) => {
+    setStartDate(e.target.value);
+  };
+
+  const startNewRoom = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const endDate = e.currentTarget.end.value;
+
+    console.log(startDate, endDate);
+
     if (!user) {
       router.push('/login');
       return;
     }
 
-    const [room] = await insertNewRoom('운좋은 올빼미', user.id);
-    await insertRoomUser(room.id, user.id, true);
+    const [room] = await insertNewRoom({
+      name: '운좋은 올빼미',
+      created_by: user.id,
+      start_date: startDate,
+      end_date: endDate
+    });
+    await insertRoomUser({ room_id: room.id, user_id: user.id, is_admin: true });
 
     router.push(`/room/${room.id}`);
   };
 
   return (
-    <button onClick={startNewRoom} disabled={isLoadingUser}>
-      모임 시작하기
-    </button>
+    <form onSubmit={startNewRoom}>
+      <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+        <input type="date" name="start" value={startDate} onChange={changeDate} />
+        <input type="date" min={startDate} name="end" />
+      </div>
+      <button>모임 시작하기</button>
+    </form>
   );
 };
 
