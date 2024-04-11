bd03ded5826ae1a71a94a0ddadead5091398ab53
ketchup0211 <dlcka1111@naver.com>
2024-04-11 20:33:55 +0900

    feat: 임시 테스트용 모달 코드 추가

--- src/components/auth/UserProfileButton.tsx
+++ src/components/auth/UserProfileButton.tsx
@@ -1,13 +1,53 @@
-import { Button } from '@nextui-org/react';
+'use client';
+
+import { Button, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from '@nextui-org/react';
 import { PiUserSquareDuotone } from 'react-icons/pi';
 
-const UserProfile = async () => {
+const UserProfile = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleOpen = () => {
+    onOpen();
+  };
   return (
-    <div className="flex gap-4 items-center">
-      <Button isIconOnly aria-label="User Profile Setting">
-        <PiUserSquareDuotone />
-      </Button>
-    </div>
+    <>
+      <div className="flex gap-4 items-center">
+        <Button isIconOnly onPress={handleOpen} aria-label="User Profile Setting">
+          <PiUserSquareDuotone />
+        </Button>
+      </div>
+      <Modal backdrop="blur" isOpen={isOpen} onClose={onClose}>
+        <ModalContent>
+          <>
+            <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
+            <ModalBody>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non risus hendrerit
+                venenatis. Pellentesque sit amet hendrerit risus, sed porttitor quam.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non risus hendrerit
+                venenatis. Pellentesque sit amet hendrerit risus, sed porttitor quam.
+              </p>
+              <p>
+                Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor adipisicing. Mollit
+                dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit officia eiusmod Lorem aliqua enim laboris
+                do dolor eiusmod. Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur proident Lorem
+                eiusmod et. Culpa deserunt nostrud ad veniam.
+              </p>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="danger" variant="light" onPress={onClose}>
+                Close
+              </Button>
+              <Button color="primary" onPress={onClose}>
+                Action
+              </Button>
+            </ModalFooter>
+          </>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
