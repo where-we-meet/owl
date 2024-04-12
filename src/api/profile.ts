e9e8d05070b8b99a7fbcfa4064dc59a522b14665
lauichan <te06230623@gmail.com>
2024-04-12 12:33:11 +0900

    refactor: api 폴더는 데이터만 가져오도록 수정

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -1,20 +1,10 @@
 import { createClient } from '@/utils/supabase/client';
-import { User } from '@supabase/supabase-js';
 
-export const getUserProfileData = async (user: User | null | undefined) => {
+export const getUserProfileData = async (id: string) => {
   const supabase = createClient();
 
-  if (!user) return null;
-
-  const { data, error } = await supabase.from('users').select('name, profile_url').eq('id', user.id).single();
+  const { data, error } = await supabase.from('users').select('name, profile_url').eq('id', id).single();
   if (error) throw error;
 
-  return {
-    authSNS: user.app_metadata.providers,
-    userInfo: {
-      userId: user.id,
-      name: data.name,
-      profileURL: data.profile_url
-    }
-  };
+  return data;
 };
