9da458c01b99a86434872aa22625831753c07ee9
lauichan <te06230623@gmail.com>
2024-04-12 12:39:46 +0900

    remove: 삭제된 페이지 컴포넌트 제거

--- src/app/my-owl/page.tsx
+++
@@ -1,9 +0,0 @@
-import Profile from '@/components/my-owl/profile/Profile';
-
-export default function MyOwl() {
-  return (
-    <main style={{ display: 'flex' }}>
-      <Profile />
-    </main>
-  );
-}
