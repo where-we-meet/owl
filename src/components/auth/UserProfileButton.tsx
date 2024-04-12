f2158d98a3da4586af4745b32adf9ada46610a48
lauichan <te06230623@gmail.com>
2024-04-12 12:34:34 +0900

    refactor: 유저 프로필 로직 수정

--- src/components/auth/UserProfileButton.tsx
+++ src/components/auth/UserProfileButton.tsx
@@ -9,55 +9,53 @@ import { changeUserProfile, updateUserName } from '@/api/supabaseCSR/supabase';
 import { ImageUploadModal } from '../my-owl/profile/editable/modal/Modal';
 import { useQueryUser } from '@/hooks/useQueryUser';
 import { getUserProfileData } from '@/api/profile';
+import { useQuery } from '@tanstack/react-query';
 
 export type UserInfo = {
-  authSNS: Array<string>;
-  userInfo: {
-    userId: string;
-    name: string;
-    profileURL: string | null;
-  };
+  name: string;
+  profile_url: string | null;
 };
 
+const MAX_NAME_LENGTH = 16;
+
 const UserProfile = () => {
-  const [data, setData] = useState<UserInfo | null>(null);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [editMode, setEditMode] = useState(false);
-  const MAX_NAME_LENGTH = 16;
+  const [userName, setUserName] = useState('');
+  const [userProfileURL, setUserProfileURL] = useState<string | null>('');
+  const [toggleModal, setToggleModal] = useState(false);
+
   const { data: user } = useQueryUser();
+  const { data, isLoading } = useQuery({
+    queryKey: ['profile'],
+    queryFn: () => getUserProfileData(user.id)
+  });
 
   useEffect(() => {
-    const fetchData = async () => {
-      const data = await getUserProfileData(user);
-      setData(data);
-    };
-    fetchData();
-  }, []);
+    if (data) {
+      setUserName(data.name);
+      setUserProfileURL(data.profile_url);
+    }
+  }, [data]);
 
-  if (!data) return null;
-
-  const { authSNS, userInfo } = data;
-
-  const [userName, setUserName] = useState(userInfo.name);
-  const [userProfileURL, setUserProfileURL] = useState(userInfo.profileURL);
-  const [toggleModal, setToggleModal] = useState(false);
   const handleToggleModal = () => {
     setToggleModal((prev) => !prev);
   };
+
   const handleChangeUserName = (event: ChangeEvent<HTMLInputElement>) => {
     const name = event.target.value;
     setUserName(name);
   };
 
-  const handleEditDone = () => {
+  const handleEditDone = async () => {
     if (userName.length < 1) {
       alert('닉네임은 최소 1글자 이상으로 지어주세요.');
     } else if (userName.length > 16) {
       alert('닉네임은 최대 16글자 이하로 지어주세요.');
     } else {
-      updateUserName(userInfo.userId, userName);
+      await updateUserName(user.id, userName);
       if (userProfileURL !== null) {
-        changeUserProfile({ userId: userInfo.userId, profile_url: userProfileURL });
+        await changeUserProfile({ userId: user.id, profile_url: userProfileURL });
       }
       setEditMode(false);
     }
@@ -68,20 +66,12 @@ const UserProfile = () => {
   };
 
   const handleClose = () => {
-    setUserName(userInfo.name);
-    setUserProfileURL(userInfo.profileURL);
     setEditMode(false);
     onClose();
   };
 
   const handleEditMode = () => {
     setEditMode((prev) => !prev);
-  };
-
-  const handleEditExit = () => {
-    setUserName(userInfo.name);
-    setUserProfileURL(userInfo.profileURL);
-    setEditMode(false);
   };
 
   return (
@@ -91,74 +81,75 @@ const UserProfile = () => {
           <PiUserSquareDuotone />
         </Button>
       </div>
-      <Modal backdrop="blur" isOpen={isOpen} onClose={handleClose} hideCloseButton>
-        <ModalContent>
-          {editMode ? (
-            <>
-              <Button isIconOnly onPress={handleEditExit}>
-                <IoChevronBack />
-              </Button>
-              <ModalHeader className="flex flex-col gap-1">프로필 수정</ModalHeader>
-              <ModalBody>
-                <div>
-                  <Image width={100} alt="profile_image" src={`${userProfileURL}`} />
-                  <Button isIconOnly onPress={handleToggleModal}>
-                    <Image src="/images/edit_mode.svg" alt="edit mode" width={24} height={21} />
-                  </Button>
-                </div>
-                <div>
+      {data && (
+        <Modal backdrop="blur" isOpen={isOpen} onClose={handleClose} hideCloseButton>
+          <ModalContent>
+            {editMode ? (
+              <>
+                <Button isIconOnly onPress={handleEditMode}>
+                  <IoChevronBack />
+                </Button>
+                <ModalHeader className="flex flex-col gap-1">프로필 수정</ModalHeader>
+                <ModalBody>
                   <div>
-                    <span>
-                      <p>닉네임을 입력해주세요</p>
-                      <p>
-                        {userName.length}/{MAX_NAME_LENGTH}
-                      </p>
-                    </span>
-                    <input type="text" onChange={handleChangeUserName} value={userName} />
-                  </div>
-                </div>
-                <div>
-                  <Button onPress={handleEditDone}>저장</Button>
-                </div>
-                {toggleModal && (
-                  <ImageUploadModal handleToggleModal={handleToggleModal} setUserProfileURL={setUserProfileURL} />
-                )}
-              </ModalBody>
-            </>
-          ) : (
-            <>
-              <Button isIconOnly onPress={handleClose}>
-                <IoClose />
-              </Button>
-              <ModalHeader className="flex flex-col gap-1">계정 정보</ModalHeader>
-              <ModalBody>
-                <Image width={100} alt="profile_image" src={`${userProfileURL}`} />
-                <div>
-                  <p>닉네임</p>
-                  <div>
-                    <p>{userName}</p>
-                    <Button onPress={handleEditMode}>
-                      <GrCaretNext />
+                    <Image width={100} alt="profile_image" src={`${userProfileURL}`} />
+                    <Button isIconOnly onPress={handleToggleModal}>
+                      <Image src="/images/edit_mode.svg" alt="edit mode" width={24} height={21} />
                     </Button>
                   </div>
-                </div>
-                <div>
-                  <p>로그인정보</p>
                   <div>
-                    <p>{authSNS} 로그인</p>
-                    {authSNS.map((SNS, index) => (
-                      <img key={index} src={`/images/${SNS}.svg`} alt={SNS} width={34} height={31} />
-                    ))}
+                    <div>
+                      <span>
+                        <p>닉네임을 입력해주세요</p>
+                        <p>
+                          {userName.length}/{MAX_NAME_LENGTH}
+                        </p>
+                      </span>
+                      <input type="text" onChange={handleChangeUserName} value={userName} />
+                    </div>
                   </div>
-                </div>
-                <div>
-                  <Button>로그아웃</Button>
-                </div>
-              </ModalBody>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+                  <div>
+                    <Button onPress={handleEditDone}>저장</Button>
+                  </div>
+                  {toggleModal && (
+                    <ImageUploadModal handleToggleModal={handleToggleModal} setUserProfileURL={setUserProfileURL} />
+                  )}
+                </ModalBody>
+              </>
+            ) : (
+              <>
+                <Button isIconOnly onPress={handleClose}>
+                  <IoClose />
+                </Button>
+                <ModalHeader className="flex flex-col gap-1">계정 정보</ModalHeader>
+                <ModalBody>
+                  <Image width={100} alt="profile_image" src={`${userProfileURL}`} />
+                  <div>
+                    <p>닉네임</p>
+                    <div>
+                      <p>{data.name}</p>
+                      <Button onPress={handleEditMode}>
+                        <GrCaretNext />
+                      </Button>
+                    </div>
+                  </div>
+                  <div>
+                    <p>로그인정보</p>
+                    <div>
+                      {user.app_metadata.providers.map((SNS: string, index: number) => (
+                        <img key={index} src={`/images/${SNS}.svg`} alt={SNS} width={34} height={31} />
+                      ))}
+                    </div>
+                  </div>
+                  <div>
+                    <Button>로그아웃</Button>
+                  </div>
+                </ModalBody>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
+      )}
     </>
   );
 };
