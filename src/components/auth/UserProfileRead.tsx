9f8ce9c0d72c41db24bfec111c47f9aa7d2ceafd
ketchup0211 <dlcka1111@naver.com>
2024-04-12 20:44:31 +0900

    feat: 프로필 수정 및 취소 기능

--- src/components/auth/UserProfileRead.tsx
+++ src/components/auth/UserProfileRead.tsx
@@ -8,23 +8,35 @@ import { useQueryUser } from '@/hooks/useQueryUser';
 import { getUserProfileData } from '@/api/profile';
 import { useQuery } from '@tanstack/react-query';
 
+export type UserProfileData = { name: string; profile_url: string | null };
+
 const UserProfileRead = ({ toggleEditMode }: { toggleEditMode: () => void }) => {
   const { onClose } = useDisclosure();
-  const [userName, setUserName] = useState('');
   const [userProfileURL, setUserProfileURL] = useState<string | null>('');
 
   const { data: user } = useQueryUser();
-  const { data, isLoading } = useQuery({
-    queryKey: ['profile'],
-    queryFn: () => getUserProfileData(user.id)
+
+  /**
+   * 실험실
+   */
+  //state for recent profile data (from supabase DB)
+  const [data, setData] = useState<UserProfileData>({
+    name: '',
+    profile_url: ''
   });
 
+  // useEffect for take recent data
   useEffect(() => {
-    if (data) {
-      setUserName(data.name);
-      setUserProfileURL(data.profile_url);
-    }
-  }, [data]);
+    const fetchData = async () => {
+      try {
+        const data = await getUserProfileData(user.id);
+        setData({ name: data.name, profile_url: data.profile_url });
+      } catch (error) {
+        console.error('Error fetching user profile data:', error);
+      }
+    };
+    fetchData();
+  });
 
   const handleClose = () => {
     toggleEditMode();
@@ -38,11 +50,11 @@ const UserProfileRead = ({ toggleEditMode }: { toggleEditMode: () => void }) => 
       </Button>
       <ModalHeader className="flex flex-col gap-1">계정 정보</ModalHeader>
       <ModalBody>
-        <Image width={100} alt="profile_image" src={`${userProfileURL}`} />
+        <Image width={100} alt="profile_image" src={`${data.profile_url}`} />
         <div>
           <p>닉네임</p>
           <div>
-            <p>{data?.name}</p>
+            <p>{data.name}</p>
             <Button onPress={toggleEditMode}>
               <GrCaretNext />
             </Button>
