ef5f3936fdd76639d95643fdf5da85d155dd3819
gidalim <gidalimforsomeday@gmail.com>
2024-04-12 23:47:26 +0900

    style: 새로운 모임 만들기 위치조정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <>
-      <Link href="/start">모임 시작하기</Link>
-    </>
+    <main className={styles.main}>
+      <section className={styles.section}>
+        <Link className={styles.start} href="/start">
+          모임 시작하기
+        </Link>
+      </section>
+    </main>
   );
 }
