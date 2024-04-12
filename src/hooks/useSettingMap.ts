dcb30f331ada5ead9e7d9408ada868f9c1b441c9
heechul94 <yhcu3517@gmail.com>
2024-04-13 00:47:39 +0900

    fix: 기존 저장된 위치가 있으면 지도에 유지하기 기능

--- src/hooks/useSettingMap.ts
+++ src/hooks/useSettingMap.ts
@@ -1,15 +1,17 @@
-import { useGpsStatusStore, useSearchDataStore } from '@/store/placeStore';
-import { myGeolocation } from '@/utils/place/myGeolocation';
+import { useGpsStatusStore, useRoomUserDataStore, useSearchDataStore } from '@/store/placeStore';
 import _ from 'lodash';
 import { useCallback, useEffect, useState } from 'react';
 import { useGetRoadAddress } from './useGetPlace';
+import { useQueryUser } from './useQueryUser';
 
 export const useSettingMap = () => {
   const [isDrag, setIsDrag] = useState(false);
 
   const { location, address, setLocation, setAddress } = useSearchDataStore((state) => state);
-  const isGpsLoading = useGpsStatusStore((state) => state.isGpsLoading);
+  const { isGpsLoading, setIsGpsLoading } = useGpsStatusStore((state) => state);
+  const roomUsers = useRoomUserDataStore((state) => state.roomUsers);
 
+  const { id: userId } = useQueryUser();
   const { data: searchAddress, isPending: isAddressPending } = useGetRoadAddress(location, isDrag);
 
   const setCenter = (map: kakao.maps.Map) => {
@@ -18,6 +20,16 @@ export const useSettingMap = () => {
   };
 
   const handleChangeCenter = useCallback(_.debounce(setCenter, 1000), []);
+
+  const roomUser = roomUsers.find((user) => user.user_id === userId);
+
+  useEffect(() => {
+    const location = { lat: Number(roomUser?.lat), lng: Number(roomUser?.lng) };
+    if (location.lat && location.lng) {
+      setLocation(location);
+      setIsGpsLoading(false);
+    }
+  }, [roomUser]);
 
   useEffect(() => {
     if (searchAddress) {
