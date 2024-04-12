aa717daa8f82aab173b44cbd802833e51cb1af78
SohyangO <shyang903@naver.com>
2024-04-12 17:17:03 +0900

    feat: 일정 설정 페이지에서는 첫번째 단계인 일정탭만 원을 채움

--- src/app/room/[id]/(setting)/layout.tsx
+++ src/app/room/[id]/(setting)/layout.tsx
@@ -1,70 +1,34 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from '@/app/room/[id]/(setting)/layout.module.css';
 import Link from 'next/link';
 import { useParams, usePathname } from 'next/navigation';
+import FirstStep from './FirstStep';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
   const { id } = useParams();
-  console.log(pathname);
+
+  const [currentStep, setCurrentStep] = useState<Number>(1);
 
   useEffect(() => {
-    const currentPath = pathname;
-    const currentStep = getCurrentStep(currentPath, id);
-    updateStepColor(currentStep);
+    if (pathname === `/room/${id}/pick-calendar`) {
+      setCurrentStep(1);
+    } else if (pathname === `/room/${id}/pick-place`) {
+      setCurrentStep(2);
+    } else {
+      setCurrentStep(3);
+    }
   }, [pathname, id]);
 
-  const getCurrentStep = (path: string, id: string | string[]) => {
-    if (path === `/room/${id}/pick-calendar`) {
-      return 1;
-    } else if (path === `room/${id}/pick-place`) {
-      return 2;
-    } else {
-      return 3;
-    }
-  };
-
-  const updateStepColor = (currentStep: number) => {
-    const stepItems = document.querySelectorAll(`.${styles.step_item}::before`);
-    stepItems.forEach((item, index) => {
-      if (index + 1 === currentStep) {
-        item.classList.add('active-step');
-      } else {
-        item.classList.remove('active-step');
-      }
-    });
-    const currentStepItem = document.querySelector(`.${styles.step_item}:nth-child(${currentStep})::before`);
-    if (currentStepItem) {
-      currentStepItem.classList.add('active-step');
-    }
-  };
+  console.log(currentStep);
 
   return (
     <>
       <div className={styles.room_container}>
         <h1 className={styles.room_name}>호기심 많은 올빼미</h1>
-        <ol className={styles.step_container}>
-          <li className={styles.step_item}>
-            <Link href={`/room/${id}/pick-calendar`} className={styles.step_title}>
-              <span className={styles.step_item_span}>1</span>
-            </Link>
-            <p>일정</p>
-          </li>
-          <li className={styles.step_item}>
-            <Link href={`/room/${id}/pick-place`} className={styles.step_title}>
-              <span className={styles.step_item_span}>2</span>
-            </Link>
-            <p>장소</p>
-          </li>
-          <li className={styles.step_item}>
-            <h3 className={styles.step_title}>
-              <span className={styles.step_item_span}>3</span>
-            </h3>
-            <p>확정</p>
-          </li>
-        </ol>
+        {currentStep === 1 && <FirstStep id={id} />}
         <div className={styles.calendar_container}>{children}</div>
       </div>
     </>
