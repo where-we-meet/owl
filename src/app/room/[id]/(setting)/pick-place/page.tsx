7237ca72744589a2d51240ca5b6eb0af4c990f46
heechul94 <yhcu3517@gmail.com>
2024-04-12 11:29:06 +0900

    feat: 위치 설정 페이지 생성

--- src/app/room/[id]/(setting)/pick-place/page.tsx
+++ src/app/room/[id]/(setting)/pick-place/page.tsx
@@ -1,5 +1,27 @@
+'use client';
+
+import LocationPicker from '@/components/room/meeting/place/LocationPicker';
+import SettingMap from '@/components/room/meeting/place/SettingMap';
+import SearchBar from '@/components/room/meeting/place/search/SearchBar';
+import { Button } from '@nextui-org/react';
+import Link from 'next/link';
+import { useParams } from 'next/navigation';
 import React from 'react';
 
-export default function Place() {
-  return <div>Place</div>;
-}
+const PickPlace = () => {
+  const { id: roomId } = useParams();
+  return (
+    <>
+      <SearchBar />
+      <SettingMap />
+      <div>
+        <Button>
+          <Link href={`/room/${roomId}/pick-calendar`}>이전</Link>
+        </Button>
+        <LocationPicker />
+      </div>
+    </>
+  );
+};
+
+export default PickPlace;
