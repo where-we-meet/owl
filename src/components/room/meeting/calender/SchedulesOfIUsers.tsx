43048c6531709473e4adad9d7bf8ca6ab42438a0
SohyangO <shyang903@naver.com>
2024-04-12 17:39:57 +0900

    chore: 캘린더에서 불필요한 코드 삭제

--- src/components/room/meeting/calender/SchedulesOfIUsers.tsx
+++ src/components/room/meeting/calender/SchedulesOfIUsers.tsx
@@ -12,14 +12,9 @@ const SchedulesOfUsers: React.FC<Props> = ({ userSchedules, day }) => {
   return (
     <>
       {userSchedules.map((schedule, index) => {
-        const styleOfCircles: React.CSSProperties = {
-          position: 'absolute',
-          backgroundColor: `hsl(140, 50, ${0.5 + index * 0.08}%)`
-        };
-
         return (
           isSameDay(new Date(String(schedule.start_date)), day) && (
-            <span key={index} className={styles.selected_date_circle} style={{ ...styleOfCircles }}></span>
+            <span key={index} className={styles.selected_date_circle_of_users}></span>
           )
         );
       })}
