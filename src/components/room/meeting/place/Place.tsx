af2bd86e2c1b4d1f5a18d5cffff6c4d980e26139
heechul94 <yhcu3517@gmail.com>
2024-04-13 19:51:41 +0900

    rename: KakaoMap -> ResultMap 이름 수정 및 참조 파일 적용

--- src/components/room/meeting/place/Place.tsx
+++ src/components/room/meeting/place/Place.tsx
@@ -1,16 +1,12 @@
 'use client';
 
-import KakaoMap from './KakaoMap';
-import LocationPicker from './LocationPicker';
 import CategorySelector from './search/CategorySelector';
-import SearchBar from './search/SearchBar';
+import ResultMap from './ResultMap';
 
 const Place = () => {
   return (
     <div>
-      <SearchBar />
-      <KakaoMap />
-      <LocationPicker />
+      <ResultMap />
       <CategorySelector />
     </div>
   );
