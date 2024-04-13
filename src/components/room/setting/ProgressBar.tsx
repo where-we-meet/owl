2dfd156acf6a18d67e06f9a385e00fb117b4807f
heechul94 <yhcu3517@gmail.com>
2024-04-13 22:05:46 +0900

    style: 설정 단계 막대 UI 생성

---
+++ src/components/room/setting/ProgressBar.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import styles from './ProgressBar.module.css';
+import { useParams, usePathname, useRouter } from 'next/navigation';
+import { nowStepInfo } from '@/utils/nowStepInfo';
+import { Progress } from '@nextui-org/react';
+
+const steps = [
+  { name: '일정', path: 'pick-calendar', value: 0 },
+  { name: '장소', path: 'pick-place', value: 50 },
+  { name: '확정', path: 'confirm', value: 100 }
+];
+
+const ProgressBar = () => {
+  const { id: roomId } = useParams();
+  const path = usePathname();
+  const router = useRouter();
+
+  const { nowStepIndex } = nowStepInfo(path);
+
+  const handleStepClick = (path: string) => {
+    router.push(`/room/${roomId}/${path}`);
+  };
+
+  return (
+    <div className={styles.steps}>
+      {steps.map((step, index) => (
+        <div
+          key={step.path}
+          className={`${styles.step} ${index <= nowStepIndex && styles.active} `}
+          onClick={() => handleStepClick(step.path)}
+        >
+          {step.name}
+        </div>
+      ))}
+
+      <Progress
+        className={styles.progressWrapper}
+        color="primary"
+        aria-label="Step"
+        value={steps[nowStepIndex].value}
+      />
+    </div>
+  );
+};
+
+export default ProgressBar;
