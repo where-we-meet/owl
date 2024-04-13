9a5e57fcc1d3b906d6a0add3ee37c367611095c1
lauichan <te06230623@gmail.com>
2024-04-13 22:04:01 +0900

    style: 룸페이지 헤더 디자인

--- src/components/room/header/RoomHeader.tsx
+++ src/components/room/header/RoomHeader.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 import UserList from './user/UserList';
 import LinkShare from '../share/LinkShare';
 
+import styles from './RoomHeader.module.css';
+
 const RoomHeader = () => {
   return (
-    <div>
+    <div className={styles.room_header}>
       <h1>호기심 많은 올빼미</h1>
       <LinkShare />
       <UserList />
