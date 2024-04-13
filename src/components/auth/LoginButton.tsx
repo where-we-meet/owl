fdedf4b7c6d720ff72596af91b18680fd75e1756
lauichan <te06230623@gmail.com>
2024-04-14 02:00:36 +0900

    feat: 로그인 redirectTo url 가져오는 로직 추가

--- src/components/auth/LoginButton.tsx
+++ src/components/auth/LoginButton.tsx
@@ -5,13 +5,20 @@ import GoogleLogo from './GoogleLogo';
 import styles from './LoginButton.module.css';
 import { createClient } from '@/utils/supabase/client';
 
+const getURL = () => {
+  let url = process?.env?.NEXT_PUBLIC_SITE_URL ?? process?.env?.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3000/';
+  url = url.includes('http') ? url : `https://${url}`;
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+  return url;
+};
+
 const LoginButton = () => {
   const logInWithOAuth = async (provider: 'google' | 'kakao') => {
     const supabase = createClient();
     await supabase.auth.signInWithOAuth({
       provider,
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_PRODUCTION_URL}/auth/callback`
+        redirectTo: getURL()
       }
     });
   };
