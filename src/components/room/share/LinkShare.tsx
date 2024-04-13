4d0d00f5e0bcc6b6d6cd30ff0ff03afd3abd4eae
lauichan <te06230623@gmail.com>
2024-04-13 22:02:40 +0900

    style: 룸 페이지 공유버튼 디자인, UI 모달

--- src/components/room/share/LinkShare.tsx
+++ src/components/room/share/LinkShare.tsx
@@ -1,14 +1,18 @@
 'use client';
-import { useState } from 'react';
-import styles from './LinkShare.module.css';
 import KakaoTalkShare from './KakaoShare';
+import {
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure
+} from '@nextui-org/react';
 
 const LinkShare = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleModal = () => {
-    setIsOpen(!isOpen);
-  };
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   const handleCopy = async () => {
     try {
@@ -20,20 +24,29 @@ const LinkShare = () => {
   };
 
   return (
-    <div className={styles.share}>
-      {isOpen && (
-        <div className={styles.modal_background}>
-          <section className={styles.modal}>
-            <input type="text" defaultValue={window.origin}></input>
-            <button onClick={handleCopy}>복사</button>
-            <a href={`mailto:?subject=${window.origin}`}>메일로 공유</a>
-            <KakaoTalkShare />
-          </section>
-        </div>
-      )}
-      <button onClick={toggleModal}>Share</button>
-      <button onClick={handleCopy}>Copy</button>
-    </div>
+    <>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} backdrop="transparent">
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">공유하기</ModalHeader>
+              <ModalBody>
+                <Input type="text" defaultValue={window.origin} disabled />
+                <button onClick={handleCopy}>복사</button>
+                <div>
+                  <a href={`mailto:?subject=${window.origin}`}>메일로 공유</a>
+                  <KakaoTalkShare />
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                <Button onPress={onClose}>Action</Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      <Button onPress={onOpen}>공유하기</Button>
+    </>
   );
 };
 
