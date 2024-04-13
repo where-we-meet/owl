43127dd1a318e9f8b0d06194f8d8feb7aa3b3acb
lauichan <te06230623@gmail.com>
2024-04-14 05:29:20 +0900

    refactor: 콜백 url 가져오는 로직 분리

---
+++ src/utils/getCallbackURL.ts
@@ -0,0 +1,6 @@
+export const getCallbackURL = () => {
+  let url = process?.env?.NEXT_PUBLIC_SITE_URL ?? process?.env?.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3000/';
+  url = url.includes('http') ? url : `https://${url}`;
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+  return `${url}/auth/callback`;
+};
