46014c69be3ce4b02146f49216facea425eaa970
heechul94 <yhcu3517@gmail.com>
2024-04-13 22:06:06 +0900

    style: 설정 페이지 레이아웃 수정

--- src/app/room/[id]/(setting)/layout.tsx
+++ src/app/room/[id]/(setting)/layout.tsx
@@ -1,35 +1,12 @@
-'use client';
-
-import React, { useEffect, useState } from 'react';
 import styles from '@/app/room/[id]/(setting)/layout.module.css';
-import { useParams, usePathname } from 'next/navigation';
-import FirstStep from './FirstStep';
-import SecondtStep from './SecondStep';
+import ProgressBar from '@/components/room/setting/ProgressBar';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const pathname = usePathname();
-  const { id } = useParams();
-
-  const [currentStep, setCurrentStep] = useState<Number>(1);
-
-  useEffect(() => {
-    if (pathname === `/room/${id}/pick-calendar`) {
-      setCurrentStep(1);
-    } else if (pathname === `/room/${id}/pick-place`) {
-      setCurrentStep(2);
-    } else {
-      setCurrentStep(3);
-    }
-  }, [pathname, id]);
-
   return (
-    <>
-      <div className={styles.room_container}>
-        {currentStep === 1 && <FirstStep id={id} />}
-        {currentStep === 2 && <SecondtStep id={id} />}
-      </div>
-      <div className={styles.calendar_container}>{children}</div>
-    </>
+    <div className={styles.setting_container}>
+      <ProgressBar />
+      <div className={styles.setting_content}>{children}</div>
+    </div>
   );
 };
 
