9d9ce3f669e8b6f614cd8aae4f10e5d46e27f296
ketchup0211 <dlcka1111@naver.com>
2024-04-14 05:28:20 +0900

    style: landing page 스타일 중간저장

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,64 @@
 import styles from './page.module.css';
-import { Button, Link } from '@nextui-org/react';
+import { Image, Button, Link } from '@nextui-org/react';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <header>
-        <h1>
-          "그래서 우리, <br />
-          언제 어디서 만나는거야?"
-        </h1>
-        <p>
-          '우리 만나는 거 맞아?', '그럼 다음에 만나자' 등<br />
-          약속을 정하기 위해 고민을 거듭했던 경험 있지 않나요?
-        </p>
-        <p>바쁜 일상 속, 우리들의 소중한 시간과 만남을 위해</p>
-        <p>
-          약속 시간과 장소를 고민하는 시간을 줄여줄 수 있는 <br /> 약속 확정 서비스, owl-link입니다
-        </p>
-        <Button href="/start" as={Link} variant="flat" size="lg">
-          모임 시작하기
-        </Button>
+      <header className={styles.header}>
+        <div className={styles.image_background}>
+          <div className={styles.background_color}>
+            <div className={styles.copy_container}>
+              <h1 className={styles.head_copy}>
+                "그래서 우리,
+                <br />
+                언제 어디서 만나는거야?"
+              </h1>
+              <span>
+                <p className={styles.middle_copy}>
+                  '우리 만나는 거 맞아?', '그럼 다음에 만나자' 등<br />
+                  약속을 정하기 위해 고민을 거듭했던 경험 있지 않나요?
+                </p>
+                <p className={styles.middle_copy}>바쁜 일상 속, 우리들의 소중한 시간과 만남을 위해</p>
+                <p className={styles.middle_copy}>
+                  약속 시간과 장소를 고민하는 시간을 줄여줄 수 있는 <br /> 약속 확정 서비스, owl-link입니다
+                </p>
+              </span>
+              <Button href="/start" as={Link} variant="flat" size="lg" className={styles.button}>
+                모임 시작하기
+              </Button>
+            </div>
+          </div>
+        </div>
       </header>
-      <section>
+      <section className={styles.section}>
         <h3>이런 경우에 쓰면 좋아요!</h3>
-        <div>
-          <span>
-            <p>icon</p>
+        <div className={styles.use_cases}>
+          <span className={styles.use_case}>
+            <Image src="images/manage_schedule.png" width={50} />
             <p>
               모임 일정 관리가
               <br />
               필요할 때
             </p>
           </span>
-          <span>
-            <p>icon</p>
+          <span className={styles.use_case}>
+            <Image src="images/appointment.png" width={50} />
             <p>
               약속을 빠르게
               <br />
               정하고 싶을 때
             </p>
           </span>
-          <span>
-            <p>icon</p>
+          <span className={styles.use_case}>
+            <Image src="images/spot.png" width={50} />
             <p>
               최적의 만남 장소를
               <br />
               찾고 싶을 때
             </p>
           </span>
-          <span>
-            <p>icon</p>
+          <span className={styles.use_case}>
+            <Image src="images/my_schedule.png" width={50} />
             <p>
               모임 일정을
               <br />한 눈에 보고 싶을 때
@@ -57,7 +66,7 @@ export default function Home() {
           </span>
         </div>
       </section>
-      <footer>
+      <footer className={styles.footer}>
         <p>@github project</p>
         <p>about us</p>
       </footer>
