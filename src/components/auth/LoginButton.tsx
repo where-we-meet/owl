3f59913c0cb24a91698c61dce601dc6869422aa3
lauichan <te06230623@gmail.com>
2024-04-14 03:04:11 +0900

    fix: 로그인 콜백 배포문제로 지운거 복구

--- src/components/auth/LoginButton.tsx
+++ src/components/auth/LoginButton.tsx
@@ -9,7 +9,7 @@ const getURL = () => {
   let url = process?.env?.NEXT_PUBLIC_SITE_URL ?? process?.env?.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3000/';
   url = url.includes('http') ? url : `https://${url}`;
   url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
-  return url;
+  return `${url}/auth/callback`;
 };
 
 const LoginButton = () => {
