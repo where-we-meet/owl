407d54a201ac32f11b9f53715d68cd87beb85f10
lauichan <te06230623@gmail.com>
2024-04-13 19:30:30 +0900

    remove: 유저 아이디 조회 방법 변경

--- src/utils/my-owl/getUserId.ts
+++
@@ -1,9 +0,0 @@
-import { getCurrentUserData } from '@/api/supabaseCSR/supabase';
-
-//유저 아이디 조회 로직
-export const getUserId = async () => {
-  const { user } = await getCurrentUserData();
-  const userId = user.id;
-
-  return userId;
-};
