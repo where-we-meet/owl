b27caaa6c3a652bf39501f9b0366daed1898411f
heechul94 <yhcu3517@gmail.com>
2024-04-13 19:00:08 +0900

    fix: 컴포넌트 이름 수정, 업데이트 로직 버튼 확정 페이지 이동 기능 버튼으로 수정

--- src/app/room/[id]/(setting)/pick-place/page.tsx
+++ src/app/room/[id]/(setting)/pick-place/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 
-import LocationPicker from '@/components/room/meeting/place/LocationPicker';
-import SettingMap from '@/components/room/meeting/place/SettingMap';
-import SearchBar from '@/components/room/meeting/place/search/SearchBar';
-import { Button } from '@nextui-org/react';
 import Link from 'next/link';
 import { useParams } from 'next/navigation';
-import React from 'react';
 
-const PickPlace = () => {
+import SettingMap from '@/components/room/meeting/place/SettingMap';
+import SearchBar from '@/components/room/meeting/place/search/SearchBar';
+
+import { Button } from '@nextui-org/react';
+
+const PickPlacePage = () => {
   const { id: roomId } = useParams();
   return (
     <>
@@ -18,10 +18,12 @@ const PickPlace = () => {
         <Button>
           <Link href={`/room/${roomId}/pick-calendar`}>이전</Link>
         </Button>
-        <LocationPicker />
+        <Button>
+          <Link href={`/room/${roomId}/confirm`}>다음</Link>
+        </Button>
       </div>
     </>
   );
 };
 
-export default PickPlace;
+export default PickPlacePage;
