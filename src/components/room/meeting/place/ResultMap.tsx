0b9e1bda448922faa0e971bfbef07145f46cc57d
heechul94 <yhcu3517@gmail.com>
2024-04-13 22:40:45 +0900

    fix: import 경로 수정

--- src/components/room/meeting/place/ResultMap.tsx
+++ src/components/room/meeting/place/ResultMap.tsx
@@ -5,7 +5,7 @@ import { useMapController } from '@/hooks/useMapController';
 import UserMarker from './UserMarker';
 import Halfway from './Halfway';
 import RangeController from './RangeController';
-import styles from './KakaoMap.module.css';
+import styles from './ResultMap.module.css';
 
 const ResultMap = () => {
   const [loading, error] = useKakaoMap();
@@ -17,7 +17,7 @@ const ResultMap = () => {
   if (error) return <div>error</div>;
 
   return (
-    <div>
+    <>
       <div className={styles.map_container}>
         <Map center={location} className={styles.map}>
           {roomUsers
@@ -37,7 +37,7 @@ const ResultMap = () => {
         </Map>
       </div>
       <div>{address}</div>
-    </div>
+    </>
   );
 };
 
