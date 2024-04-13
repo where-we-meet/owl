bc8efd7ae742dc2fe0f236c22dbd1949fa9ef2fe
lauichan <te06230623@gmail.com>
2024-04-13 22:03:12 +0900

    style: 시작페이지 디지인

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -1,7 +1,12 @@
 import StartMeeting from '@/components/room/StartMeeting';
+import styles from './page.module.css';
 
 const page = async () => {
-  return <StartMeeting />;
+  return (
+    <main className={styles.main}>
+      <StartMeeting />
+    </main>
+  );
 };
 
 export default page;
