39fa484e2500f464c442549da65b28829e2efd24
lauichan <te06230623@gmail.com>
2024-04-14 05:43:05 +0900

    refactor: 함수 이름 이해하기 쉽게, import 수정

--- src/components/auth/LoginButton.tsx
+++ src/components/auth/LoginButton.tsx
@@ -3,16 +3,16 @@
 import KakaoSymbol from './KakaoSymbol';
 import GoogleLogo from './GoogleLogo';
 import styles from './LoginButton.module.css';
-import { logInWithOAuth } from '@/api/auth';
+import { signInWithOAuth } from '@/api/auth/signInWithOAuth';
 
 const LoginButton = () => {
   return (
     <form className={styles.form}>
-      <button className={styles.kakao} onClick={() => logInWithOAuth('kakao')} role="button">
+      <button className={styles.kakao} onClick={() => signInWithOAuth('kakao')} role="button">
         <KakaoSymbol />
         <span>카카오 로그인</span>
       </button>
-      <button className={styles.google} onClick={() => logInWithOAuth('google')} role="button">
+      <button className={styles.google} onClick={() => signInWithOAuth('google')} role="button">
         <GoogleLogo />
         <span>구글 로그인</span>
       </button>
