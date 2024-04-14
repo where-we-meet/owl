251ff40f3d166b5017f7e7e2c7c12fe409025a0b
lauichan <te06230623@gmail.com>
2024-04-15 06:04:53 +0900

    refactor: 컴포넌트 위치 이동

--- src/components/room/meeting/ResultPlace.tsx
+++ src/components/room/meeting/ResultPlace.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import RangeController from './place/RangeController';
 import ResultMap from './place/ResultMap';
-import CategorySelector from './place/search/CategorySelector';
 import styles from './ResultPlace.module.css';
 
 const ResultPlace = () => {
@@ -10,9 +8,7 @@ const ResultPlace = () => {
     <section>
       <div className={styles.container}>
         <ResultMap />
-        <CategorySelector />
       </div>
-      <RangeController />
     </section>
   );
 };
