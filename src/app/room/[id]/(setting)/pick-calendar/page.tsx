ac887868e080bbcc3217bb4620f51d370afe368f
heechul94 <yhcu3517@gmail.com>
2024-04-14 16:21:19 +0900

    style: 일정 설정 페이지 UI수정

--- src/app/room/[id]/(setting)/pick-calendar/page.tsx
+++ src/app/room/[id]/(setting)/pick-calendar/page.tsx
@@ -1,7 +1,25 @@
+'use client';
+
 import Calender from '@/components/room/meeting/calender/Calender';
+import ResetSchedule from '@/components/room/meeting/calender/ResetSchedule';
+import { useCalendarStore } from '@/store/calendarStore';
+import checkSelectedDates from '@/utils/calendar/checkSelectedDates';
+import { Button, Link } from '@nextui-org/react';
+import { useParams } from 'next/navigation';
 
 const PickCalendar = () => {
-  return <Calender />;
+  const { id } = useParams();
+  const selectedDates = useCalendarStore((state) => state.selectedDates);
+
+  return (
+    <>
+      <ResetSchedule />
+      <Calender />
+      <Button href={`/room/${id}/pick-place`} size="sm" disabled={!checkSelectedDates(selectedDates)} as={Link}>
+        다음
+      </Button>
+    </>
+  );
 };
 
 export default PickCalendar;
