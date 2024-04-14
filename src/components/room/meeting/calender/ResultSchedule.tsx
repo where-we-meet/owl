8664857bbe9f686e9f5ab0c72421fa1b28d693fc
lauichan <te06230623@gmail.com>
2024-04-15 08:30:34 +0900

    feat: 모임 현황 페이지 투표순으로 날짜 표시

--- src/components/room/meeting/calender/ResultSchedule.tsx
+++ src/components/room/meeting/calender/ResultSchedule.tsx
@@ -9,12 +9,15 @@ import { Link } from '@nextui-org/react';
 const ResultSchedule = () => {
   const { id: roomId }: { id: string } = useParams();
   const { userSchedules } = useGetCalendar(roomId);
-  const { maxDates, maxLength } = mostSchedule(userSchedules);
+  const dates = mostSchedule(userSchedules);
+
   return (
     <div className={styles.box}>
-      <strong>{maxLength}명의 유저가 선호하는 날짜</strong>
-      {maxDates.map((date, i) => (
-        <div key={i}>{date}</div>
+      {dates.map(([date, array], i) => (
+        <li key={i}>
+          {date}
+          <span>{array!.length}</span>
+        </li>
       ))}
       <Link href={`/room/${roomId}/pick-calendar`} className={styles.edit_calendar}>
         내 일정 변경하기
