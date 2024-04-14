e8c49afa4399d5dd055b0108d084ea53ee140308
heechul94 <yhcu3517@gmail.com>
2024-04-14 16:22:46 +0900

    feat: 본인 날짜 삭제 useMutation 커스텀 훅 생성

--- src/hooks/useMutateUserData.ts
+++ src/hooks/useMutateUserData.ts
@@ -1,4 +1,4 @@
-import { upsertSchedule, updateStartLocation } from '@/api/supabaseCSR/supabase';
+import { upsertSchedule, updateStartLocation, deleteSchedule } from '@/api/supabaseCSR/supabase';
 import { UserLocationData } from '@/types/place.types';
 import { UpsertUserSchedule } from '@/types/roomUser';
 import { useMutation } from '@tanstack/react-query';
@@ -13,6 +13,15 @@ export const useMutateUserData = () => {
     mutationFn: async (payload: Payload) => {
       const updateLocationResponse = await updateStartLocation(payload.userLocationData);
       const updateCalendarResponse = await upsertSchedule(payload.userSchedules);
+    }
+  });
+  return { mutate, isSuccess };
+};
+
+export const useDeleteUserSchedule = () => {
+  const { mutate, isSuccess } = useMutation({
+    mutationFn: async (payload: { roomId: string; userId: string }) => {
+      await deleteSchedule(payload);
     }
   });
   return { mutate, isSuccess };
