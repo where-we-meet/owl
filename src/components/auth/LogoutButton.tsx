fba25807fc227b0366845a985356637e5dd1c741
ketchup0211 <dlcka1111@naver.com>
2024-04-14 23:10:13 +0900

    style: 헤더 아이콘 interaction 추가

--- src/components/auth/LogoutButton.tsx
+++ src/components/auth/LogoutButton.tsx
@@ -1,6 +1,7 @@
+import styles from './LogoutButton.module.css';
 const Logout = () => {
   return (
-    <form action="/auth/signout" method="post" title="잘가올...">
+    <form className={styles.logout} action="/auth/signout" method="post" title="잘가올...">
       <button type="submit">로그아웃</button>
     </form>
   );
