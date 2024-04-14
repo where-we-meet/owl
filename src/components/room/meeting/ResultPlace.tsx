753c43f34c5620f7c35f6eba4ebe7bc7d2c4ed4b
lauichan <te06230623@gmail.com>
2024-04-15 05:25:58 +0900

    style: 모임 현황 장소 표시 UI

--- src/components/room/meeting/ResultPlace.tsx
+++ src/components/room/meeting/ResultPlace.tsx
@@ -3,13 +3,16 @@
 import RangeController from './place/RangeController';
 import ResultMap from './place/ResultMap';
 import CategorySelector from './place/search/CategorySelector';
+import styles from './ResultPlace.module.css';
 
 const ResultPlace = () => {
   return (
     <section>
-      <ResultMap />
+      <div className={styles.container}>
+        <ResultMap />
+        <CategorySelector />
+      </div>
       <RangeController />
-      <CategorySelector />
     </section>
   );
 };
