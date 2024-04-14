e80e3688c3884618625547dec7db08b9dbfafd53
lauichan <te06230623@gmail.com>
2024-04-15 08:29:34 +0900

    feat: 모임 확정 버튼 추가

--- src/components/room/ConfirmedButton.tsx
+++ src/components/room/ConfirmedButton.tsx
@@ -1,40 +1,68 @@
 'use client';
+import { useGetCalendar } from '@/hooks/useGetCalendar';
+import { useHalfwayDataStore } from '@/store/placeStore';
+import { mostSchedule } from '@/utils/mostSchedule';
+import { useParams } from 'next/navigation';
 import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from '@nextui-org/react';
+import styles from './ConfirmedButton.module.css';
+import { FormEvent } from 'react';
+import { useQueryUser } from '@/hooks/useQueryUser';
+import { useQuery } from '@tanstack/react-query';
+import { getRoomIsConfirmed, updateRoomData } from '@/api/room';
 
 const ConfirmedButton = () => {
+  const { id: roomId }: { id: string } = useParams();
+  const { id: userId } = useQueryUser();
+  const { userSchedules } = useGetCalendar(roomId);
+  const { maxDates, maxLength } = mostSchedule(userSchedules);
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const { lat, lng, location } = useHalfwayDataStore();
+  const { data: room, isPending } = useQuery({
+    queryKey: ['room', 'confirmed', 'createdBy'],
+    queryFn: () => getRoomIsConfirmed(roomId),
+    select: (data) => data[0]
+  });
+
+  const handleConfirm = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!lat || !lng || !location) return;
+    const confirmed_date = e.currentTarget.date.value;
+    await updateRoomData(roomId, { lat: String(lat), lng: String(lng), location, verified: true, confirmed_date });
+  };
+
+  if (room && room.created_by !== userId) return null;
+
   return (
     <>
-      <Button onPress={onOpen}>확정</Button>
+      <Button onPress={onOpen} disabled={isPending}>
+        확정
+      </Button>
       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
         <ModalContent>
           {(onClose) => (
             <>
               <ModalHeader className="flex flex-col gap-1">모임 일정, 장소를 확정해주세요</ModalHeader>
-              <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non risus hendrerit
-                  venenatis. Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non risus hendrerit
-                  venenatis. Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor adipisicing. Mollit
-                  dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit officia eiusmod Lorem aliqua enim laboris
-                  do dolor eiusmod. Et mollit incididunt nisi consectetur esse laborum eiusmod pariatur proident Lorem
-                  eiusmod et. Culpa deserunt nostrud ad veniam.
-                </p>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  취소
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  확정
-                </Button>
-              </ModalFooter>
+              <form onSubmit={handleConfirm}>
+                <ModalBody>
+                  <div className={styles.date}>
+                    {maxDates.map((date, i) => (
+                      <label key={i}>
+                        <input type="radio" name="date" value={date} />
+                        {date}
+                      </label>
+                    ))}
+                  </div>
+                  <p>{location}</p>
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="danger" variant="light" onPress={onClose}>
+                    취소
+                  </Button>
+                  <Button type="submit" color="primary" onPress={onClose}>
+                    확정
+                  </Button>
+                </ModalFooter>
+              </form>
             </>
           )}
         </ModalContent>
