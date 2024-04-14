1efa6a6045d9367f760f5a9ce4c073be6ae6090f
heechul94 <yhcu3517@gmail.com>
2024-04-15 01:15:00 +0900

    fix: 경도 위도 NaN 뜨는 문제 null로 나오게끔 변경

--- src/utils/place/calcHalfwayPoint.ts
+++ src/utils/place/calcHalfwayPoint.ts
@@ -4,13 +4,17 @@ import convexHull from '@/utils/place/convexHull';
 import type { RoomUser } from '@/types/roomUser';
 
 export const calcHalfwayPoint = (roomUsers: RoomUser[]) => {
+  if (roomUsers.length === 0) return { lat: null, lng: null };
   const hasLocationUsers = roomUsers
     .filter((user) => !!user.start_location)
     .map((user) => ({ location: { lat: Number(user.lat), lng: Number(user.lng) } }));
+  if (hasLocationUsers.length === 1) {
+    return hasLocationUsers[0].location;
+  } else {
+    const sortedMarkers = convexHull(hasLocationUsers);
 
-  const sortedMarkers = convexHull(hasLocationUsers);
+    const halfwayPoint = calcCenterPoint(sortedMarkers);
 
-  const halfwayPoint = calcCenterPoint(sortedMarkers);
-
-  return halfwayPoint;
+    return halfwayPoint;
+  }
 };
