b37eed3a65c17844d035bb186e56197b987d8400
lauichan <te06230623@gmail.com>
2024-04-15 08:15:25 +0900

    feat: 모임 확정여부, 방장, 모임 정보 업데이트

--- src/api/room.ts
+++ src/api/room.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@/utils/supabase/client';
+import { eq } from 'lodash';
 
 const supabase = createClient();
 
@@ -33,5 +34,20 @@ export const insertRoomUser = async ({
 
   if (insertUserError) throw insertUserError;
 
+  return data;
+};
+
+export const getRoomIsConfirmed = async (roomId: string) => {
+  const { data, error } = await supabase.from('rooms').select('verified, created_by').eq('id', roomId);
+  if (error) throw error;
+  return data;
+};
+
+export const updateRoomData = async (
+  roomId: string,
+  updated: { lat: string; lng: string; location: string; verified: boolean; confirmed_date: string }
+) => {
+  const { data, error } = await supabase.from('rooms').update(updated).eq('id', roomId).select();
+  if (error) throw error;
   return data;
 };
