6f81286b689c9df5e2bd8ab5a14ef60b230c90f5
heechul94 <yhcu3517@gmail.com>
2024-04-15 01:20:36 +0900

    fix: 컨트롤러에서 분리된 원 반경 적용

--- src/components/room/meeting/place/ResultMap.tsx
+++ src/components/room/meeting/place/ResultMap.tsx
@@ -1,15 +1,14 @@
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { Circle, Map, MapMarker } from 'react-kakao-maps-sdk';
 import { useKakaoMap } from '@/hooks/useKakaoMap';
 import { useMapController } from '@/hooks/useMapController';
 
 import UserMarker from './UserMarker';
 import Halfway from './Halfway';
-import RangeController from './RangeController';
 import styles from './ResultMap.module.css';
 
 const ResultMap = () => {
   const [loading, error] = useKakaoMap();
-  const { location, address, halfwayPoint, roomUsers, searchCategory = [] } = useMapController();
+  const { address, halfwayPoint, range, roomUsers, searchCategory = [] } = useMapController();
 
   const isHalfwayValid = halfwayPoint.lat && halfwayPoint.lng;
 
@@ -18,25 +17,36 @@ const ResultMap = () => {
 
   return (
     <>
-      <div className={styles.map_container}>
-        <Map center={location} className={styles.map}>
-          {roomUsers
-            .filter(({ lat, lng }) => lat !== null && lng !== null)
-            .map(({ id, lat, lng }) => (
-              <UserMarker key={id} id={id} lat={lat as string} lng={lng as string} />
-            ))}
-          {searchCategory.map(({ x, y }, i) => (
-            <MapMarker key={i} position={{ lat: +y, lng: +x }} />
-          ))}
-          {isHalfwayValid && (
-            <>
-              <Halfway location={halfwayPoint} />
-              <RangeController center={halfwayPoint} />
-            </>
-          )}
-        </Map>
-      </div>
       <div>{address}</div>
+      <div className={styles.map_container}>
+        {isHalfwayValid && (
+          <>
+            <Map center={halfwayPoint} className={styles.map}>
+              {roomUsers
+                .filter(({ lat, lng }) => lat !== null && lng !== null)
+                .map(({ id, lat, lng }) => (
+                  <UserMarker key={id} id={id} lat={lat as string} lng={lng as string} />
+                ))}
+              {searchCategory.map(({ x, y }, i) => (
+                <MapMarker key={i} position={{ lat: +y, lng: +x }} />
+              ))}
+              <>
+                <Halfway location={halfwayPoint} />
+                <Circle
+                  center={halfwayPoint}
+                  radius={range as number}
+                  strokeWeight={3}
+                  strokeColor={'#000000'}
+                  strokeOpacity={0.1}
+                  strokeStyle={'solid'}
+                  fillColor={'#00a0e9'}
+                  fillOpacity={0.05}
+                />
+              </>
+            </Map>
+          </>
+        )}
+      </div>
     </>
   );
 };
