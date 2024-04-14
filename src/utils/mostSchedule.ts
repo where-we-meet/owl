263ce58e289de308c25aef3ee22b6c8b387883ca
heechul94 <yhcu3517@gmail.com>
2024-04-15 01:15:29 +0900

    feat: 가장 많이 선택된 날짜 구하는 함수

---
+++ src/utils/mostSchedule.ts
@@ -0,0 +1,23 @@
+export const mostSchedule = (userSchedules: any) => {
+  const result = Object.groupBy(userSchedules, ({ start_date }: { start_date: string }) => start_date || 'not-setting');
+  const sortResult = Object.entries(result).sort((a, b) => {
+    const aLength = a[1] ? a[1].length : 0;
+    const bLength = b[1] ? b[1].length : 0;
+    return aLength - bLength;
+  });
+
+  let maxLength = 0;
+  let maxDates: string[] = [];
+
+  sortResult.forEach(([date, array]) => {
+    const length = Array.isArray(array) ? array.length : 0;
+    if (length > maxLength) {
+      maxLength = length;
+      maxDates = [date];
+    } else if (length === maxLength) {
+      maxDates.push(date);
+    }
+  });
+
+  return maxDates;
+};
