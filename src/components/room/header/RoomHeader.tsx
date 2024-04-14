9295a992b7fa5d9ee175bc397f60f6af0700ef07
lauichan <te06230623@gmail.com>
2024-04-15 04:19:20 +0900

    feat: 새로운 유저가 방에 들어오면 일정을 선택할 수 있는 페이지로 이동

--- src/components/room/header/RoomHeader.tsx
+++ src/components/room/header/RoomHeader.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import UserList from './user/UserList';
 import LinkShare from '../share/LinkShare';
 import { useQueryUser } from '@/hooks/useQueryUser';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import { insertRoomUser } from '@/api/room';
 import { useGetRoomUsers } from '@/hooks/useGetRoomUsers';
 import { useQuery } from '@tanstack/react-query';
@@ -11,6 +11,7 @@ import { getRooomData } from '@/api/supabaseCSR/supabase';
 import styles from './RoomHeader.module.css';
 
 const RoomHeader = () => {
+  const router = useRouter();
   const { id: userId } = useQueryUser();
   const { id: roomId }: { id: string } = useParams();
   const { data: room, isPending } = useQuery({ queryKey: ['room', roomId], queryFn: () => getRooomData(roomId) });
@@ -19,6 +20,7 @@ const RoomHeader = () => {
   const joinNewUser = async () => {
     const isExistUser = roomUsers.some((user) => user.user_id === userId);
     if (isExistUser) return;
+    router.replace(`/room/${roomId}/pick-calendar`);
     await insertRoomUser({ room_id: roomId, user_id: userId, is_admin: false });
   };
 
