061637cf57c915e0ff7233bce5faaa1fadbcfdf9
ketchup0211 <dlcka1111@naver.com>
2024-04-14 21:46:58 +0900

    style: 유저 프로필 설정 버튼 변경

--- src/components/header/UserProfileButton.tsx
+++ src/components/header/UserProfileButton.tsx
@@ -1,16 +1,31 @@
 'use client';
 
-import { Modal, ModalContent, useDisclosure } from '@nextui-org/react';
-import { useState } from 'react';
+import { Avatar, Modal, ModalContent, useDisclosure } from '@nextui-org/react';
+import { useEffect, useState } from 'react';
 import UserProfileUpdate from './UserProfileUpdate';
 import UserProfileRead from './UserProfileRead';
 import { PiUserSquareDuotone } from 'react-icons/pi';
 
 import styles from './UserProfileButton.module.css';
+import { useQueryUser } from '@/hooks/useQueryUser';
+import { getUserProfileData } from '@/api/profile';
 
 const UserProfile = () => {
+  const user = useQueryUser();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getUserProfileData(user.id);
+        setProfile(data.profile_url);
+      } catch (error) {
+        console.error('Error fetching user profile data:', error);
+      }
+    };
+    fetchData();
+  }, []);
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [editMode, setEditMode] = useState(false);
+  const [profile, setProfile] = useState<null | string>(null);
 
   const handleClose = () => {
     setEditMode(false);
@@ -24,7 +39,13 @@ const UserProfile = () => {
   return (
     <>
       <div className="flex gap-4 items-center">
-        <PiUserSquareDuotone onClick={onOpen} size="1.5rem" aria-label="User Profile Setting" />
+        <Avatar
+          className={styles.profile}
+          onClick={onOpen}
+          showFallback
+          name={user.user_metadata.user_name}
+          src={`${profile}`}
+        />
       </div>
 
       <Modal className={styles.modal} backdrop="blur" isOpen={isOpen} onClose={handleClose} hideCloseButton>
