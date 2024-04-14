19503226ec58afdf7b8002962e20c043d7808a26
ketchup0211 <dlcka1111@naver.com>
2024-04-15 00:59:02 +0900

    style: (스파이) 룸 페이지 헤더 ui 수정

--- src/components/room/header/user/UserList.tsx
+++ src/components/room/header/user/UserList.tsx
@@ -1,22 +1,20 @@
-import { Avatar, Tooltip } from '@nextui-org/react';
+import { Avatar, AvatarGroup, Tooltip } from '@nextui-org/react';
 import styles from './UserList.module.css';
 import type { RoomUser } from '@/types/roomUser';
 
 const UserList = ({ roomUsers }: { roomUsers: RoomUser[] }) => {
   return (
-    <ul className={styles.user_list}>
+    <AvatarGroup max={5} className={styles.user_list}>
       {roomUsers.map((user) => {
         return (
           user.users && (
-            <li key={user.id}>
-              <Tooltip showArrow={true} content={user.start_location} placement="bottom-start">
-                <Avatar isBordered src={`${user.users.profile_url}`} showFallback name={user.users.name} />
-              </Tooltip>
-            </li>
+            <Tooltip key={user.id} showArrow={true} content={user.start_location} placement="bottom-start">
+              <Avatar isBordered src={`${user.users.profile_url}`} showFallback name={user.users.name} />
+            </Tooltip>
           )
         );
       })}
-    </ul>
+    </AvatarGroup>
   );
 };
 
