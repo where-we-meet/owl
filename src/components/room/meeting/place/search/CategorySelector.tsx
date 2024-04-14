c3b096a7b93d499960c1efcc3d73046b4b9ca06d
heechul94 <yhcu3517@gmail.com>
2024-04-15 01:12:07 +0900

    style: 카테고리 아이콘 적용
    Co-authored-by: CHUMMY_CHAM(trueDOM) <ketchup0211@users.noreply.github.com>
    Co-authored-by: nauichan <lauichan@users.noreply.github.com>

--- src/components/room/meeting/place/search/CategorySelector.tsx
+++ src/components/room/meeting/place/search/CategorySelector.tsx
@@ -1,11 +1,25 @@
-import { useHalfwayDataStore, useSearchDataStore } from '@/store/placeStore';
 import { MouseEvent } from 'react';
+import { useHalfwayDataStore, useRangeStore, useSearchDataStore } from '@/store/placeStore';
 
-const SEARCH_CATEGORY = ['맛집', '카페', '음식점', '문화시설', '관광명소', '지하철역', '숙박'];
+import { IoCafe, IoRestaurant } from 'react-icons/io5';
+import { FaTrainSubway, FaLandmark } from 'react-icons/fa6';
+import { RiMovie2Line, RiHotelBedFill } from 'react-icons/ri';
+import { BiSolidParking } from 'react-icons/bi';
+
+const SEARCH_CATEGORY = [
+  { name: '맛집', icon: <IoRestaurant /> },
+  { name: '카페', icon: <IoCafe /> },
+  { name: '문화시설', icon: <RiMovie2Line /> },
+  { name: '관광명소', icon: <FaLandmark /> },
+  { name: '지하철역', icon: <FaTrainSubway /> },
+  { name: '주차장', icon: <BiSolidParking /> },
+  { name: '숙박', icon: <RiHotelBedFill /> }
+];
 
 const CategorySelector = () => {
   const setSearchOption = useSearchDataStore((state) => state.setSearchOption);
-  const { lat, lng, range } = useHalfwayDataStore((state) => state.halfwayData);
+  const { lat, lng } = useHalfwayDataStore((state) => state);
+  const range = useRangeStore((state) => state.range);
 
   const handleCategorySearch = (event: MouseEvent<HTMLUListElement>) => {
     const selectCategory = (event.target as HTMLLIElement).id;
@@ -20,8 +34,9 @@ const CategorySelector = () => {
   return (
     <ul onClick={handleCategorySearch}>
       {SEARCH_CATEGORY.map((item) => (
-        <li key={item} id={item}>
-          {item}
+        <li key={item.name} id={item.name}>
+          {item.icon}
+          <span>{item.name}</span>
         </li>
       ))}
     </ul>
