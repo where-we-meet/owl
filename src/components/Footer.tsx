14e6a79a49872eb8481c3d2587d93a8a8e84f970
ketchup0211 <dlcka1111@naver.com>
2024-04-15 04:36:00 +0900

    style: footer 변경

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,16 @@
 import { Link } from '@nextui-org/react';
 import styles from './Footer.module.css';
-
+import { FaGithub } from 'react-icons/fa';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <Link href="https://github.com/where-we-meet/owl">@github owllink</Link>
-      <p>about us</p>
+      <Link href="https://github.com/where-we-meet/owl">
+        <FaGithub />
+        &nbsp;owl-link
+      </Link>
+      <div>
+        <p>about us</p>
+      </div>
     </footer>
   );
 };
