4627e9cd3b6266ac19a682c386fbe8dbdef573ad
ketchup0211 <dlcka1111@naver.com>
2024-04-14 22:57:12 +0900

    style: 모임 모달 열릴 떄 및 닫힐 떄 애니메이션 추가

--- src/components/header/MeetingModalButton.tsx
+++ src/components/header/MeetingModalButton.tsx
@@ -4,6 +4,7 @@ import { HiRectangleStack } from 'react-icons/hi2';
 import { Modal, ModalBody, ModalContent, useDisclosure } from '@nextui-org/react';
 import { MouseEvent } from 'react';
 import styles from './MeetingModalButton.module.css';
+import { exit } from 'process';
 
 export const MeetingModalButton = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -15,7 +16,33 @@ export const MeetingModalButton = () => {
   return (
     <div className={styles.meeting_modal_btn} title="참가 중인 모임 보기">
       <HiRectangleStack onClick={onOpen} size="1.4rem" />
-      <Modal className={styles.modal} backdrop="opaque" size="3xl" isOpen={isOpen} onOpenChange={onClose}>
+      <Modal
+        className={styles.modal}
+        backdrop="opaque"
+        size="3xl"
+        isOpen={isOpen}
+        onOpenChange={onClose}
+        motionProps={{
+          variants: {
+            enter: {
+              x: 0,
+              opacity: 1,
+              transition: {
+                duration: 0.3,
+                ease: 'easeInOut'
+              }
+            },
+            exit: {
+              x: -100,
+              opacity: 0,
+              transition: {
+                duration: 0.2,
+                ease: 'easeIn'
+              }
+            }
+          }
+        }}
+      >
         <ModalContent>
           <ModalBody className={styles.modalbody}>
             <div onClick={(e) => handleMeetingRedirect(e)}>
