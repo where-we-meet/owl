9116fa175c835d9793a7c741deb3bc268ec3060c
lauichan <te06230623@gmail.com>
2024-04-15 04:20:06 +0900

    feat: 방 일정을 확정할 수 있는 버튼 추가

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,12 +1,17 @@
 import ResultPlace from '@/components/room/meeting/ResultPlace';
 import ResultSchedule from '@/components/room/meeting/calender/ResultSchedule';
+import styles from './page.module.css';
+import { Button } from '@nextui-org/react';
 
 const RoomPage = () => {
   return (
-    <>
+    <div className={styles.container}>
       <ResultSchedule />
       <ResultPlace />
-    </>
+      <div className={styles.footer}>
+        <Button>확정</Button>
+      </div>
+    </div>
   );
 };
 
