e3f80b8e46c5f0056b587ce7197c4d85e4ce2cb7
lauichan <te06230623@gmail.com>
2024-04-14 16:24:03 +0900

    refactor: 룸 유저 가져오는 로직 이동

--- src/components/room/header/user/UserList.tsx
+++ src/components/room/header/user/UserList.tsx
@@ -1,29 +1,8 @@
-'use client';
-import { insertRoomUser } from '@/api/room';
-import { useGetRoomData } from '@/hooks/useGetRoomData';
-import { useQueryUser } from '@/hooks/useQueryUser';
-import { useParams } from 'next/navigation';
-import { useEffect } from 'react';
-import styles from './UserList.module.css';
 import { Avatar, Tooltip } from '@nextui-org/react';
+import styles from './UserList.module.css';
+import type { RoomUser } from '@/types/roomUser';
 
-const UserList = () => {
-  const { id: userId } = useQueryUser();
-  const { id: roomId }: { id: string } = useParams();
-  const { roomUsers } = useGetRoomData(roomId, userId);
-
-  const joinNewUser = async () => {
-    const isExistUser = roomUsers.some((user) => user.user_id === userId);
-    if (isExistUser) return;
-    await insertRoomUser({ room_id: roomId, user_id: userId, is_admin: false });
-  };
-
-  useEffect(() => {
-    if (roomUsers.length > 0) {
-      joinNewUser();
-    }
-  }, [roomUsers.length]);
-
+const UserList = ({ roomUsers }: { roomUsers: RoomUser[] }) => {
   return (
     <ul className={styles.user_list}>
       {roomUsers.map((user) => {
