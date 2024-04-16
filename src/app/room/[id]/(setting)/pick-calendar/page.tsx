267edc9df8466dec34ddf0766534697d4b517930
lauichan <te06230623@gmail.com>
2024-04-16 22:28:39 +0900

    refactor: Calendar 컴포넌트 재사용을 위해 props로 데이터 넣기

--- src/app/room/[id]/(setting)/pick-calendar/page.tsx
+++ src/app/room/[id]/(setting)/pick-calendar/page.tsx
@@ -3,19 +3,31 @@
 import Calender from '@/components/room/meeting/calender/Calender';
 import { useCalendarStore } from '@/store/calendarStore';
 import checkSelectedDates from '@/utils/calendar/checkSelectedDates';
-import { Button, Link } from '@nextui-org/react';
 import { useParams } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
+import { getRangeOfSchedule } from '@/api/supabaseCSR/supabase';
+import { useGetCalendar } from '@/hooks/useGetCalendar';
+import { Button, Link } from '@nextui-org/react';
 import styles from './page.module.css';
 
 const PickCalendar = () => {
-  const { id }: { id: string } = useParams();
+  const { id: roomId }: { id: string } = useParams();
   const selectedDates = useCalendarStore((state) => state.selectedDates);
+
+  const { userSchedules } = useGetCalendar(roomId);
+  const { data: scheduleRange, isPending } = useQuery({
+    queryKey: ['range', roomId],
+    queryFn: () => getRangeOfSchedule(roomId),
+    select: (data) => data[0]
+  });
+
+  if (!scheduleRange || isPending) return <>로딩중</>;
 
   return (
     <>
-      <Calender />
+      <Calender range={scheduleRange} schedules={userSchedules} />
       <div className={styles.footer}>
-        <Button href={`/room/${id}/pick-place`} disabled={!checkSelectedDates(selectedDates)} as={Link}>
+        <Button href={`/room/${roomId}/pick-place`} disabled={!checkSelectedDates(selectedDates)} as={Link}>
           다음
         </Button>
       </div>
