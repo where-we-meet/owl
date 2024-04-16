ad8f4af2b5a767dd2bb325efcd29b05aa55dd60c
heechul94 <yhcu3517@gmail.com>
2024-04-17 01:09:59 +0900

    fix: 장소를 선택한 user가 없을때 중심 위치 초기값 반환 수정

--- src/utils/place/calcHalfwayPoint.ts
+++ src/utils/place/calcHalfwayPoint.ts
@@ -4,10 +4,10 @@ import convexHull from '@/utils/place/convexHull';
 import type { RoomUser } from '@/types/roomUser';
 
 export const calcHalfwayPoint = (roomUsers: RoomUser[]) => {
-  if (roomUsers.length === 0) return { lat: null, lng: null };
   const hasLocationUsers = roomUsers
     .filter((user) => !!user.start_location)
     .map((user) => ({ location: { lat: Number(user.lat), lng: Number(user.lng) } }));
+  if (hasLocationUsers.length === 0) return { lat: null, lng: null };
   if (hasLocationUsers.length === 1) {
     return hasLocationUsers[0].location;
   } else {
