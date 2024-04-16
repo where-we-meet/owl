1f402b91b03c6b95699ecced12a58797aea8c02a
lauichan <te06230623@gmail.com>
2024-04-16 23:24:49 +0900

    refactor: 불필요한 코드 제거, 모임 생성페이지 헤더

--- src/app/start/(create)/layout.tsx
+++ src/app/start/(create)/layout.tsx
@@ -1,9 +1,11 @@
 import ProgressBar from '@/components/room/setting/ProgressBar';
 import styles from './layout.module.css';
+import RoomHeader from '@/components/room/header/RoomHeader';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className={styles.setting_container}>
+      <RoomHeader />
       <ProgressBar basePath={`/start`} />
       {children}
     </div>
--- src/app/start/layout.tsx
+++ src/app/start/layout.tsx
@@ -1,11 +1,9 @@
-import RoomHeader from '@/components/room/header/RoomHeader';
 import { ReactNode } from 'react';
 import styles from './layout.module.css';
 
 const RoomLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main className={styles.main}>
-      {/* <RoomHeader /> */}
       <div>{children}</div>
     </main>
   );
