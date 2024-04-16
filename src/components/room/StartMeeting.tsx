c6b6b7f29eafaaf574410fd46538eef3292670a5
lauichan <te06230623@gmail.com>
2024-04-16 22:33:29 +0900

    fix: StartMeeting컴포넌트에서 모임 생성 로직 이동

--- src/components/room/StartMeeting.tsx
+++ src/components/room/StartMeeting.tsx
@@ -1,19 +1,20 @@
 'use client';
 
-import { insertNewRoom, insertRoomUser } from '@/api/room';
-import { useQueryUser } from '@/hooks/useQueryUser';
 import { getCurrentFormattedDate } from '@/utils/getCurrentFormattedDate';
-import { useRouter } from 'next/navigation';
-import { ChangeEvent, FormEvent, useState } from 'react';
-import styles from './StartMeeting.module.css';
-import { Button, Input } from '@nextui-org/react';
+import { ChangeEvent, useState } from 'react';
 import { MdStart } from 'react-icons/md';
+import { useCreateRoomStore } from '@/store/createRoomStore';
+import { useRouter } from 'next/navigation';
+import { useCalendarStore } from '@/store/calendarStore';
+import { Button, Input } from '@nextui-org/react';
+import styles from './StartMeeting.module.css';
+
 const StartMeeting = () => {
   const router = useRouter();
-  const user = useQueryUser();
-
   const [startDate, setStartDate] = useState(getCurrentFormattedDate());
   const [endDate, setEndDate] = useState(getCurrentFormattedDate());
+  const setDateRange = useCreateRoomStore((state) => state.setDateRange);
+  const setSelectedDates = useCalendarStore((state) => state.setSelectedDates);
 
   const changeStartDate = (e: ChangeEvent<HTMLInputElement>) => {
     setStartDate(e.target.value);
@@ -23,26 +24,14 @@ const StartMeeting = () => {
     setEndDate(e.target.value);
   };
 
-  const startNewRoom = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (!user) return;
-
-    const endDate = e.currentTarget.end.value;
-
-    const [room] = await insertNewRoom({
-      name: '운좋은 올빼미',
-      created_by: user.id,
-      start_date: startDate,
-      end_date: endDate
-    });
-    await insertRoomUser({ room_id: room.id, user_id: user.id, is_admin: true });
-
-    router.push(`/room/${room.id}/pick-calendar`);
+  const createRoomOption = () => {
+    setSelectedDates([]);
+    setDateRange({ startDate, endDate });
+    router.push('/start/pick-calendar');
   };
 
   return (
-    <form onSubmit={startNewRoom}>
+    <form>
       <h1 className={styles.title}>모임 일정의 범위를 선택해주세요</h1>
       <div className={styles.date}>
         <label>
@@ -62,7 +51,7 @@ const StartMeeting = () => {
         </label>
       </div>
       <div className={styles.start_button}>
-        <Button type="submit">
+        <Button onPress={createRoomOption}>
           모임 시작하기
           <MdStart />
         </Button>
