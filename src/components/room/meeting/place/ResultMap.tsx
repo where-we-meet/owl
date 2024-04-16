3ea80f945740fbaa9bee60dc701bbdd64b90f8fa
heechul94 <yhcu3517@gmail.com>
2024-04-17 01:11:51 +0900

    feat: 카테고리 마커 지도에 적용

--- src/components/room/meeting/place/ResultMap.tsx
+++ src/components/room/meeting/place/ResultMap.tsx
@@ -1,19 +1,22 @@
-import { Circle, Map, MapMarker } from 'react-kakao-maps-sdk';
+import { useParams } from 'next/navigation';
+
+import { Circle, Map } from 'react-kakao-maps-sdk';
 import { useKakaoMap } from '@/hooks/useKakaoMap';
 import { useMapController } from '@/hooks/useMapController';
 
 import UserMarker from './UserMarker';
 import Halfway from './Halfway';
-import styles from './ResultMap.module.css';
-import { useParams } from 'next/navigation';
-import { Link } from '@nextui-org/react';
 import CategorySelector from './search/CategorySelector';
 import RangeController from './RangeController';
+import CategoryMarker from './search/CategoryMarker';
+
+import { Link } from '@nextui-org/react';
+import styles from './ResultMap.module.css';
 
 const ResultMap = () => {
   const [loading, error] = useKakaoMap();
   const { id: roomId } = useParams();
-  const { address, halfwayPoint, range, roomUsers, searchCategory = [] } = useMapController();
+  const { address, halfwayPoint, range, clickId, setClickId, roomUsers, searchCategory = [] } = useMapController();
 
   const isHalfwayValid = halfwayPoint.lat && halfwayPoint.lng;
 
@@ -25,14 +28,19 @@ const ResultMap = () => {
       <div className={styles.map_container}>
         {isHalfwayValid && (
           <>
-            <Map center={halfwayPoint} className={styles.map}>
+            <Map
+              center={halfwayPoint}
+              className={styles.map}
+              onClick={() => setClickId('')}
+              onTouchEnd={() => setClickId('')}
+            >
               {roomUsers
                 .filter(({ lat, lng }) => lat !== null && lng !== null)
                 .map(({ id, lat, lng }) => (
                   <UserMarker key={id} id={id} lat={lat as string} lng={lng as string} />
                 ))}
-              {searchCategory.map(({ x, y }, i) => (
-                <MapMarker key={i} position={{ lat: +y, lng: +x }} />
+              {searchCategory.map((info) => (
+                <CategoryMarker key={info.id} info={info} clickId={clickId} setClickId={setClickId} />
               ))}
               <>
                 <Halfway location={halfwayPoint} />
