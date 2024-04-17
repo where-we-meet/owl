a6aa95a775a1c19beaf00a8ffc054419a98fdaef
gidalim <gidalimforsomeday@gmail.com>
2024-04-17 16:04:13 +0900

    fix: 올빼미 생성기 오류 수정 및 적용 보류 오타수정

--- src/components/room/header/RoomHeader.tsx
+++ src/components/room/header/RoomHeader.tsx
@@ -6,24 +6,22 @@ import { useQueryUser } from '@/hooks/useQueryUser';
 import { useParams } from 'next/navigation';
 import { useGetRoomUsers } from '@/hooks/useGetRoomUsers';
 import { useQuery } from '@tanstack/react-query';
-import { getRooomData } from '@/api/supabaseCSR/supabase';
+import { getRoomData } from '@/api/supabaseCSR/supabase';
 import Link from 'next/link';
 import styles from './RoomHeader.module.css';
-import roomNameGenerator from '@/utils/roomNameGenerator';
 
 const RoomHeader = () => {
   const { id: userId } = useQueryUser();
   const { id: roomId }: { id: string } = useParams();
-  const { data: room } = useQuery({ queryKey: ['room', roomId], queryFn: () => getRooomData(roomId) });
+  const { data: room } = useQuery({ queryKey: ['room', roomId], queryFn: () => getRoomData(roomId) });
   const { roomUsers } = useGetRoomUsers(roomId, userId);
-  const roomName = roomNameGenerator()
 
   return (
     <div className={styles.room_header}>
       <div>
         <div className={styles.left}>
           <h1 className={styles.room_title}>
-            <Link href={`/room/${roomId}`}>{room ? room.name : roomName}</Link>
+            <Link href={`/room/${roomId}`}>{room ? room.name : '운좋은 올빼미'}</Link>
           </h1>
           {room && <LinkShare />}
         </div>
