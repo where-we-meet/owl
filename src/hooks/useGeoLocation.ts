bc85575516e87dcc1fe66abfc03b9781a6e82e7e
heechul94 <yhcu3517@gmail.com>
2024-04-17 22:57:15 +0900

    fix: gps 커스텀 훅 내부 로직 전역에서 가져오는 유저 정보로 적용

--- src/hooks/useGeoLocation.ts
+++ src/hooks/useGeoLocation.ts
@@ -7,14 +7,16 @@ import { useRoomUserDataStore } from '@/store/roomUserStore';
 export const useGeoLocation = () => {
   const setLocation = useSearchDataStore((state) => state.setLocation);
   const { setIsGpsLoading, setErrorMessage } = useGpsStatusStore((state) => state);
-  const roomUsers = useRoomUserDataStore((state) => state.roomUsers);
-
-  const { id } = useQueryUser();
-  const user = roomUsers.find((user) => user.user_id === id);
+  const roomUser = useRoomUserDataStore((state) => state.roomUser);
 
   const handleSetGeolocation = async () => {
     setIsGpsLoading(true);
-    const { lat, lng, status, errorMessage } = await myGeolocation();
+    const {
+      lat,
+      lng,
+      status,
+      errorMessage
+    }: { lat: number | null; lng: number | null; status: boolean; errorMessage: string | null } = await myGeolocation();
     if (lat && lng) {
       setLocation({ lat, lng });
       setIsGpsLoading(status);
@@ -25,14 +27,14 @@ export const useGeoLocation = () => {
   };
 
   useEffect(() => {
-    if (user && user.start_location === '') {
-      console.log('if', user, user.start_location);
+    if (roomUser && roomUser.start_location === '') {
+      console.log('if', roomUser, roomUser.start_location);
       handleSetGeolocation();
     } else {
-      console.log('else', user, user?.start_location);
+      console.log('else', roomUser, roomUser?.start_location);
       setIsGpsLoading(false);
     }
-  }, [user?.start_location]);
+  }, [roomUser?.start_location]);
 
   return { handleSetGeolocation };
 };
