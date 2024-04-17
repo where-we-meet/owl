8db2bc1c101e3555b9293de693fdfd272af3ac27
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-17 15:33:45 +0900

    fix: 일정 선택 시 ,세션 스토리지에 저장 후 확정 페이지로 넘어감

--- src/app/room/[id]/(setting)/pick-calendar/page.tsx
+++ src/app/room/[id]/(setting)/pick-calendar/page.tsx
@@ -9,10 +9,15 @@ import { getRangeOfSchedule } from '@/api/supabaseCSR/supabase';
 import { useGetCalendar } from '@/hooks/useGetCalendar';
 import { Button, Link } from '@nextui-org/react';
 import styles from './page.module.css';
+import { useGetSchedule } from '@/hooks/useGetSchedule';
+import { useQueryUser } from '@/hooks/useQueryUser';
+import { useEffect } from 'react';
 
 const PickCalendar = () => {
+  const { id: userId }: { id: string } = useQueryUser();
   const { id: roomId }: { id: string } = useParams();
   const selectedDates = useCalendarStore((state) => state.selectedDates);
+  const setSelectedDates = useCalendarStore((state) => state.setSelectedDates);
 
   const { userSchedules } = useGetCalendar(roomId);
   const { data: scheduleRange, isPending } = useQuery({
@@ -20,6 +25,14 @@ const PickCalendar = () => {
     queryFn: () => getRangeOfSchedule(roomId),
     select: (data) => data[0]
   });
+
+  const { myData: mySchedule } = useGetSchedule(userId, roomId);
+
+  useEffect(() => {
+    if (mySchedule) {
+      setSelectedDates(mySchedule.map((schedule) => new Date(String(schedule.start_date))));
+    }
+  }, [mySchedule.length]);
 
   if (!scheduleRange || isPending) return <>로딩중</>;
 
