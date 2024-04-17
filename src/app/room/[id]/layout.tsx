53c3255b8758a7c8bbf3c7c6d512af0b8338b3fa
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-17 16:56:19 +0900

    fix: 인앱브라우저에서 외부브라우저 호출 코드 추가

--- src/app/room/[id]/layout.tsx
+++ src/app/room/[id]/layout.tsx
@@ -1,8 +1,38 @@
+'use client';
+
 import RoomHeader from '@/components/room/header/RoomHeader';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import styles from './layout.module.css';
 
 const RoomLayout = ({ children }: { children: ReactNode }) => {
+  useEffect(() => {
+    const useragt = navigator.userAgent.toLowerCase();
+    const target_url = location.href;
+
+    if (useragt.match(/kakaotalk/i)) {
+      //카카오톡 외부브라우저로 호출
+      window.location.href = 'kakaotalk://web/openExternal?url=' + encodeURIComponent(target_url);
+    } else if (useragt.match(/line/i)) {
+      if (target_url.indexOf('?') !== -1) {
+        window.location.href = target_url + '&openExternalBrowser=1';
+      } else {
+        window.location.href = target_url + '?openExternalBrowser=1';
+      }
+    }
+    if (useragt.match(/iphone|ipad|ipod/i)) {
+      // 아이폰은 강제로 사파리를 실행할 수 없다 ㅠㅠ
+      // 모바일 대응 뷰포트 강제 설정
+      const mobile = document.createElement('meta');
+      mobile.name = 'viewport';
+      mobile.content = 'width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui';
+      document.getElementsByTagName('head')[0].appendChild(mobile);
+    } else {
+      // 안드로이드는 Chrome이 설치되어있음으로 강제로 스킴 실행한다.
+      window.location.href =
+        'intent://' + target_url.replace(/https?:\/\//i, '') + '#Intent;scheme=http;package=com.android.chrome;end';
+    }
+  }, []);
+
   return (
     <main className={styles.main}>
       <RoomHeader />
