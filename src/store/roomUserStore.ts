762d76d1fd066a61463aeaf61552bbf49e93e6dc
heechul94 <yhcu3517@gmail.com>
2024-04-17 22:48:26 +0900

    fix: 방의 유저 설정 데이터 전역관리로 변경

--- src/store/roomUserStore.ts
+++ src/store/roomUserStore.ts
@@ -3,29 +3,15 @@ import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
 type RoomUserData = {
-  roomUsers: RoomUser[];
-  setRoomUsers: (roomUsers: RoomUser[]) => void;
-  addRoomUserData: (payload: RoomUser) => void;
-  updateRoomUserData: (payload: RoomUser) => void;
+  roomUser: RoomUser | null;
+  setRoomUser: (roomUser: RoomUser) => void;
 };
 
 export const useRoomUserDataStore = create(
   persist<RoomUserData>(
-    (set, get) => ({
-      roomUsers: [],
-      setRoomUsers: (roomUsers) => set({ roomUsers }),
-      addRoomUserData: (payload) =>
-        set(() => ({
-          roomUsers: [...get().roomUsers, payload]
-        })),
-      updateRoomUserData: (payload) =>
-        set(() => ({
-          roomUsers: get().roomUsers.map((user) =>
-            user.id === payload.id
-              ? { ...user, start_location: payload.start_location, lat: payload.lat, lng: payload.lng }
-              : user
-          )
-        }))
+    (set) => ({
+      roomUser: null,
+      setRoomUser: (roomUser) => set({ roomUser })
     }),
     {
       name: 'room-userdata-storage',
