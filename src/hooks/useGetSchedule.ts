c2f9431e395088c6b31680d178c94f9456b86ede
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-17 15:33:45 +0900

    fix: 일정 선택 시 ,세션 스토리지에 저장 후 확정 페이지로 넘어감

--- src/hooks/useGetSchedule.ts
+++ src/hooks/useGetSchedule.ts
@@ -1,9 +1,7 @@
 import { getUserSchedule } from '@/api/supabaseCSR/supabase';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
 export const useGetSchedule = (userId: string, roomId: string) => {
-  const queryClient = useQueryClient();
-
   const { data = [] } = useQuery({
     queryKey: ['getUserSchedules', roomId],
     queryFn: () => getUserSchedule(roomId),
