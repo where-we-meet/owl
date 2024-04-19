f42722a9a55821a889e3e5ae7ab5b2476a5f1dd4
lauichan <te06230623@gmail.com>
2024-04-19 22:26:08 +0900

    feat: 방을 생성할 때 유저가 없으면 유저리스트에 추가

--- src/components/room/sidebar/user/UserList.tsx
+++ src/components/room/sidebar/user/UserList.tsx
@@ -3,13 +3,32 @@
 import { useGetRoomUsers } from '@/hooks/useGetRoomUsers';
 import { useQueryUser } from '@/hooks/useQueryUser';
 import { useParams } from 'next/navigation';
-import React from 'react';
+import React, { useEffect } from 'react';
 import UserCard from './UserCard';
+import { upsertRoomUser } from '@/api/room';
 
 const UserList = () => {
   const { id: userId } = useQueryUser();
   const { id: roomId }: { id: string } = useParams();
   const { roomUsers } = useGetRoomUsers(roomId, userId);
+
+  const joinNewUser = async () => {
+    await upsertRoomUser({
+      room_id: roomId,
+      user_id: userId,
+      start_location: null,
+      is_admin: false,
+      lat: null,
+      lng: null
+    });
+  };
+
+  useEffect(() => {
+    const isExistUser = roomUsers.some((user) => user.user_id === userId);
+    if (!isExistUser) {
+      joinNewUser();
+    }
+  }, [roomUsers.length]);
 
   return (
     <>
