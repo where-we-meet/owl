3da3271a2786133263c0197bd5f4c1c3e35005c1
heechul94 <yhcu3517@gmail.com>
2024-04-19 14:45:57 +0900

    fix: setting page 구조 변경

---
+++ src/app/room/[id]/page.tsx
@@ -0,0 +1,20 @@
+import SettingMap from '@/components/room/meeting/place/SettingMap';
+import Sidebar from '@/components/room/sidebar/Sidebar';
+import RoomHeader from '@/components/room/header/RoomHeader';
+import styles from './page.module.css';
+
+const SettingPage = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.sidebar_wrapper}>
+        <RoomHeader />
+        <Sidebar />
+      </div>
+      <div className={styles.map_wrapper}>
+        <SettingMap />
+      </div>
+    </div>
+  );
+};
+
+export default SettingPage;
