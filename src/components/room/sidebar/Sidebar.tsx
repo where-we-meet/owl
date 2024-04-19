405e35437b60fc2d91fc7899fab34eaa22f4b2d2
heechul94 <yhcu3517@gmail.com>
2024-04-19 14:49:49 +0900

    feat: 사이드바 생성

---
+++ src/components/room/sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import Calender from '../meeting/calender/Calender';
+import UserList from './user/UserList';
+
+const Sidebar = () => {
+  return (
+    <>
+      <Calender />
+      <UserList />
+    </>
+  );
+};
+
+export default Sidebar;
