323f74a286b6457eafdec139cbf8eefb2ed090e5
lauichan <te06230623@gmail.com>
2024-04-19 16:20:13 +0900

    fix: 줄정리, 변수이름 정리

--- src/app/room/[id]/layout.tsx
+++ src/app/room/[id]/layout.tsx
@@ -5,30 +5,27 @@ import styles from './layout.module.css';
 
 const RoomLayout = ({ children }: { children: ReactNode }) => {
   useEffect(() => {
-    const useragt = navigator.userAgent.toLowerCase();
-    const target_url = location.href;
+    const userAgent = navigator.userAgent.toLowerCase();
+    const targetURL = location.href;
 
-    if (useragt.match(/kakaotalk/i)) {
-      //카카오톡 외부브라우저로 호출
-      window.location.href = 'kakaotalk://web/openExternal?url=' + encodeURIComponent(target_url);
-    } else if (useragt.match(/line/i)) {
-      if (target_url.indexOf('?') !== -1) {
-        window.location.href = target_url + '&openExternalBrowser=1';
+    if (userAgent.match(/kakaotalk/i)) {
+      window.location.href = 'kakaotalk://web/openExternal?url=' + encodeURIComponent(targetURL);
+    } else if (userAgent.match(/line/i)) {
+      if (targetURL.indexOf('?') !== -1) {
+        window.location.href = targetURL + '&openExternalBrowser=1';
       } else {
-        window.location.href = target_url + '?openExternalBrowser=1';
+        window.location.href = targetURL + '?openExternalBrowser=1';
       }
     }
-    if (useragt.match(/iphone|ipad|ipod/i)) {
-      // 아이폰은 강제로 사파리를 실행할 수 없다 ㅠㅠ
-      // 모바일 대응 뷰포트 강제 설정
+
+    if (userAgent.match(/iphone|ipad|ipod/i)) {
       const mobile = document.createElement('meta');
       mobile.name = 'viewport';
       mobile.content = 'width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui';
       document.getElementsByTagName('head')[0].appendChild(mobile);
     } else {
-      // 안드로이드는 Chrome이 설치되어있음으로 강제로 스킴 실행한다.
       window.location.href =
-        'intent://' + target_url.replace(/https?:\/\//i, '') + '#Intent;scheme=http;package=com.android.chrome;end';
+        'intent://' + targetURL.replace(/https?:\/\//i, '') + '#Intent;scheme=http;package=com.android.chrome;end';
     }
   }, []);
 
