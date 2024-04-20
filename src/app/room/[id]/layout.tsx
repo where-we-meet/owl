6224fdbe22562431741bc66bcb572f5d016533f9
lauichan <te06230623@gmail.com>
2024-04-20 20:09:12 +0900

    fix: PC에서도 모바일 외부앱을 열려는 에러 수정

--- src/app/room/[id]/layout.tsx
+++ src/app/room/[id]/layout.tsx
@@ -9,23 +9,27 @@ const RoomLayout = ({ children }: { children: ReactNode }) => {
     const targetURL = location.href;
 
     if (userAgent.match(/kakaotalk/i)) {
-      window.location.href = 'kakaotalk://web/openExternal?url=' + encodeURIComponent(targetURL);
+      location.href = 'kakaotalk://web/openExternal?url=' + encodeURIComponent(targetURL);
     } else if (userAgent.match(/line/i)) {
       if (targetURL.indexOf('?') !== -1) {
-        window.location.href = targetURL + '&openExternalBrowser=1';
+        location.href = targetURL + '&openExternalBrowser=1';
       } else {
-        window.location.href = targetURL + '?openExternalBrowser=1';
+        location.href = targetURL + '?openExternalBrowser=1';
       }
-    }
-
-    if (userAgent.match(/iphone|ipad|ipod/i)) {
-      const mobile = document.createElement('meta');
-      mobile.name = 'viewport';
-      mobile.content = 'width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui';
-      document.getElementsByTagName('head')[0].appendChild(mobile);
-    } else {
-      window.location.href =
-        'intent://' + targetURL.replace(/https?:\/\//i, '') + '#Intent;scheme=http;package=com.android.chrome;end';
+    } else if (
+      userAgent.match(
+        /inapp|naver|snapchat|wirtschaftswoche|thunderbird|instagram|everytimeapp|whatsApp|electron|wadiz|aliapp|zumapp|iphone(.*)whale|android(.*)whale|kakaostory|band|twitter|DaumApps|DaumDevice\/mobile|FB_IAB|FB4A|FBAN|FBIOS|FBSS|trill|SamsungBrowser\/[^1]/i
+      )
+    ) {
+      if (userAgent.match(/iphone|ipad|ipod/i)) {
+        var mobile = document.createElement('meta');
+        mobile.name = 'viewport';
+        mobile.content = 'width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui';
+        document.getElementsByTagName('head')[0].appendChild(mobile);
+      } else {
+        location.href =
+          'intent://' + targetURL.replace(/https?:\/\//i, '') + '#Intent;scheme=http;package=com.android.chrome;end';
+      }
     }
   }, []);
 
