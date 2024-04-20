a97743bc43ac1dffff2204e44ccfd4b7fd389cd1
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-21 01:14:09 +0900

    style: userList의 border 스타일 변경

--- src/components/room/sidebar/user/UserCard.tsx
+++ src/components/room/sidebar/user/UserCard.tsx
@@ -1,4 +1,6 @@
+import { useQueryUser } from '@/hooks/useQueryUser';
 import { Avatar } from '@nextui-org/avatar';
+import styles from './userCard.module.css';
 
 type User = {
   created_at: string;
@@ -16,19 +18,28 @@ type User = {
 };
 
 const UserCard = ({ user }: { user: User }) => {
+  const { id: userId } = useQueryUser();
   const userInfo = {
     name: user.users?.name || '올빼미',
     avatar: user.users?.profile_url,
     startLocation: user.start_location
   };
 
+  const isCurrentUser = user.user_id === userId;
+
   return (
     <div>
-      <div>
-        <div>{userInfo.name}</div>
-        <div>{userInfo.startLocation}</div>
+      <div className={isCurrentUser ? styles.usercard_container : styles.mycard_container}>
+        <div className={styles.avatar_container}>
+          <Avatar isBordered src={`${userInfo.avatar}`} showFallback name={userInfo.name} />
+        </div>
+        <div className={styles.userdata_container}>
+          <p className={styles.username}>{userInfo.name}</p>
+          <p className={styles.user_address}>
+            {userInfo.startLocation ? userInfo.startLocation : '출발 위치를 설정해주세요.'}
+          </p>
+        </div>
       </div>
-      <Avatar isBordered src={`${userInfo.avatar}`} showFallback name={userInfo.name} />
     </div>
   );
 };
