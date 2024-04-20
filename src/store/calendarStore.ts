0122316b71949987a4fa0f324867a02b59bbee08
lauichan <te06230623@gmail.com>
2024-04-20 20:17:43 +0900

    fix: zustand 전역 상태 persist 기능 제거

--- src/store/calendarStore.ts
+++ src/store/calendarStore.ts
@@ -1,20 +1,11 @@
 import { create } from 'zustand';
-import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface CalendarState {
   selectedDates: Date[];
   setSelectedDates: (selectedDates: Date[]) => void;
 }
 
-export const useCalendarStore = create(
-  persist<CalendarState>(
-    (set) => ({
-      selectedDates: [],
-      setSelectedDates: (selectedDates) => set({ selectedDates })
-    }),
-    {
-      name: 'setting-calendar-storage',
-      storage: createJSONStorage(() => sessionStorage)
-    }
-  )
-);
+export const useCalendarStore = create<CalendarState>((set) => ({
+  selectedDates: [],
+  setSelectedDates: (selectedDates) => set({ selectedDates })
+}));
