2f024d1cd15bd82c9ee51c02370cf2b81cbddf91
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-20 22:23:17 +0900

    fix: 방장이 확정 후 새로고침 시 확정페이지로 이동

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,9 +1,31 @@
+'use client';
+
 import SettingMap from '@/components/room/place/SettingMap';
 import Sidebar from '@/components/room/sidebar/Sidebar';
 import RoomHeader from '@/components/room/header/RoomHeader';
+import { useQueryUser } from '@/hooks/useQueryUser';
+import { useEffect } from 'react';
+import { useParams, useRouter } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
+import { getRoomIsConfirmed } from '@/api/room';
 import styles from './page.module.css';
 
 const SettingPage = () => {
+  const router = useRouter();
+  const { id: userId } = useQueryUser();
+  const { id: roomId }: { id: string } = useParams();
+  const { data: room } = useQuery({
+    queryKey: ['room', 'confirmed', 'createdBy'],
+    queryFn: () => getRoomIsConfirmed(roomId),
+    select: (data) => data[0]
+  });
+
+  useEffect(() => {
+    if (room && room.verified && room.created_by !== userId) {
+      router.push(`/room/${roomId}/confirm`);
+    }
+  }, [room?.verified]);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.sidebar_wrapper}>
