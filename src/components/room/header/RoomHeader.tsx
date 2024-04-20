c070564fa8a38395fd3c91a7555be877090a16c0
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-21 03:20:42 +0900

    style: 방장 아이콘 추가

--- src/components/room/header/RoomHeader.tsx
+++ src/components/room/header/RoomHeader.tsx
@@ -3,24 +3,28 @@ import LinkShare from '../share/LinkShare';
 import { useParams } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import { getRoomData } from '@/api/supabaseCSR/supabase';
-import styles from './RoomHeader.module.css';
 import ConfirmedButton from '../ConfirmedButton';
+import { FaCrown } from 'react-icons/fa';
+import styles from './RoomHeader.module.css';
 
 const RoomHeader = () => {
   const { id: roomId }: { id: string } = useParams();
   const { data: room } = useQuery({ queryKey: ['room', roomId], queryFn: () => getRoomData(roomId) });
-
   return (
     <div className={styles.room_header}>
       <div>
-        <div className={styles.left}>
-          <h1 className={styles.room_title}>
-            <p>{room ? room.name : '올빼미를 불러오는 중'}</p>
-          </h1>
-          {room && <LinkShare />}
+        <div className={styles.title_container}>
+          <p>
+            <FaCrown /> 방장 이름
+          </p>
+          <h1 className={styles.room_title}>{room ? room.name : '올빼미를 불러오는 중'}</h1>
+          {/* {room && <LinkShare />} */}
         </div>
-        <ConfirmedButton />
+        <div>
+          <ConfirmedButton />
+        </div>
       </div>
+      <p className={styles.center_address}>중심 위치 주소를 보여줍니다.</p>
     </div>
   );
 };
