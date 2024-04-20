96562131c65da06568a7c89168f4321f1ad4e8d0
heechul94 <yhcu3517@gmail.com>
2024-04-21 02:09:00 +0900

    fix:gcTime Infinity -> 86400000ms 로 변경

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,7 +6,7 @@ import { NextUIProvider } from '@nextui-org/react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function Providers({ children }: React.PropsWithChildren) {
-  const queryClient = new QueryClient({ defaultOptions: { queries: { gcTime: Infinity } } });
+  const queryClient = new QueryClient({ defaultOptions: { queries: { gcTime: 86_400_000 } } });
   return (
     <QueryClientProvider client={queryClient}>
       <NextUIProvider>{children}</NextUIProvider>
