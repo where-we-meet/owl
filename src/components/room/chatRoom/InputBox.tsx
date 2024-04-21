d7aece19ca657f54f553649fb7f473e7359b9f77
기다림 <gidalimforsomeday@gmail.com>
2024-04-22 06:04:36 +0900

    feat: 신규 메시지를 현재 로그인 중인 유저의 이름으로 출력

--- src/components/room/chatRoom/InputBox.tsx
+++ src/components/room/chatRoom/InputBox.tsx
@@ -2,16 +2,42 @@
 
 import { createClient } from '@/utils/supabase/client';
 import styles from './InputBox.module.css';
+import { useQueryUser } from '@/hooks/useQueryUser';
+import { useMessage } from '@/store/messageStore';
+import { useQueryUsersData } from '@/hooks/useQueryUsersData';
 
 export const InputBox = () => {
   const supabase = createClient();
+  const user = useQueryUser();
+  const addMessage = useMessage((state) => state.addMessage);
+  const { userData } = useQueryUsersData(user.id);
+
+  console.log('받아오는ㄷ ㅔ이터', userData);
   const handleSendMessage = async (text: string) => {
-    alert(text);
+    if (text.trim()) {
+      const newMessage = {
+        id: crypto.randomUUID(),
+        text,
+        send_by: user.id,
+        is_edit: false,
+        created_at: new Date().toISOString(),
+        users: {
+          id: userData[0].id,
+          profile_url: userData[0].profile_url,
+          name: userData[0].name,
+          created_at: userData[0].created_at
+        }
+      };
 
-    const { data, error } = await supabase.from('message').insert({ text });
+      addMessage(newMessage);
 
-    if (error) throw error;
-    return data;
+      const { data, error } = await supabase.from('message').insert({ text });
+
+      if (error) throw error;
+      return data;
+    } else {
+      console.error('빈칸 금지관련 에러를 출력합시다');
+    }
   };
 
   return (
