66e7a3b0038c344847a8d62b631686a01737be71
heechul94 <yhcu3517@gmail.com>
2024-04-21 22:44:05 +0900

    feat: 출발 위치 표시 영역 생성

--- src/components/room/sidebar/Sidebar.tsx
+++ src/components/room/sidebar/Sidebar.tsx
@@ -1,10 +1,13 @@
 import Calender from '../calender/Calender';
+
+import StartLocationBox from '../place/StartLocationBox';
 import UserList from './user/UserList';
 
 const Sidebar = () => {
   return (
     <>
       <Calender />
+      <StartLocationBox />
       <UserList />
     </>
   );
