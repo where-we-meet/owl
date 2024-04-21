5640143a1fa5a7c61580863896bfb7bf751ceda4
기다림 <gidalimforsomeday@gmail.com>
2024-04-22 05:26:24 +0900

    feat : message 박스 분리를 위한 컴포넌트 분리

--- src/components/room/chatRoom/ListMessage.tsx
+++ src/components/room/chatRoom/ListMessage.tsx
@@ -1,36 +1,14 @@
-import Image from 'next/image';
-import defaultImage from '../../../../public/images/about_owl_image.jpg';
 import styles from './ListMessage.module.css';
 import { useMessage } from '@/store/messageStore';
+import { Message } from './Message';
 
 export const ListMessage = () => {
   const messages = useMessage((state) => state.messages);
 
   return (
     <section className={styles.container}>
-      {messages.map((item) => {
-        return (
-          <div className={styles.chatbox_body} key={item.id}>
-            <figure className={styles.profiles}>
-              <div>
-                <Image
-                  src={defaultImage}
-                  alt="profile_image"
-                  width={30}
-                  height={30}
-                  style={{ width: 30, height: 30 }}
-                />
-              </div>
-              <div className={styles.content}>
-                <div className={styles.user_info}>
-                  <h3 className={styles.name}>이름</h3>
-                  <h3 className={styles.date_time}>{new Date().toDateString()}</h3>
-                </div>
-                <p className={styles.chat_log}>asdasdasdasdasdasdasdasdasd</p>
-              </div>
-            </figure>
-          </div>
-        );
+      {messages.map((message) => {
+        return <Message key={message.id} message={message} />;
       })}
     </section>
   );
---
+++ src/components/room/chatRoom/Message.tsx
@@ -0,0 +1,28 @@
+import { IMessage } from '@/store/messageStore';
+import Image from 'next/image';
+import styles from './Message.module.css';
+
+export const Message = ({ message }: { message: IMessage }) => {
+  return (
+    <div className={styles.chatbox_body} key={message.id}>
+      <figure className={styles.profiles}>
+        <div>
+          <Image
+            src={`${message.users?.profile_url}`}
+            alt={message.users?.name!}
+            width={40}
+            height={40}
+            style={{ width: 40, height: 40 }}
+          />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.user_info}>
+            <h3 className={styles.name}>{message.users?.name}</h3>
+            <h3 className={styles.date_time}>{new Date(message.created_at).toDateString()}</h3>
+          </div>
+          <p className={styles.chat_log}>{message.text}</p>
+        </div>
+      </figure>
+    </div>
+  );
+};
