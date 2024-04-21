921eeb85e963b578cb45286f58079e856976b913
ketchup0211 <dlcka1111@naver.com>
2024-04-21 17:36:25 +0900

    chore: 불필요한 import 삭제

--- src/components/room/calender/SchedulesOfMe.tsx
+++ src/components/room/calender/SchedulesOfMe.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { isSameDay } from 'date-fns';
 import styles from './SchedulesOfMe.module.css';
-import { UserSchedule } from './EntireOfMonth';
 
 type Props = {
   selectedDate: Date[];
@@ -9,7 +8,6 @@ type Props = {
 };
 const SchedulesOfMe: React.FC<Props> = ({ selectedDate, day }) => {
   let isSelectedDay = selectedDate.some((date) => isSameDay(date, day));
-
   return isSelectedDay ? <span className={styles.selected_date_circle} /> : <></>;
 };
 
