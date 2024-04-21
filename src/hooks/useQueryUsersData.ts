00ffdb011961e0b7d3c196900f257f7e97a45fbe
기다림 <gidalimforsomeday@gmail.com>
2024-04-22 06:03:31 +0900

    feat: 유저 테이블의 정보를 받아오고, 현재 로그인 중인 유저를 걸러내는 커스텀훅을 제작

---
+++ src/hooks/useQueryUsersData.ts
@@ -0,0 +1,10 @@
+import { getUsersData } from '@/api/supabaseCSR/supabase';
+import { useQuery } from '@tanstack/react-query';
+
+export const useQueryUsersData = (userId: string) => {
+  const { data = [], isPending } = useQuery({ queryKey: ['usersTableData'], queryFn: () => getUsersData(userId) });
+
+  const userData = data.filter((user) => user.id === userId);
+
+  return { userData, isPending };
+};
