1fcfa02a977f12cf414533880d12c2b6425d3b52
lauichan <te06230623@gmail.com>
2024-04-22 00:37:44 +0900

    feat: 사이드바 레이아웃, 내가 참여한 모임들, 유저 프로필 등 헤더에 있는 정보 옮기기

--- src/components/room/sidebar/Sidebar.tsx
+++ src/components/room/sidebar/Sidebar.tsx
@@ -1,15 +1,32 @@
+import { Image, Link } from '@nextui-org/react';
 import Calender from '../calender/Calender';
-
 import StartLocationBox from '../place/StartLocationBox';
+import RoomHeader from '../header/RoomHeader';
 import UserList from './user/UserList';
+import MyRooms from './MyRooms';
+import UserProfile from '@/components/header/profile/UserProfileButton';
+import styles from './Sidebar.module.css';
 
 const Sidebar = () => {
   return (
-    <>
-      <Calender />
-      <StartLocationBox />
-      <UserList />
-    </>
+    <aside className={styles.aside}>
+      <nav className={styles.channel}>
+        <Image src="/images/logo.png" />
+        <MyRooms />
+        <div className={styles.profile}>
+          <UserProfile />
+        </div>
+      </nav>
+      <div className={styles.room}>
+        <Link className={styles.brand} color="foreground" href="/">
+          OWL-LiNK
+        </Link>
+        <RoomHeader />
+        <Calender />
+        <StartLocationBox />
+        <UserList />
+      </div>
+    </aside>
   );
 };
 
