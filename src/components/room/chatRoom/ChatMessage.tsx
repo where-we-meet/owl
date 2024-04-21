64e4aba67ff52be91f8881c5d920b97bac017d12
기다림 <gidalimforsomeday@gmail.com>
2024-04-22 06:04:36 +0900

    feat: 신규 메시지를 현재 로그인 중인 유저의 이름으로 출력

--- src/components/room/chatRoom/ChatMessage.tsx
+++ src/components/room/chatRoom/ChatMessage.tsx
@@ -2,12 +2,10 @@
 import { Suspense } from 'react';
 import { ListMessage } from './ListMessage';
 import { InitMessages } from './InitMessages';
-import { createClient } from '@/utils/supabase/client';
 import { getMessageData } from '@/api/supabaseCSR/supabase';
 import { useQueryUser } from '@/hooks/useQueryUser';
 
 export const ChatMessage = async () => {
-  const supabase = createClient();
   const user = useQueryUser();
   const data = await getMessageData(user.id);
 
