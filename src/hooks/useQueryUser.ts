2a472502e06875eb7766a00f5ab0e2c9ce5b92aa
lauichan <te06230623@gmail.com>
2024-04-22 00:30:43 +0900

    fix: getQueryData에서 useQuery를 사용하고 getQueryData를 initialData로 사용하기

--- src/hooks/useQueryUser.ts
+++ src/hooks/useQueryUser.ts
@@ -1,7 +1,15 @@
-import { User } from '@supabase/supabase-js';
-import { useQueryClient } from '@tanstack/react-query';
+import { getSession } from '@/api/auth/getSession';
+import { createClient } from '@/utils/supabase/client';
+import type { User } from '@supabase/supabase-js';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 export const useQueryUser = () => {
+  const supabase = createClient();
   const queryClient = useQueryClient();
-  return queryClient.getQueryData(['auth']) as User;
+  const { data: user } = useQuery({
+    queryKey: ['auth'],
+    queryFn: () => getSession(supabase),
+    initialData: queryClient.getQueryData(['auth']) as User
+  });
+  return user;
 };
