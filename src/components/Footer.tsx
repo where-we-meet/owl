809e3008e510645c5849a48f3f88db3828e1206e
ketchup0211 <dlcka1111@naver.com>
2024-04-22 01:24:53 +0900

    style: footer 스타일 일부 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,15 +4,19 @@ import { FaGithub } from 'react-icons/fa';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <Link href="https://github.com/where-we-meet/owl">
-        <FaGithub />
-        &nbsp;owl-link
-      </Link>
-      <div>
-        <Link href="/about"> about us</Link>
+      <div>이용약관</div>
+      <div className={styles.about_container}>
+        <Link className={styles.github} href="https://github.com/where-we-meet/owl">
+          <FaGithub />
+          &nbsp;owl-link
+        </Link>
+        <Link href="/about">@about us</Link>
       </div>
     </footer>
   );
 };
 
 export default Footer;
+{
+  /* <Link href="/about">@about us</Link> */
+}
