762d5ce17a934ee5feaa5493f88daf9ad431a174
heechul94 <yhcu3517@gmail.com>
2024-04-21 19:37:34 +0900

    fix: queryKey이름, gcTime 5분으로 변경

--- src/hooks/useQueryPlace.ts
+++ src/hooks/useQueryPlace.ts
@@ -4,27 +4,30 @@ import { useQuery } from '@tanstack/react-query';
 
 export const useQueryAddress = (location: { lat: number; lng: number }, isDrag: boolean) => {
   const { data, isPending } = useQuery({
-    queryKey: ['room', location],
+    queryKey: ['address', location],
     queryFn: async () => await getAddress(location),
-    enabled: !!location.lat && !!location.lng && !isDrag
+    enabled: !!location.lat && !!location.lng && !isDrag,
+    gcTime: 300_000
   });
   return { data, isPending };
 };
 
 export const useQuerySearchPlace = (searchKeyword: string) => {
   const { data, isPending } = useQuery({
-    queryKey: ['room', searchKeyword],
+    queryKey: ['search-keywoard', searchKeyword],
     queryFn: async () => await getSearchPlace(searchKeyword, null),
-    enabled: !!searchKeyword
+    enabled: !!searchKeyword,
+    gcTime: 300_000
   });
   return { data, isPending };
 };
 
 export const useQuerySearchCategory = (searchOption: SearchOptionData) => {
   const { data, isPending } = useQuery<Place[]>({
-    queryKey: ['room', searchOption],
+    queryKey: ['search-category', searchOption],
     queryFn: async () => await getSearchPlace('', searchOption),
-    enabled: !!searchOption
+    enabled: !!searchOption,
+    gcTime: 300_000
   });
   return { data, isPending };
 };
