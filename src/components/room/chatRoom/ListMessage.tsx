2ec5b3a2940c0de344cd9b6f20504bc8d9e8e621
기다림 <gidalimforsomeday@gmail.com>
2024-04-22 23:59:16 +0900

    feat : realtime 로직을 추가

--- src/components/room/chatRoom/ListMessage.tsx
+++ src/components/room/chatRoom/ListMessage.tsx
@@ -1,9 +1,31 @@
+'use client';
 import styles from './ListMessage.module.css';
-import { useMessage } from '@/store/messageStore';
+import { IMessage, useMessage } from '@/store/messageStore';
 import { Message } from './Message';
+import { useEffect } from 'react';
+import { createClient } from '@/utils/supabase/client';
 
-export const ListMessage = () => {
-  const messages = useMessage((state) => state.messages);
+export const ListMessage = ({ roomId }: { roomId: string }) => {
+  const supabase = createClient();
+  const { messages, addMessage } = useMessage((state) => state);
+
+  useEffect(() => {
+    const channel = supabase
+      .channel('chatMessage')
+      .on(
+        'postgres_changes',
+        { event: 'INSERT', schema: 'public', table: 'message', filter: `room_id=eq.${roomId}` },
+        (payload) => {
+          const newMessage = payload.new as IMessage;
+          addMessage(newMessage);
+        }
+      )
+      .subscribe();
+
+    return () => {
+      channel.unsubscribe();
+    };
+  }, [supabase]);
 
   return (
     <section className={styles.container}>
