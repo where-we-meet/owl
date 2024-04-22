7ce5a7af05498f7b67937bd8b89a3ba6a59fc5f8
lauichan <te06230623@gmail.com>
2024-04-22 20:12:15 +0900

    style: 사이드바 슬라이드 효과 열고 닫기 토글 기능

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -3,16 +3,19 @@
 import SettingMap from '@/components/room/place/SettingMap';
 import Sidebar from '@/components/room/sidebar/Sidebar';
 import { useQueryUser } from '@/hooks/useQueryUser';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import { getRoomIsConfirmed } from '@/api/room';
 import styles from './page.module.css';
+import ToggleSidebar from '@/components/room/sidebar/ToggleSidebar';
 
 const SettingPage = () => {
   const router = useRouter();
   const { id: userId } = useQueryUser();
   const { id: roomId }: { id: string } = useParams();
+  const [isOpened, setIsOpened] = useState(true);
+
   const { data: room } = useQuery({
     queryKey: ['room', 'confirmed', 'createdBy'],
     queryFn: () => getRoomIsConfirmed(roomId),
@@ -25,11 +28,19 @@ const SettingPage = () => {
     }
   }, [room?.verified]);
 
+  const toggleSidebar = () => {
+    setIsOpened((prev) => !prev);
+  };
+
   return (
-    <div className={styles.wrapper}>
-      <Sidebar />
+    <div className={`${styles.wrapper} ${isOpened ? styles.opened : ''}`}>
+      <aside className={styles.aside}>
+        <Sidebar />
+        <ToggleSidebar toggleSidebar={toggleSidebar} />
+      </aside>
+
       <section className={styles.map}>
-        <SettingMap />
+        <SettingMap isOpened={isOpened} />
       </section>
     </div>
   );
