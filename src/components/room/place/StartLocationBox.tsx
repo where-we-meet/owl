f910b1e24f7fa76fe0ddf38cda0d3cbb4c4240b5
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-23 01:05:17 +0900

    fix: 안내문구 삭제

--- src/components/room/place/StartLocationBox.tsx
+++ src/components/room/place/StartLocationBox.tsx
@@ -2,24 +2,8 @@ import { useSearchDataStore } from '@/store/placeStore';
 import { useRoomUserDataStore } from '@/store/roomUserStore';
 import LocationSwitch from './LocationSwitch';
 import styles from './StartLocationBox.module.css';
-import { useEffect, useRef, useState } from 'react';
 
 const StartLocationBox = () => {
-  const [isDialogVisible, setIsDialogVisible] = useState(true);
-
-  const dialogRef = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (dialogRef.current && !dialogRef.current.contains(event.target as Node)) {
-        setIsDialogVisible(false);
-      }
-    };
-    if (isDialogVisible) {
-      document.addEventListener('click', handleClickOutside);
-    }
-  }, [isDialogVisible]);
-
   const roomUser = useRoomUserDataStore((state) => state.roomUser);
   const address = useSearchDataStore((state) => state.address);
 
@@ -27,11 +11,6 @@ const StartLocationBox = () => {
 
   return (
     <div className={styles.start_location_container}>
-      {isDialogVisible && (
-        <div ref={dialogRef} className={styles.label}>
-          핀을 눌러 출발 위치를 확정해주세요.
-        </div>
-      )}
       <div className={styles.wrapper}>
         <div className={`${styles.box} ${isPinned && styles.selected}  `}>
           {isPinned ? roomUser.start_location : address}
