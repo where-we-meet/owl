a677dbd4d075e4a88750a6bbc0ada2921e820ade
기다림 <gidalimforsomeday@gmail.com>
2024-04-22 23:58:57 +0900

    feat: 데이터를 출력하는 로직에서 roomId와 userId를 출력하는 부분을 수정

--- src/components/room/chatRoom/Message.tsx
+++ src/components/room/chatRoom/Message.tsx
@@ -1,4 +1,7 @@
+'use client';
 import { IMessage } from '@/store/messageStore';
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownSection, DropdownItem, Button } from '@nextui-org/react';
+import { IoIosMore } from 'react-icons/io';
 import styles from './Message.module.css';
 
 export const Message = ({ message }: { message: IMessage }) => {
@@ -15,13 +18,34 @@ export const Message = ({ message }: { message: IMessage }) => {
           />
         </div>
         <div className={styles.content}>
-          <div className={styles.user_info}>
-            <h3 className={styles.name}>{message.users?.name}</h3>
-            <h3 className={styles.date_time}>{new Date(message.created_at).toDateString()}</h3>
+          <div className={styles.chat_info}>
+            <div className={styles.user_info}>
+              <h3 className={styles.name}>{message.users?.name}</h3>
+              <h3 className={styles.date_time}>{new Date(message.created_at).toDateString()}</h3>
+            </div>
+            <MessageMenu />
           </div>
           <p className={styles.chat_log}>{message.text}</p>
         </div>
       </figure>
     </div>
+  );
+};
+
+export const MessageMenu = () => {
+  return (
+    <Dropdown>
+      <DropdownTrigger>
+        <Button variant="bordered">
+          <IoIosMore />
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Static Actions">
+        <DropdownItem key="edit">수정</DropdownItem>
+        <DropdownItem key="delete" className="text-danger" color="danger">
+          삭제
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
   );
 };
