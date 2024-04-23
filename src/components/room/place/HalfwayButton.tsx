8ca9dd72d68d09ad06933a3afd2b7e9e0e5b811c
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-23 19:39:31 +0900

    style: 중심 위치 주소 문구 위치 변경 및 아이콘 추가

--- src/components/room/place/HalfwayButton.tsx
+++ src/components/room/place/HalfwayButton.tsx
@@ -1,5 +1,7 @@
 import { useHalfwayDataStore } from '@/store/halfwayStore';
 import { useSearchDataStore } from '@/store/placeStore';
+import { IoMdPin } from 'react-icons/io';
+import styles from './HalfwayButton.module.css';
 
 const HalfwayButton = () => {
   const setLocation = useSearchDataStore((state) => state.setLocation);
@@ -12,7 +14,9 @@ const HalfwayButton = () => {
 
   return (
     <>
-      <button onClick={moveToCenter}>중심지점으로 이동</button>
+      <button className={styles.center_address} title="중심 위치로 이동한다올" onClick={moveToCenter}>
+        <IoMdPin /> 중심 위치 주소
+      </button>
     </>
   );
 };
