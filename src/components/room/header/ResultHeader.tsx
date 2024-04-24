6fe4ddc8c61bdf82e9383a7d6ad22b3f258a9266
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-24 20:41:40 +0900

    feat: 공유하기, 프로필 버튼 연결

--- src/components/room/header/ResultHeader.tsx
+++ src/components/room/header/ResultHeader.tsx
@@ -1,8 +1,12 @@
 'use client';
+
 import { useMapController } from '@/hooks/useMapController';
 import { useQuery } from '@tanstack/react-query';
 import { getRoomData } from '@/api/supabaseCSR/supabase';
+import UserProfile from '@/components/header/profile/UserProfileButton';
+import LinkShare from '../share/LinkShare';
 import styles from './ResultHeader.module.css';
+import { Image } from '@nextui-org/react';
 
 const ResultHeader = ({ roomId }: { roomId: string }) => {
   const { data: room } = useQuery({ queryKey: ['room', roomId], queryFn: () => getRoomData(roomId) });
@@ -13,9 +17,15 @@ const ResultHeader = ({ roomId }: { roomId: string }) => {
       <div className={styles.box}>
         <div className={styles.title_container}>
           <h1 className={styles.room_title}>{room ? room.name : '올빼미를 불러오는 중'}</h1>
+          <div className={styles.wrap}>
+            <LinkShare />
+            <UserProfile />
+          </div>
         </div>
-        <div className={styles.address}>
+        <div className={styles.confirm_info}>
+          <Image src="/pin.svg" className={styles.pin} />
           <p>{address}</p>
+          <span>/</span>
           <p>{room ? room.confirmed_date : '확정 날짜를 불러오는 중'}</p>
         </div>
       </div>
