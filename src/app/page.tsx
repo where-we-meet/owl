bd527e7e64fa278ca975ee9ef66a4c59bd4f24f5
ketchup0211 <dlcka1111@naver.com>
2024-04-24 16:24:39 +0900

    chore: 사진 변경해서 삽입 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,17 +31,17 @@ export default function Home() {
         </section>
         <section className={styles.section}>
           <Article
-            image_url="images/manage_schedule2.png"
+            image_url="images/landing_first.png"
             subtitle="빠르게 모임 생성하기"
             contents={`OWL-LiNK를 통해 모임 일정을 잡기 위한 방 개설을 손쉽게 해보세요. [모임 시작하기]를 누르고, 모임 일정 [시작일]과 [종료일]을 설정하면 완료! 터치 몇 번으로 모임 방을 만들 수 있어요.`}
           />
           <Article
-            image_url="images/manage_schedule2.png"
+            image_url="images/landing_second.png"
             subtitle="신속한 일정과 위치 전달 UI"
             contents={`시시각각 변하는 개인의 일정과 장소를 일일이 조사, 보고하느라 번거롭지는 않으신가요? OWL-LiNK는 합리적이고 직관적인 실시간 UI를 통해, 누구에게나 편한 정보 교환 커뮤니케이션을 제공합니다.`}
           />
           <Article
-            image_url="images/manage_schedule2.png"
+            image_url="images/landing_third.png"
             subtitle="최적의 모임 장소 제공 & 주변 인프라 조회"
             contents={`OWL-LiNK는 모임에 참가한 유저들의 출발 위치에 기반하여 최적의 모임 장소를 선정합니다. 또한, 주변 인프라 정보를 제공해주어 가볼만한 장소들을 쉽게 알아볼 수 있어요.`}
           />
