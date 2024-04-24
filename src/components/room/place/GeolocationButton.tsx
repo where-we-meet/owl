861e48d015eb0656e23b6ff964781431027cb2f2
heechul94 <yhcu3517@gmail.com>
2024-04-24 23:24:21 +0900

    feat: 알림 모달 추가

--- src/components/room/place/GeolocationButton.tsx
+++ src/components/room/place/GeolocationButton.tsx
@@ -2,13 +2,18 @@ import { useGeoLocation } from '@/hooks/useGeoLocation';
 
 import { MdGpsFixed } from 'react-icons/md';
 import styles from './GeolocationButton.module.css';
+import AlertModal from './AlertModal';
 
 const GeolocationButton = () => {
-  const { handleSetGeolocation } = useGeoLocation();
+  const { isOpen, onOpenChange, handleSetGeolocation } = useGeoLocation();
   return (
-    <div className={styles.wrapper} onClick={handleSetGeolocation}>
-      <MdGpsFixed size={25} />
-    </div>
+    <>
+      <div className={styles.wrapper} onClick={handleSetGeolocation}>
+        <MdGpsFixed size={25} />
+      </div>
+
+      <AlertModal isOpen={isOpen} onOpenChange={onOpenChange} />
+    </>
   );
 };
 
