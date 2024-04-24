19ae722b2f67dca1854b05f997351bdab117e366
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-24 20:53:10 +0900

    chore: 미사용 코드 삭제, import 순서 변경

--- src/app/room/[id]/confirm/page.tsx
+++ src/app/room/[id]/confirm/page.tsx
@@ -1,13 +1,13 @@
 import ResultMap from '@/components/room/place/ResultMap';
 import ResultHeader from '@/components/room/header/ResultHeader';
-import styles from './page.module.css';
 import { ChatRoom } from '@/components/room/chatRoom/ChatRoom';
+import styles from './page.module.css';
 
 const RoomPage = ({ params }: { params: { id: string } }) => {
   return (
     <div className={styles.container}>
       <ResultHeader roomId={params.id} />
-      <ResultMap roomId={params.id} />
+      <ResultMap />
       <ChatRoom roomId={params.id} />
     </div>
   );
