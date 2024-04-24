aee94e401e071d57b3c677175d9e12466021e2f4
heechul94 <yhcu3517@gmail.com>
2024-04-24 23:00:04 +0900

    fix: 버튼 눌렀을 때만 실행 되도록 변경

--- src/hooks/useGeoLocation.ts
+++ src/hooks/useGeoLocation.ts
@@ -1,41 +1,26 @@
-import { useEffect } from 'react';
 import { useGpsStatusStore, useSearchDataStore } from '@/store/placeStore';
-import { useRoomUserDataStore } from '@/store/roomUserStore';
-import { myGeolocation } from '@/utils/place/myGeolocation';
+import { GeolocationResult, myGeolocation } from '@/utils/place/myGeolocation';
 
 export const useGeoLocation = () => {
   const setLocation = useSearchDataStore((state) => state.setLocation);
   const { setIsGpsLoading, setErrorMessage } = useGpsStatusStore((state) => state);
-  const roomUser = useRoomUserDataStore((state) => state.roomUser);
 
   const handleSetGeolocation = async () => {
-    setIsGpsLoading(true);
-    const {
-      lat,
-      lng,
-      status,
-      errorMessage
-    }: { lat: number | null; lng: number | null; status: boolean; errorMessage: string | null } = await myGeolocation();
-    if (lat && lng) {
-      setLocation({ lat, lng });
-      setIsGpsLoading(status);
-    } else if (errorMessage) {
-      setErrorMessage(errorMessage);
-      setIsGpsLoading(status);
+    try {
+      setIsGpsLoading(true);
+      const { lat, lng, status, errorMessage }: GeolocationResult = await myGeolocation();
+      if (lat && lng) {
+        setLocation({ lat, lng });
+        setIsGpsLoading(status);
+      } else if (errorMessage) {
+        alert('현재 위치를 가져오려면 위치 권한을 승인해주세요.');
+        setErrorMessage(errorMessage);
+        setIsGpsLoading(status);
+      }
+    } catch (error) {
+      console.error(error);
     }
   };
-
-  useEffect(() => {
-    if (roomUser) {
-      if (roomUser.start_location !== '' && roomUser.start_location !== null) {
-        setIsGpsLoading(false);
-      } else {
-        handleSetGeolocation();
-      }
-    } else {
-      handleSetGeolocation();
-    }
-  }, []);
 
   return { handleSetGeolocation };
 };
