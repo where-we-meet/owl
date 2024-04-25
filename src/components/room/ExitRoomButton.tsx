fe30e9014530651057f0394e2b2f77b36c0c189f
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-25 22:30:18 +0900

    style: confim창 modal창으로 변경

--- src/components/room/ExitRoomButton.tsx
+++ src/components/room/ExitRoomButton.tsx
@@ -4,12 +4,15 @@ import { useQueryUser } from '@/hooks/useQueryUser';
 import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 import { ImExit } from 'react-icons/im';
+import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from '@nextui-org/react';
+import styles from './ExitRoomButton.module.css';
 
 export const ExitRoomButton = ({ roomId }: { roomId: string }) => {
   const { id: userId } = useQueryUser();
   const router = useRouter();
   const { mutateAsync: deleteExitUser } = useDeleteExitUser(roomId);
   const { mutateAsync: deleteRoom } = useDeleteRoom();
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
 
   const { data: room, isPending } = useQuery({
     queryKey: ['room', 'confirmed', 'createdBy'],
@@ -17,21 +20,43 @@ export const ExitRoomButton = ({ roomId }: { roomId: string }) => {
     select: (data) => data[0]
   });
 
-  const handleDeleteUser = () => {
+  const handleDeleteUser = async () => {
     if (room?.created_by !== userId) {
       deleteExitUser({ roomId, userId });
       router.push('/');
     } else {
-      confirm('정말 나가시겠어요? 방장이 나가면 이 모임방은 삭제됩니다.');
       deleteRoom({ roomId, userId });
       router.push('/');
     }
   };
+
   return (
     <>
-      <button onClick={handleDeleteUser}>
+      <Button size="sm" onPress={onOpen} variant="light" className={styles.exit_button}>
         <ImExit />
-      </button>
+      </Button>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader>확인</ModalHeader>
+              <ModalBody>
+                {room?.created_by !== userId
+                  ? '정말 나가시겠어요? 선택한 일정과 출발 위치가 삭제됩니다.'
+                  : '정말 나가시겠어요? 방장이 나가면 이 모임방은 삭제됩니다.'}
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  취소
+                </Button>
+                <Button type="submit" color="primary" onPress={handleDeleteUser}>
+                  확인
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </>
   );
 };
