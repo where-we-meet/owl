3df2614a99330499cf0d34e39d445b039da4a0d4
lauichan <te06230623@gmail.com>
2024-04-26 06:28:53 +0900

    feat: 개발자 모드에서 PWA 비활성화

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,8 @@ const withPWA = withPWAInit({
   dest: 'public',
   fallbacks: {
     document: '/~offline'
-  }
+  },
+  disable: process.env.NODE_ENV === 'development'
 });
 
 /** @type {import('next').NextConfig} */
