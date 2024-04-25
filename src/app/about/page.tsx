85bc3638909131d86cc5313c47f57e68072d0531
ketchup0211 <dlcka1111@naver.com>
2024-04-26 02:48:52 +0900

    chore: 컴포넌트화 완료

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,102 +1,30 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import owl_image from '../../../public/images/about_owl_image.jpg';
 import PayPalDonate from '@/components/auth/PayPalDonate';
+import { MemberCard } from '@/components/about/MemberCard';
 import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
-import { FaGithub } from 'react-icons/fa6';
 import styles from './page.module.css';
 
-const DEVELOPERS_INFO = {
-  양희철: 'https://github.com/heechul94',
-  이참: 'https://github.com/ketchup0211',
-  나의찬: 'https://github.com/lauichan',
-  오소향: 'https://github.com/SohyangO',
-  박강토: 'https://github.com/gidalim'
-};
+const DEVELOPERS_INFO = [
+  { name: '양희철', githubURL: 'https://github.com/heechul94' },
+  { name: '이참', githubURL: 'https://github.com/ketchup0211' },
+  { name: '나의찬', githubURL: 'https://github.com/lauichan' },
+  { name: '오소향', githubURL: 'https://github.com/SohyangO' },
+  { name: '박강토', githubURL: 'https://github.com/gidalim' }
+];
 export default function AboutPage() {
   return (
     <>
       <Header />
       <main className={styles.main}>
-        <section className={styles.header_image}>
-          <header className={styles.header}>
-            <h1 className={styles.title}>저희는 개발자 5인으로 구성된 팀 We입니다.</h1>
-          </header>
-        </section>
-        <section className={styles.section}>
-          <div className={styles.profiles}>
-            <figure className={styles.figure}>
-              <Image src={owl_image} width={125} height={125} alt="default image" />
-            </figure>
-            <div className={styles.profiles_contents}>
-              <p>양희철</p>
-              <p>저희는 이번 프로젝트를 통해...</p>
-            </div>
-            <div className={styles.github_link}>
-              <Link href="https://github.com/heechul94">
-                <FaGithub color="#8a2be2" size={45} />
-              </Link>
-            </div>
-          </div>
-          <div className={styles.profiles}>
-            <figure className={styles.figure}>
-              <Image src={owl_image} width={125} height={125} alt="default image" />
-            </figure>
-            <div className={styles.profiles_contents}>
-              <p>이참</p>
-              <p>저희는 이번 프로젝트를 통해...</p>
-            </div>
-            <div className={styles.github_link}>
-              <Link href="https://github.com/ketchup0211">
-                <FaGithub color="#8a2be2" size={45} />
-              </Link>
-            </div>
-          </div>
-          <div className={styles.profiles}>
-            <figure className={styles.figure}>
-              <Image src={owl_image} width={125} height={125} alt="default image" />
-            </figure>
-            <div className={styles.profiles_contents}>
-              <p>나의찬</p>
-              <p>저희는 이번 프로젝트를 통해...</p>
-            </div>
-            <div className={styles.github_link}>
-              <Link href="https://github.com/lauichan">
-                <FaGithub color="#8a2be2" size={45} />
-              </Link>
-            </div>
-          </div>
-          <div className={styles.profiles}>
-            <figure className={styles.figure}>
-              <Image src={owl_image} width={125} height={125} alt="default image" />
-            </figure>
-            <div className={styles.profiles_contents}>
-              <p>오소향</p>
-              <p>저희는 이번 프로젝트를 통해...</p>
-            </div>
-            <div className={styles.github_link}>
-              <Link href="https://github.com/SohyangO">
-                <FaGithub color="#8a2be2" size={45} />
-              </Link>
-            </div>
-          </div>
-          <div className={styles.profiles}>
-            <figure className={styles.figure}>
-              <Image src={owl_image} width={125} height={125} alt="default image" />
-            </figure>
-            <div className={styles.profiles_contents}>
-              <p>박강토</p>
-              <p>저희는 이번 프로젝트를 통해...</p>
-            </div>
-            <div className={styles.github_link}>
-              <Link href="https://github.com/gidalim">
-                <FaGithub color="#8a2be2" size={45} />
-              </Link>
-            </div>
-          </div>
-        </section>
+        <header className={styles.header}>
+          <h1 className={styles.title}>저희는 개발자 5인으로 구성된 팀 We입니다.</h1>
+        </header>
         <PayPalDonate />
+        <section className={styles.section}>
+          {DEVELOPERS_INFO.map((developer, index) => (
+            <MemberCard key={index} name={developer.name} githubURL={developer.githubURL} />
+          ))}
+        </section>
       </main>
       <Footer />
     </>
--- src/components/about/MemberCard.tsx
+++ src/components/about/MemberCard.tsx
@@ -3,18 +3,18 @@ import Link from 'next/link';
 import styles from './MemberCard.module.css';
 import { FaGithub } from 'react-icons/fa6';
 
-export const MemberCard = ({ name, githubLink }: { name: string; githubLink: string }) => {
+export const MemberCard = ({ name, githubURL }: { name: string; githubURL: string }) => {
   return (
     <div className={styles.profiles}>
       <figure className={styles.figure}>
-        <Image src="images/about_owl_image.jpg" width={125} height={125} alt="default image" />
+        <Image src="/images/about_owl_image.jpg" width={125} height={125} alt="default image" />
       </figure>
       <div className={styles.profiles_contents}>
-        <p>양희철</p>
+        <p>{name}</p>
         <p>저희는 이번 프로젝트를 통해...</p>
       </div>
       <div className={styles.github_link}>
-        <Link href="https://github.com/heechul94">
+        <Link href={githubURL}>
           <FaGithub color="#8a2be2" size={45} />
         </Link>
       </div>
