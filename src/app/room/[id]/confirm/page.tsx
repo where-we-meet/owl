73f3547538526add01c5b902bda173fd87c6f448
heechul94 <yhcu3517@gmail.com>
2024-04-25 18:07:53 +0900

    style: page 구조 재 설정

--- src/app/room/[id]/confirm/page.tsx
+++ src/app/room/[id]/confirm/page.tsx
@@ -2,11 +2,15 @@ import ResultMap from '@/components/room/place/ResultMap';
 import ResultHeader from '@/components/room/header/ResultHeader';
 import { ChatRoom } from '@/components/room/chatRoom/ChatRoom';
 import styles from './page.module.css';
+import CategorySelector from '@/components/room/place/search/CategorySelector';
 
 const RoomPage = ({ params }: { params: { id: string } }) => {
   return (
     <div className={styles.container}>
-      <ResultHeader roomId={params.id} />
+      <div className={styles.header_container}>
+        <ResultHeader roomId={params.id} />
+        <CategorySelector />
+      </div>
       <ResultMap />
       <ChatRoom roomId={params.id} />
     </div>
