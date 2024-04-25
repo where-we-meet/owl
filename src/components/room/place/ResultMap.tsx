8eeeb8cd2ce4b8132ed2095027711c8b72173fad
heechul94 <yhcu3517@gmail.com>
2024-04-25 18:04:13 +0900

    fix: 무분별한 fragment 제거

--- src/components/room/place/ResultMap.tsx
+++ src/components/room/place/ResultMap.tsx
@@ -6,7 +6,6 @@ import { useMapController } from '@/hooks/useMapController';
 
 import UserMarker from './UserMarker';
 import Halfway from './Halfway';
-import CategorySelector from './search/CategorySelector';
 import CategoryMarker from './search/CategoryMarker';
 
 import styles from './ResultMap.module.css';
@@ -23,44 +22,35 @@ const ResultMap = () => {
 
   return (
     <>
-      <div className={styles.map_container}>
-        <div className={styles.category_container}>
-          <CategorySelector />
-        </div>
+      {isHalfwayValid && (
+        <Map
+          center={halfwayPoint}
+          className={styles.map}
+          onClick={() => setClickId('')}
+          onTouchEnd={() => setClickId('')}
+        >
+          {roomUsers
+            .filter(({ lat, lng }) => lat !== null && lng !== null)
+            .map(({ id, lat, lng, users }) => (
+              <UserMarker key={id} id={id} lat={lat as string} lng={lng as string} user={users} />
+            ))}
+          {searchCategory.map((info) => (
+            <CategoryMarker key={info.id} info={info} clickId={clickId} setClickId={setClickId} />
+          ))}
 
-        {isHalfwayValid && (
-          <>
-            <Map
-              center={halfwayPoint}
-              className={styles.map}
-              onClick={() => setClickId('')}
-              onTouchEnd={() => setClickId('')}
-            >
-              {roomUsers
-                .filter(({ lat, lng }) => lat !== null && lng !== null)
-                .map(({ id, lat, lng, users }) => (
-                  <UserMarker key={id} id={id} lat={lat as string} lng={lng as string} user={users} />
-                ))}
-              {searchCategory.map((info) => (
-                <CategoryMarker key={info.id} info={info} clickId={clickId} setClickId={setClickId} />
-              ))}
-              <>
-                <Halfway location={halfwayPoint} />
-                <Circle
-                  center={halfwayPoint}
-                  radius={range as number}
-                  strokeWeight={3}
-                  strokeColor={'#000000'}
-                  strokeOpacity={0.1}
-                  strokeStyle={'solid'}
-                  fillColor={'#00a0e9'}
-                  fillOpacity={0.05}
-                />
-              </>
-            </Map>
-          </>
-        )}
-      </div>
+          <Halfway location={halfwayPoint} />
+          <Circle
+            center={halfwayPoint}
+            radius={range as number}
+            strokeWeight={3}
+            strokeColor={'#000000'}
+            strokeOpacity={0.1}
+            strokeStyle={'solid'}
+            fillColor={'#00a0e9'}
+            fillOpacity={0.05}
+          />
+        </Map>
+      )}
     </>
   );
 };
