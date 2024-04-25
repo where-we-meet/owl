cdf6d2b2a8800fd8213dba247158f638acf174dd
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-25 18:08:55 +0900

    feat: 방장이 방을 나가면 방이 삭제됨

--- src/components/room/ExitRoomButton.tsx
+++ src/components/room/ExitRoomButton.tsx
@@ -1,16 +1,31 @@
-import { useDeleteExitUser } from '@/hooks/useMutateUserData';
+import { getIsAdmin } from '@/api/room';
+import { useDeleteExitUser, useDeleteRoom } from '@/hooks/useMutateUserData';
 import { useQueryUser } from '@/hooks/useQueryUser';
+import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 import { ImExit } from 'react-icons/im';
 
 export const ExitRoomButton = ({ roomId }: { roomId: string }) => {
   const { id: userId } = useQueryUser();
   const router = useRouter();
-  const { mutateAsync, isSuccess } = useDeleteExitUser(roomId);
+  const { mutateAsync: deleteExitUser } = useDeleteExitUser(roomId);
+  const { mutateAsync: deleteRoom } = useDeleteRoom();
+
+  const { data: room, isPending } = useQuery({
+    queryKey: ['room', 'confirmed', 'createdBy'],
+    queryFn: () => getIsAdmin(roomId, userId),
+    select: (data) => data[0]
+  });
 
   const handleDeleteUser = () => {
-    mutateAsync({ roomId, userId });
-    router.push('/');
+    if (room?.created_by !== userId) {
+      deleteExitUser({ roomId, userId });
+      router.push('/');
+    } else {
+      confirm('정말 나가시겠어요? 방장이 나가면 이 모임방은 삭제됩니다.');
+      deleteRoom({ roomId, userId });
+      router.push('/');
+    }
   };
   return (
     <>
