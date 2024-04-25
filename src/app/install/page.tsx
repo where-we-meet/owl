d49bca547dc43a1935d5a1ea526140518578dc11
ketchup0211 <dlcka1111@naver.com>
2024-04-25 22:59:01 +0900

    style: 설치 가이드라인 추가

--- src/app/install/page.tsx
+++ src/app/install/page.tsx
@@ -29,6 +29,13 @@ const InstallPage = () => {
             <img src="images/real_logo_with_text.png" alt="OWL-LiNK logo" width={100} />
             <InstallAppButton />
           </span>
+          <p className={styles.main_text}> 이제 앱으로 편하게 이용하세요!</p>
+          <span className={styles.install_description_container}>
+            <p className={styles.install_description}>
+              [앱 설치하기] 버튼을 누르면 아래와 같이 다운로드 확인 창이 뜹니다.
+            </p>
+            <img className={styles.install_img} src="images/install_app.png" alt="install" width={300} />
+          </span>
         </div>
       </section>
       <section className={styles.section}>
