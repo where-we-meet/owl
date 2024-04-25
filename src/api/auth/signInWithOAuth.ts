c73d4335d51f20c7c7b523f714c6f0b4b470d758
lauichan <te06230623@gmail.com>
2024-04-25 16:10:59 +0900

    feat: 로그인 이전 페이지로 리다이렉트 하기

--- src/api/auth/signInWithOAuth.ts
+++ src/api/auth/signInWithOAuth.ts
@@ -1,12 +1,12 @@
 import { getCallbackURL } from '@/utils/getCallbackURL';
 import { createClient } from '@/utils/supabase/client';
 
-export const signInWithOAuth = async (provider: 'google' | 'kakao') => {
+export const signInWithOAuth = async (provider: 'google' | 'kakao', next: string) => {
   const supabase = createClient();
   await supabase.auth.signInWithOAuth({
     provider,
     options: {
-      redirectTo: getCallbackURL()
+      redirectTo: getCallbackURL(next)
     }
   });
 };
