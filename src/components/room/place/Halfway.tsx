9aac7586ca98df549d1c8e75afe6f4670b39ebda
heechul94 <yhcu3517@gmail.com>
2024-04-25 14:32:47 +0900

    style: halway marker z-index 수정

--- src/components/room/place/Halfway.tsx
+++ src/components/room/place/Halfway.tsx
@@ -1,18 +1,13 @@
-import { MapMarker } from 'react-kakao-maps-sdk';
+import { CustomOverlayMap, MapMarker } from 'react-kakao-maps-sdk';
+import styles from './Halfway.module.css';
 
 const Halfway = ({ location }: { location: { lat: number; lng: number } }) => {
   return (
-    <MapMarker
-      key={'halfway'}
-      position={location}
-      image={{
-        src: '/images/center_owl.png',
-        size: {
-          width: 80,
-          height: 70
-        }
-      }}
-    />
+    <CustomOverlayMap position={location} yAnchor={1}>
+      <div className={styles.halfway_container}>
+        <img className={styles.marker} src="/images/center_owl.png" alt="user-avatar" />
+      </div>
+    </CustomOverlayMap>
   );
 };
 
