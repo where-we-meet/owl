6fb21fba6281def279b17a841f20aa51be90826f
lauichan <te06230623@gmail.com>
2024-04-25 16:23:48 +0900

    feat: 로그인 버튼에 리다이렉트 주소 가져오기

--- src/components/auth/LoginButton.tsx
+++ src/components/auth/LoginButton.tsx
@@ -4,15 +4,19 @@ import KakaoSymbol from './KakaoSymbol';
 import GoogleLogo from './GoogleLogo';
 import styles from './LoginButton.module.css';
 import { signInWithOAuth } from '@/api/auth/signInWithOAuth';
+import { useSearchParams } from 'next/navigation';
 
 const LoginButton = () => {
+  const params = useSearchParams();
+  const next = params.get('next') ?? '/';
+
   return (
     <form className={styles.form}>
-      <button className={styles.kakao} onClick={() => signInWithOAuth('kakao')} role="button">
+      <button className={styles.kakao} onClick={() => signInWithOAuth('kakao', next)} role="button">
         <KakaoSymbol />
         <span>카카오 로그인</span>
       </button>
-      <button className={styles.google} onClick={() => signInWithOAuth('google')} role="button">
+      <button className={styles.google} onClick={() => signInWithOAuth('google', next)} role="button">
         <GoogleLogo />
         <span>구글 로그인</span>
       </button>
