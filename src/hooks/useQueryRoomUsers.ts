fa2043542016a1ce1aaa36adb295579e5a2b1f7c
lauichan <te06230623@gmail.com>
2024-04-26 06:46:06 +0900

    refactor: 모임에 있는 유저 가져오는 로직 수정

--- src/hooks/useQueryRoomUsers.ts
+++ src/hooks/useQueryRoomUsers.ts
@@ -1,16 +1,32 @@
-import { getRoomUsersData } from '@/api/supabaseCSR/supabase';
 import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'next/navigation';
+import { getRoomUsersData, getRoomUsersProfile } from '@/api/supabaseCSR/supabase';
+import { mergeRoomUsersById } from '@/utils/mergeRoomUsersById';
+import { useQueryUser } from './useQueryUser';
 
-export const useQueryRoomUsers = (roomId: string, userId: string) => {
+export const useQueryRoomUsers = () => {
+  const { id: roomId }: { id: string } = useParams();
+  const { id: userId } = useQueryUser();
+
   const { data = [], isPending } = useQuery({
     queryKey: ['room-users', roomId],
     queryFn: () => getRoomUsersData(roomId)
   });
 
-  const adminUser = data.filter((user) => user.is_admin);
-  const currentUser = data.filter((user) => !user.is_admin && user.user_id === userId);
-  const otherUsers = data.filter((user) => !user.is_admin && user.user_id !== userId);
+  const userIds = data.map((user) => user.user_id);
+
+  const { data: userProfiles = [] } = useQuery({
+    queryKey: ['room-users-profile', roomId],
+    queryFn: () => getRoomUsersProfile(userIds),
+    enabled: userIds.length > 0
+  });
+
+  const userList = mergeRoomUsersById(data, userProfiles);
+
+  const adminUser = userList.filter((user) => user.is_admin);
+  const currentUser = userList.filter((user) => !user.is_admin && user.user_id === userId);
+  const otherUsers = userList.filter((user) => !user.is_admin && user.user_id !== userId);
   const roomUsers = [...adminUser, ...currentUser, ...otherUsers];
 
-  return { roomUsers, isPending };
+  return { roomUsers, currentUser, isPending };
 };
