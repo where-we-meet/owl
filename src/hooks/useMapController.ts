20c7e5a2cd6c5a1c9f8339ba48a16e49c9ab2252
heechul94 <yhcu3517@gmail.com>
2024-04-25 20:08:48 +0900

    fix: 지도 중심 이동하기 기능 수정

--- src/hooks/useMapController.ts
+++ src/hooks/useMapController.ts
@@ -1,19 +1,21 @@
 import { useQueryAddress, useQuerySearchCategory } from '@/hooks/useQueryPlace';
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useRangeStore, useSearchDataStore } from '@/store/placeStore';
 import { calcHalfwayPoint } from '@/utils/place/calcHalfwayPoint';
 import { useParams } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
 import { getRoomUsersData } from '@/api/supabaseCSR/supabase';
 import { useHalfwayDataStore } from '@/store/halfwayStore';
+import { debounce } from 'lodash';
 
 export const useMapController = () => {
   const { id: roomId }: { id: string } = useParams();
 
+  const [isDrag, setIsDrag] = useState(false);
   const [address, setAddress] = useState('');
   const [clickId, setClickId] = useState('');
 
-  const { searchOption } = useSearchDataStore((state) => state);
+  const { location, setLocation, searchOption } = useSearchDataStore((state) => state);
   const range = useRangeStore((state) => state.range);
   const updateHalfwayData = useHalfwayDataStore((state) => state.updateHalfwayData);
 
@@ -26,9 +28,17 @@ export const useMapController = () => {
   const halfwayPoint = useMemo(() => calcHalfwayPoint(roomUsers), [roomUsers]);
   const { data } = useQueryAddress(halfwayPoint as { lat: number; lng: number }, false);
 
+  const setCenter = (map: kakao.maps.Map) => {
+    const latlng = map.getCenter();
+    setLocation({ lat: latlng.getLat(), lng: latlng.getLng() });
+  };
+
+  const handleChangeCenter = useCallback(debounce(setCenter, 200), []);
+
   useEffect(() => {
-    if (halfwayPoint) {
+    if (halfwayPoint.lat && halfwayPoint.lng) {
       updateHalfwayData({ lat: halfwayPoint.lat, lng: halfwayPoint.lng });
+      setLocation({ lat: halfwayPoint.lat, lng: halfwayPoint.lng });
     }
   }, [halfwayPoint]);
 
@@ -41,12 +51,16 @@ export const useMapController = () => {
   }, [data]);
 
   return {
+    location,
     range,
     address,
     halfwayPoint,
     roomUsers,
     searchCategory,
     clickId,
-    setClickId
+    setClickId,
+    isDrag,
+    setIsDrag,
+    handleChangeCenter
   };
 };
