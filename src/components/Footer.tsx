091effa8a6efed28e5911f376ebfa9861c80aebb
ketchup0211 <dlcka1111@naver.com>
2024-04-25 21:25:39 +0900

    style: footer 이용약관 위치 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,6 @@ import { FaGithub } from 'react-icons/fa';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div>이용약관</div>
       <div className={styles.about_container}>
         <Link className={styles.github} href="https://github.com/where-we-meet/owl">
           <FaGithub />
@@ -12,6 +11,7 @@ const Footer = () => {
         </Link>
         <Link href="/about">@about us</Link>
       </div>
+      <div title="준비중입니다.">이용약관</div>
     </footer>
   );
 };
