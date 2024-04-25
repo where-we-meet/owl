dd17808a9c0ddf0bfd41af4f75af08792ab0652a
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-26 02:06:58 +0900

    fix: 사이드바에서 확정된 방 클릭 시, 현황 페이지로 이동하는 문제 해결

--- src/components/room/sidebar/MyRooms.tsx
+++ src/components/room/sidebar/MyRooms.tsx
@@ -1,16 +1,16 @@
-import { useQueryMyRooms } from '@/hooks/useQueryMyRooms';
+import { MeetingInfo, useQueryMyRooms } from '@/hooks/useQueryMyRooms';
 import { useRouter } from 'next/navigation';
 import { BsPlusSquareDotted } from 'react-icons/bs';
+import { Tooltip } from '@nextui-org/react';
 import Link from 'next/link';
 import styles from './MyRooms.module.css';
-import { Tooltip } from '@nextui-org/react';
 
 const MyRooms = () => {
   const router = useRouter();
   const { myRooms, isPending } = useQueryMyRooms();
 
-  const handleClickRoom = (roomId: string) => {
-    router.push(`/room/${roomId}`);
+  const handleClickRoom = (roomId: string | null, room: MeetingInfo) => {
+    room.verified ? router.push(`/room/${roomId}/confirm`) : router.push(`/room/${roomId}`);
   };
 
   return (
@@ -18,7 +18,7 @@ const MyRooms = () => {
       <ul className={styles.rooms}>
         {myRooms.map((room) => (
           <Tooltip key={room.id} color="foreground" placement="right" content={room.name}>
-            <li className={styles.room} onClick={() => handleClickRoom(room.id)}>
+            <li className={styles.room} onClick={() => handleClickRoom(room.id, room)}>
               {room.name?.substring(0, 1)}
             </li>
           </Tooltip>
