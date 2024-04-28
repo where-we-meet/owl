8ba8689e3b4eb7d314a9117f9370594abd17f1f3
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-28 23:46:38 +0900

    style: 서비스 소개, 프로젝트 후기 박스 추가

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,7 @@ import { MemberCard } from '@/components/about/MemberCard';
 import PayPalDonate from '@/components/auth/PayPalDonate';
 import styles from './page.module.css';
 import Image from 'next/image';
+import { color } from 'framer-motion';
 
 const DEVELOPERS_INFO = [
   { name: '양희철', githubURL: 'https://github.com/heechul94' },
@@ -14,22 +15,56 @@ const DEVELOPERS_INFO = [
 export default function AboutPage() {
   return (
     <>
-      <title>Team OWNLP</title>
+      <title>Team We</title>
       <main className={styles.main}>
-        <header className={styles.header}>
-          <Image src="/images/real_logo_without_text_white.png" alt="logo" width={100} height={100} />
-          <h1 className={styles.title}>We</h1>
-        </header>
-        <div className={styles.divider}>
-          <div className={styles.divider_bar} />
-          <span className={styles.divider_text}>Member & Project</span>
-          <div className={styles.divider_bar} />
+        <div className={styles.container}>
+          <header className={styles.header}>
+            <Image src="/images/real_logo_without_text_white.png" alt="logo" width={100} height={100} />
+            <h1 className={styles.title}>We</h1>
+          </header>
+          <div className={styles.divider}>
+            <div className={styles.divider_bar} />
+            <span className={styles.divider_text}>Member & Project</span>
+            <div className={styles.divider_bar} />
+          </div>
+          <div className={styles.section}>
+            {DEVELOPERS_INFO.map((developer, index) => (
+              <MemberCard key={index} name={developer.name} githubURL={developer.githubURL} />
+            ))}
+          </div>
+          <div className={styles.content_container}>
+            <div className={styles.content_wrap}>
+              <div className={styles.owl_link_produce_container}>
+                <div className={styles.owl_link_img_box}>
+                  <Image src="/images/center_owl.png" alt="logo" width={100} height={100} />
+                </div>
+                <div className={styles.owl_link_content_box}>
+                  <span className={styles.title}>OWL-LiNK</span>
+                  <div className={styles.button_wrap}>
+                    <div className={styles.link_button}>
+                      약속을 잡을 때 결정사항과 고민거리를 줄여주어 편리하고, 더 나아가 다음 모임을 기대하게 만드는
+                      서비스를 제공하고자 합니다.
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.review_card}>
+                <div className={styles.review_card_wrap}>
+                  <div className={styles.profile_image_box}>
+                    <img src="" className={styles.profile_image}></img>
+                  </div>
+                  <div className={styles.review_box}>
+                    <span className={styles.review_title}>
+                      좋은 팀원분들과 함께 할 수 있어서 많이 배우는 시간이었습니다.
+                    </span>
+                    <span className={styles.review_content}>올빼미, Team We</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={styles.section}>
-          {DEVELOPERS_INFO.map((developer, index) => (
-            <MemberCard key={index} name={developer.name} githubURL={developer.githubURL} />
-          ))}
-        </div>
+
         <PayPalDonate />
       </main>
     </>
