8993c05e9d445eb76726af8a2524963538512f6a
lauichan <te06230623@gmail.com>
2024-04-29 22:38:29 +0900

    remove: 중복 쿼리 제거

--- src/hooks/useQueryUsersData.ts
+++
@@ -1,7 +0,0 @@
-import { getUsersData } from '@/api/supabaseCSR/supabase';
-import { useQuery } from '@tanstack/react-query';
-
-export const useQueryUsersData = (userId: string) => {
-  const { data = [], isPending } = useQuery({ queryKey: ['usersTableData'], queryFn: () => getUsersData(userId) });
-  return { data, isPending };
-};
