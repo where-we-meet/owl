db7cfdfeb40a837a3d4bbea26c1774c8c8dfd4c3
lauichan <te06230623@gmail.com>
2024-04-29 22:34:43 +0900

    refactor: image alt 속성 추가

--- src/components/room/StartMeeting.tsx
+++ src/components/room/StartMeeting.tsx
@@ -28,6 +28,7 @@ const StartMeeting = () => {
 
   const createRoomOption = async () => {
     setIsFetching(true);
+
     const [newRoom] = await insertNewRoom({
       name: roomNameGenerator(),
       created_by: userId,
@@ -47,6 +48,7 @@ const StartMeeting = () => {
       lat: null,
       lng: null
     });
+
     router.replace(`/room/${newRoom.id}`);
   };
 
