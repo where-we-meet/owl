60ab8482a5117f45cf4d53548a6a58dd902c3fce
heechul94 <yhcu3517@gmail.com>
2024-04-29 16:25:34 +0900

    feat: category icon svg 컴포넌트 생성

---
+++ src/components/room/place/search/CategoryIcons.tsx
@@ -0,0 +1,133 @@
+import type { CSSProperties } from 'react';
+
+export const BedIcon = (props: { style?: CSSProperties }) => (
+  <svg
+    id="Layer_1"
+    width="27"
+    height="27"
+    data-name="Layer 1"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 48 48"
+    {...props}
+  >
+    <defs>
+      <style>{'.cls-1 {fill:#000;stroke-width:0px;}    '}</style>
+    </defs>
+    <path
+      className="cls-1"
+      d="M10,24h28c2.21,0,4,1.79,4,4v2c0,.55-.45,1-1,1H7c-.55,0-1-.45-1-1v-2c0-2.21,1.79-4,4-4Z"
+    />
+    <path
+      className="cls-1"
+      d="M40,33H8c-1.1,0-2,.9-2,2v4c0,.55.45,1,1,1h1c.55,0,1-.45,1-1v-2h30v2c0,.55.45,1,1,1h1c.55,0,1-.45,1-1v-4c0-1.1-.9-2-2-2Z"
+    />
+    <path
+      className="cls-1"
+      d="M33,10H15c-2.76,0-5,2.24-5,5v6c0,.55.45,1,1,1h5v-1c0-2.76,2.24-5,5-5h6c2.76,0,5,2.24,5,5v1h5c.55,0,1-.45,1-1v-6c0-2.76-2.24-5-5-5Z"
+    />
+    <path className="cls-1" d="M20,18h8c1.1,0,2,.9,2,2v2h-12v-2c0-1.1.9-2,2-2Z" />
+  </svg>
+);
+
+export const BusIcon = (props: { style?: CSSProperties }) => (
+  <svg
+    id="Layer_1"
+    data-name="Layer 1"
+    xmlns="http://www.w3.org/2000/svg"
+    width="27"
+    height="27"
+    viewBox="0 0 48 48"
+    {...props}
+  >
+    <defs>
+      <style>{'.cls-1 {fill:#000;stroke-width:0px;}    '}</style>
+    </defs>
+    <path
+      className="cls-1"
+      d="M38,8H10c-1.1,0-2,.9-2,2v25c0,1.1.9,2,2,2h1v3c0,.55.45,1,1,1h3c.55,0,1-.45,1-1v-3h16v3c0,.55.45,1,1,1h3c.55,0,1-.45,1-1v-3h1c1.1,0,2-.9,2-2V10c0-1.1-.9-2-2-2ZM14,34c-.83,0-1.5-.67-1.5-1.5s.67-1.5,1.5-1.5,1.5.67,1.5,1.5-.67,1.5-1.5,1.5ZM14,28c-1.1,0-2-.9-2-2v-10c0-1.1.9-2,2-2h6c1.1,0,2,.9,2,2v10c0,1.1-.9,2-2,2h-6ZM29,33c0,.55-.45,1-1,1h-8c-.55,0-1-.45-1-1v-1c0-.55.45-1,1-1h8c.55,0,1,.45,1,1v1ZM34,34c-.83,0-1.5-.67-1.5-1.5s.67-1.5,1.5-1.5,1.5.67,1.5,1.5-.67,1.5-1.5,1.5ZM36,26c0,1.1-.9,2-2,2h-6c-1.1,0-2-.9-2-2v-10c0-1.1.9-2,2-2h6c1.1,0,2,.9,2,2v10Z"
+    />
+  </svg>
+);
+
+export const CafeIcon = (props: { style?: CSSProperties }) => (
+  <svg
+    id="Layer_1"
+    data-name="Layer 1"
+    xmlns="http://www.w3.org/2000/svg"
+    width="27px"
+    height="27px"
+    viewBox="0 0 48 48"
+    {...props}
+  >
+    <defs>
+      <style>{'.cls-1 {fill:#000;}.cls-1}'}</style>
+    </defs>
+    <rect className="cls-2" x={4} y={4} width={40} height={40} />
+    <path
+      className="cls-1"
+      d="M8,8h24c1.1,0,2,.9,2,2v12c0,6.62-5.38,12-12,12h-4c-6.62,0-12-5.38-12-12v-12c0-1.1.9-2,2-2Z"
+    />
+    <path
+      className="cls-1"
+      d="M39,16c.55,0,1,.45,1,1v3c0,.55-.45,1-1,1h-5v-5h5M39,12h-9v13h9c2.76,0,5-2.24,5-5v-3c0-2.76-2.24-5-5-5h0Z"
+    />
+    <rect className="cls-1" x={4} y={36} width={32} height={4} rx={2} ry={2} />
+  </svg>
+);
+
+export const MovieIcon = (props: { style?: CSSProperties }) => (
+  <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" {...props}>
+    <defs>
+      <style>{'.cls-1{fill: #000;}.cls-1,.cls-2 {stroke-width: 0px;}.cls-2 {fill: #fff}'}</style>
+    </defs>
+    <circle className="cls-1" cx={24} cy={24} r={17} />
+    <path className="cls-1" d="M24,37h15c1.1,0,2,.9,2,2h0c0,1.1-.9,2-2,2h-15v-4h0Z" />
+    <circle className="cls-2" cx={24} cy={14} r={4} />
+    <circle className="cls-2" cx={24} cy={34} r={4} />
+    <circle className="cls-2" cx={34} cy={24} r={4} />
+    <circle className="cls-2" cx={14} cy={24} r={4} />
+    <circle className="cls-2" cx={24} cy={24} r={2} />
+  </svg>
+);
+
+export const PantheonIcon = (props: { style?: CSSProperties }) => (
+  <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" {...props}>
+    <defs>
+      <style>{'.cls-1 {fill:#000;stroke-width:0px;}'}</style>
+    </defs>
+    <path
+      className="cls-1"
+      d="M39,22c.55,0,1-.45,1-1v-1c0-.55-.45-1-1-1H9c-.55,0-1,.45-1,1v1c0,.55.45,1,1,1h1v15h-1c-.55,0-1,.45-1,1v1c0,.55.45,1,1,1h30c.55,0,1-.45,1-1v-1c0-.55-.45-1-1-1h-1v-15h1ZM14,37v-15h4v15h-4ZM22,37v-15h4v15h-4ZM34,37h-4v-15h4v15Z"
+    />
+    <path className="cls-1" d="M24,8l-16,7v1c0,.55.45,1,1,1h30c.55,0,1-.45,1-1v-1l-16-7Z" />
+  </svg>
+);
+
+export const ParkingIcon = (props: { style?: CSSProperties }) => (
+  <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" {...props}>
+    <defs>
+      <style>{'.cls-1 {fill:#000;stroke-width:0px;}'}</style>
+    </defs>
+    <path
+      className="cls-1"
+      d="M36,8H12c-2.21,0-4,1.79-4,4v24c0,2.21,1.79,4,4,4h24c2.21,0,4-1.79,4-4V12c0-2.21-1.79-4-4-4ZM30,22c0,1.1-.9,2-2,2h-7v8c0,.55-.45,1-1,1h-1c-.55,0-1-.45-1-1v-16c0-1.1.9-2,2-2h8c1.1,0,2,.9,2,2v6Z"
+    />
+    <rect className="cls-1" x={21} y={17} width={6} height={4} rx={1} ry={1} />
+  </svg>
+);
+
+export const SpoonIcon = (props: { style?: CSSProperties }) => (
+  <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" {...props}>
+    <defs>
+      <style>{'.cls-1 {fill:#000;stroke-width:0px;}'}</style>
+    </defs>
+    <path
+      className="cls-1"
+      d="M15,6c-3.87,0-7,4.48-7,10,0,4.53,2.11,8.35,5,9.58v14.42c0,1.1.9,2,2,2s2-.9,2-2v-14.42c2.89-1.23,5-5.05,5-9.58,0-5.52-3.13-10-7-10Z"
+    />
+    <path
+      className="cls-1"
+      d="M38.5,6c-.83,0-1.5.67-1.5,1.5v10.5h-2.5V7.5c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5,1.5v10.5h-2.5V7.5c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5,1.5v14.5c0,2.21,1.79,4,4,4h1v14c0,1.1.9,2,2,2s2-.9,2-2v-14h1c2.21,0,4-1.79,4-4V7.5c0-.83-.67-1.5-1.5-1.5Z"
+    />
+  </svg>
+);
