ab7c0bc091525ddcdae1a14796067f3570782891
SohyangO <154496294+SohyangO@users.noreply.github.com>
2024-04-29 22:43:58 +0900

    style: 올빼미 이미지를 프로필 이미지로 변경

--- src/components/about/MemberCard.tsx
+++ src/components/about/MemberCard.tsx
@@ -1,24 +1,24 @@
-import Image from 'next/image';
 import Link from 'next/link';
-import styles from './MemberCard.module.css';
-import { FaGithub } from 'react-icons/fa6';
 import { Avatar } from '@nextui-org/react';
+import styles from './MemberCard.module.css';
 
 export const MemberCard = ({
   name,
   githubURL,
+  profile,
   handleMemberClick,
   index
 }: {
   name: string;
   githubURL: string;
+  profile: string;
   handleMemberClick: (index: number) => void;
   index: number;
 }) => {
   return (
     <div className={styles.profile} onClick={() => handleMemberClick(index)}>
       <div className={styles.profile_contents}>
-        <Avatar src="/images/about_owl_image.jpg" className={styles.profile_avatar} />
+        <Avatar src={profile} className={styles.profile_avatar} />
         <div className={styles.default_info}>
           <p>{name}</p>
           <Link href={githubURL} style={{ color: '#9d9d9d' }}>
